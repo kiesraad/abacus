8577f32c6814b7b1fd1d4f8ca972329d1f33f993
Les Kleuver <les.kleuver@gmail.com>
2024-03-14 16:20:35 +0100

    Add stricter typescript rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:playwright/recommended",

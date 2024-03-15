5aef55d7a95d52f08919ffe161163d203b0f7e58
Mark Janssen <20283+praseodym@users.noreply.github.com>
2024-03-15 15:16:24 +0100

    Move `openapi.json` check to test

--- backend/src/bin/gen-openapi.rs
+++ backend/src/bin/gen-openapi.rs
@@ -1,31 +1,28 @@
-use std::env;
 use std::fs;
-use std::io::Read;
-use std::process;
 
 use backend::create_openapi;
 
 fn main() {
+    let doc = get_openapi_json();
+    fs::write("./openapi.json", doc).expect("Could not write openapi.json");
+    println!("Updated openapi.json");
+}
+
+fn get_openapi_json() -> String {
     let openapi = create_openapi();
-    let doc = openapi
+    openapi
         .to_pretty_json()
-        .expect("Could not generate OpenAPI JSON");
+        .expect("Could not generate OpenAPI JSON")
+}
 
-    if env::args().any(|arg| arg == "--check") {
-        let mut current_doc = String::new();
-        fs::File::open("./openapi.json")
-            .expect("Could not open openapi.json")
-            .read_to_string(&mut current_doc)
-            .expect("Could not read openapi.json");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        if doc == current_doc {
-            println!("openapi.json is up to date");
-        } else {
-            println!("openapi.json is not up to date");
-            process::exit(1);
-        }
-    } else {
-        fs::write("./openapi.json", doc).expect("Could not write openapi.json");
-        println!("Updated openapi.json");
+    #[test]
+    fn generated_openapi_json_is_up_to_date() {
+        let newest = get_openapi_json();
+        let current = fs::read_to_string("./openapi.json").expect("Could not read openapi.json");
+        assert_eq!(newest, current, "openapi.json is not up to date");
     }
 }

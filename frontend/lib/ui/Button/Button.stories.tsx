280cf591dd4dc0692a0db7fa5cd4e7e2d0ec552b
Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>
2024-04-04 11:42:48 +0200

    adds stories and tests for Button component

--- frontend/lib/ui/Button/Button.stories.tsx
+++ frontend/lib/ui/Button/Button.stories.tsx
@@ -3,3 +3,25 @@ import type { Story } from "@ladle/react";
 import { Button } from "./Button";
 
 export const DefaultButton: Story = () => <Button>Click me</Button>;
+
+export const EnabledButton: Story<{
+    text:string;
+    label:string;
+}> = ({label, text}) => <Button type="button" aria-label={label} >{text}</Button>;
+
+DefaultButton.args = {
+    text: "Click me!",
+    label: "I'm a label!",
+}
+
+export const DisabledButton: Story<{
+    text:string;
+    label:string;
+    disabled: boolean;
+}> = ({label, disabled, text}) => <Button type="button" aria-label={label} disabled={disabled}>{text}</Button>;
+
+DisabledButton.args = {
+    text: "I'm disabled!",
+    label: "I'm a dislabel!",
+    disabled: true,
+}
--- frontend/lib/ui/Button/Button.test.tsx
+++ frontend/lib/ui/Button/Button.test.tsx
@@ -1,8 +1,37 @@
-import { render } from "@testing-library/react";
-import { Button } from "./Button";
+import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 
-test("The button works", () => {
-  render(<Button>Click me</Button>);
-  expect(true).toBe(true);
+import {DefaultButton, EnabledButton, DisabledButton} from "./Button.stories";
+
+
+test("The default button is enabled", () => {
+  render(<DefaultButton></DefaultButton>);
+
+  const buttonElement = screen.getByRole('button', {
+    name: 'Click me',
+  });
+
+  buttonElement.click();
+  expect(buttonElement).not.toBeDisabled();
+
+});
+
+test("The enabled button is enabled", () => {
+  render(<EnabledButton text="Click me!" label="default-button"></EnabledButton>);
+
+  const buttonElement = screen.getByRole('button', {
+    name: 'default-button',
+  });
+
+  expect(buttonElement).not.toBeDisabled();
+});
+
+test("The disabled button is disabled", () => {
+  render(<DisabledButton text="Try and click me!" label="disabled-button" disabled ></DisabledButton>);
+
+  const buttonElement = screen.getByRole('button', {
+    name: 'disabled-button',
+  });
+
+  expect(buttonElement).toBeDisabled();
 });

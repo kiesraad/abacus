ba8b9453864de0de67e6edb7b9ef4b188728bfd0
Les <les.kleuver@gmail.com>
2024-04-11 12:26:18 +0200

    Update frontend/playwright.config.ts
    Co-authored-by: Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -12,7 +12,7 @@ const config: PlaywrightTestConfig = defineConfig({
   retries: process.env.CI ? 2 : 0,
   // use all available cores (2) on github actions. default is 50%, use that locally
   workers: process.env.CI ? "100%" : undefined,
-  // timeout: 2 * 60 * 1000, // 2 minutes, surely overkill
+  timeout: 10 * 1000, // 10 seconds, because tests are small for now
   fullyParallel: true,
   use: {
     trace: "retain-on-failure",

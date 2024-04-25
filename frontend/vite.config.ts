2d902b2373ce437ab8eec8d00332125751ccfeb2
Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>
2024-04-25 15:25:21 +0200

    exclude msw from vite dep optimization to fix playwright tests (#29)
    exclude msw from vite dep optimization
    
    Playwright tests were failing, depending on which ones you run
    and on the number of Playwright workers. A consistent way to
    reproduce the problem was to run all tests with 3 Playwright workers.
    This caused the app e2e tests to fail beacuse of a 504 Outdated
    Optimize Dep response from node_modules/.vite/deps/msw.js.
    
    Excluding msw from the vite dep optimization seems to fix this issue.
    Note that using the --force flag or config option for vite dep
    optimization, might improve the situation, but didn't solve it.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(() => ({
     "process.env.MSW": true,
     "process.env.VERSION": JSON.stringify(pkgjson.version)
   },
+  optimizeDeps: { exclude: ["msw"] },
   plugins: [react()],
   resolve: {
     alias: mapObj(

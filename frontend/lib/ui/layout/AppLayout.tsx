7a406714d43ecdd3bba79ebb3ee6894e6f90c70e
Les <les.kleuver@gmail.com>
2024-04-29 13:59:26 +0200

    add app layout component

---
+++ frontend/lib/ui/layout/AppLayout.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import cls from "./applayout.module.css";
+
+export interface AppLayoutProps {
+  children: React.ReactNode;
+}
+
+export function AppLayout({ children }: AppLayoutProps) {
+  return <div className={cls["app-layout"]}>{children}</div>;
+}

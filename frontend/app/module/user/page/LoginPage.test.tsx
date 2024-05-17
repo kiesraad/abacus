77d398a50c53c0e9514dc294585190beff31b2e0
Lionqueen94 <lionqueen94@gmail.com>
2024-05-17 16:19:29 +0200

    Using the custom render function for LoginPage test

--- frontend/app/module/user/page/LoginPage.test.tsx
+++ frontend/app/module/user/page/LoginPage.test.tsx
@@ -1,15 +1,10 @@
-import { render } from "@testing-library/react";
+import { render } from "app/test/unit/test-utils";
 import { describe, expect, test } from "vitest";
 import { LoginPage } from "app/module/user";
-import { BrowserRouter } from "react-router-dom";
 
 describe("LoginPage", () => {
   test("Enter form field values", () => {
-    render(
-      <BrowserRouter>
-        <LoginPage />
-      </BrowserRouter>,
-    );
+    render(<LoginPage />);
     expect(true).toBe(true);
   });
 });

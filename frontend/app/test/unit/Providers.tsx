1fba71dbd75694dfa4aedfcec1fd127b3f593859
Lionqueen94 <lionqueen94@gmail.com>
2024-05-21 08:59:24 +0200

    Fixed failing test

--- frontend/app/test/unit/Providers.tsx
+++ frontend/app/test/unit/Providers.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { StrictMode } from "react";
 import { ApiProvider } from "@kiesraad/api";
+import { BrowserRouter } from "react-router-dom";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <StrictMode>
-      <ApiProvider host="http://testhost">{children}</ApiProvider>
+      <BrowserRouter>
+        <ApiProvider host="http://testhost">{children}</ApiProvider>
+      </BrowserRouter>
     </StrictMode>
   );
 };

6999233111e95f9d5e0d68b42fbef10b55e50664
Les <les.kleuver@gmail.com>
2024-05-27 09:48:02 +0200

    add Alert component

---
+++ frontend/lib/ui/Alert/Alert.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { AlertType } from "../ui.types";
+import cls from "./Alert.module.css";
+import { cn } from "@kiesraad/util";
+import { IconButton } from "../IconButton/IconButton";
+import { IconCross, IconError, IconThumbsup, IconWarning, IconInfo } from "@kiesraad/icon";
+
+export interface AlertProps {
+  type: AlertType;
+  children: React.ReactNode;
+  onClose?: () => void;
+}
+
+export function Alert({ type, onClose, children }: AlertProps) {
+  return (
+    <div className={cn(cls.alert, cls[type])}>
+      {onClose && (
+        <IconButton icon={<IconCross />} variant="ghost" size="md" isRound onClick={onClose} />
+      )}
+      <aside>{renderIconForType(type)}</aside>
+      <section>{children}</section>
+    </div>
+  );
+}
+
+function renderIconForType(type: AlertType) {
+  switch (type) {
+    case "error":
+      return <IconError />;
+    case "warning":
+      return <IconWarning />;
+    case "notify":
+      return <IconInfo />;
+    case "success":
+      return <IconThumbsup />;
+  }
+}

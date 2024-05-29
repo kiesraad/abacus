46b7bb2e46b8e47b5190544afe28cf812d89c325
Les <les.kleuver@gmail.com>
2024-05-29 14:00:22 +0200

    fix test

---
+++ frontend/lib/util/hook/usePreventFormEnterSubmit.test.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { describe, expect, test, vi } from "vitest";
+import { render } from "app/test/unit";
+import userEvent from "@testing-library/user-event";
+import { usePreventFormEnterSubmit } from "./usePreventFormEnterSubmit";
+
+const TestComponent = ({ cb }: { cb: (event: React.FormEvent) => void }) => {
+  const formRef = React.useRef<HTMLFormElement>(null);
+  usePreventFormEnterSubmit(formRef);
+  return (
+    <div>
+      <form ref={formRef} onSubmit={cb}>
+        <input id="input-test" type="text" />
+      </form>
+    </div>
+  );
+};
+
+describe("usePreventFormEnterSubmit", () => {
+  test("prevents form submission on Enter", async () => {
+    const fn = vi.fn((event: React.FormEvent) => {
+      event.preventDefault();
+    });
+    const { getByTestId } = render(<TestComponent cb={fn} />);
+
+    const input = getByTestId("input-test");
+    input.focus();
+    await userEvent.type(input, "hello{enter}");
+
+    expect(fn).not.toHaveBeenCalled();
+  });
+});

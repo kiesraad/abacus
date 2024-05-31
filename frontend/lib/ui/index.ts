eb1f489ffb4d9fef2e8e41346d0172fbf343f307
Lionqueen94 <lionqueen94@gmail.com>
2024-05-31 12:10:34 +0200

    Added use of Modal when clicking 'Invoer afbreken' Closes #38

--- frontend/lib/ui/index.ts
+++ frontend/lib/ui/index.ts
@@ -1,5 +1,6 @@
 import "./style/index.css";
 
+export * from "./Alert/Alert";
 export * from "./Badge";
 export * from "./BottomBar/BottomBar";
 export * from "./Button/Button";
@@ -7,5 +8,6 @@ export * from "./frame/AppFrame";
 export * from "./IconButton/IconButton";
 export * from "./InputField/InputField";
 export * from "./InputGrid/InputGrid";
-export * from "./ProgressList";
+export * from "./Modal/Modal";
+export * from "./ProgressList/ProgressList";
 export * from "./Spinner/Spinner";

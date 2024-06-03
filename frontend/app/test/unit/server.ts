77e0515b623bb3ac90c01edc2ba4af84a3c715e2
Les <les.kleuver@gmail.com>
2024-06-03 09:37:52 +0200

    restore license text

--- frontend/app/test/unit/server.ts
+++ frontend/app/test/unit/server.ts
@@ -1,3 +1,8 @@
+/* License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright Oxide Computer Company */
+
 import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
 

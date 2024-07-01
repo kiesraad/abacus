754d646cd0519b46e29e8aa71f816e46dd4a4239
cikzh <marlonpeeters@tweedegolf.com>
2024-07-01 16:00:20 +0200

    Unique constraint now on election_id and number

--- backend/migrations/3_polling_stations.sql
+++ backend/migrations/3_polling_stations.sql
@@ -1,9 +1,9 @@
 CREATE TABLE polling_stations
 (
-    id                    INTEGER PRIMARY KEY AUTOINCREMENT,
+    id                    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     election_id           INTEGER NOT NULL,
     name                  TEXT NOT NULL,
-    number                INTEGER NOT NULL UNIQUE,
+    number                INTEGER NOT NULL,
     number_of_voters      INTEGER,
     polling_station_type  TEXT NOT NULL,
     street                TEXT NOT NULL,
@@ -13,4 +13,5 @@ CREATE TABLE polling_stations
     locality              TEXT NOT NULL,
 
     FOREIGN KEY (election_id) REFERENCES elections(id)
+    CONSTRAINT number UNIQUE (election_id, number)
 );

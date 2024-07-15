42cd324df51a4ba52c6b457f7df2b9ec7629ae1f
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2024-07-15 17:12:00 +0200

    makes usage of (React)TestingLibrary consistent for CandidatesVotes tests

--- frontend/app/component/form/candidates_votes_form/CandidatesVotesForm.test.tsx
+++ frontend/app/component/form/candidates_votes_form/CandidatesVotesForm.test.tsx
@@ -63,7 +63,6 @@ describe("Test CandidatesVotesForm", () => {
     render(Component);
 
     const candidate1 = screen.getByTestId("candidate_votes-0.votes");
-    await user.clear(candidate1);
     await user.type(candidate1, "12345");
     expect(candidate1).toHaveValue("12.345");
 
@@ -85,7 +84,6 @@ describe("Test CandidatesVotesForm", () => {
 
     const candidate1 = screen.getByTestId("candidate_votes-0.votes");
     expect(candidate1).toHaveFocus();
-    await user.clear(candidate1);
     await user.type(candidate1, "12345");
     expect(candidate1).toHaveValue("12.345");
 
@@ -93,7 +91,6 @@ describe("Test CandidatesVotesForm", () => {
 
     const candidate2 = screen.getByTestId("candidate_votes-1.votes");
     expect(candidate2).toHaveFocus();
-    await user.clear(candidate2);
     await user.type(candidate2, "6789");
     expect(candidate2).toHaveValue("6.789");
 
@@ -101,7 +98,6 @@ describe("Test CandidatesVotesForm", () => {
 
     const candidate3 = screen.getByTestId("candidate_votes-2.votes");
     expect(candidate3).toHaveFocus();
-    await user.clear(candidate3);
     await user.type(candidate3, "123");
     expect(candidate3).toHaveValue("123");
 
@@ -109,7 +105,6 @@ describe("Test CandidatesVotesForm", () => {
 
     const candidate4 = screen.getByTestId("candidate_votes-3.votes");
     expect(candidate4).toHaveFocus();
-    await user.clear(candidate4);
     await user.paste("4242");
     expect(candidate4).toHaveValue("4.242");
 
@@ -117,7 +112,6 @@ describe("Test CandidatesVotesForm", () => {
 
     const candidate5 = screen.getByTestId("candidate_votes-4.votes");
     expect(candidate5).toHaveFocus();
-    await user.clear(candidate5);
     await user.type(candidate5, "12");
     expect(candidate5).toHaveValue("12");
 
@@ -125,7 +119,6 @@ describe("Test CandidatesVotesForm", () => {
 
     const candidate6 = screen.getByTestId("candidate_votes-5.votes");
     expect(candidate6).toHaveFocus();
-    await user.clear(candidate6);
     // Test if maxLength on field works
     await user.type(candidate6, "1000000000");
     expect(candidate6).toHaveValue("100.000.000");
@@ -134,7 +127,6 @@ describe("Test CandidatesVotesForm", () => {
 
     const candidate7 = screen.getByTestId("candidate_votes-6.votes");
     expect(candidate7).toHaveFocus();
-    await user.clear(candidate7);
     await user.type(candidate7, "3");
     expect(candidate7).toHaveValue("3");
 
@@ -143,7 +135,6 @@ describe("Test CandidatesVotesForm", () => {
     const total = screen.getByTestId("total");
     await user.click(total);
     expect(total).toHaveFocus();
-    await user.clear(total);
     await user.type(total, "555");
     expect(total).toHaveValue("555");
 
@@ -154,145 +145,137 @@ describe("Test CandidatesVotesForm", () => {
     expect(result).toHaveTextContent(/^Success$/);
   });
 
-  describe("CandidateVotesForm Api call", () => {
-    test("CandidateVotesForm request body is equal to the form data", async () => {
-      const spy = vi.spyOn(global, "fetch");
+  test("CandidateVotesForm request body is equal to the form data", async () => {
+    const spy = vi.spyOn(global, "fetch");
 
-      const politicalGroupMockData: PoliticalGroup = {
-        number: 1,
-        name: "Lijst 1 - Vurige Vleugels Partij",
-        candidates: [
+    const politicalGroupMockData: PoliticalGroup = {
+      number: 1,
+      name: "Lijst 1 - Vurige Vleugels Partij",
+      candidates: [
+        {
+          number: 1,
+          initials: "E.",
+          first_name: "Eldor",
+          last_name: "Zilverlicht",
+          locality: "Amsterdam",
+        },
+        {
+          number: 2,
+          initials: "G.",
+          first_name: "Grom",
+          last_name: "Donderbrul",
+          locality: "Rotterdam",
+        },
+      ],
+    };
+
+    const electionMockData: Election = {
+      id: 1,
+      name: "Municipal Election",
+      category: "Municipal",
+      election_date: "2024-11-30",
+      nomination_date: "2024-11-01",
+      political_groups: [
+        politicalGroupMockData,
+        {
+          number: 2,
+          name: "Lijst 2 - Wijzen van Water en Wind",
+          candidates: [
+            {
+              number: 1,
+              initials: "A.",
+              first_name: "Alice",
+              last_name: "Foo",
+              locality: "Amsterdam",
+              gender: "Female",
+            },
+            {
+              number: 2,
+              initials: "C.",
+              first_name: "Charlie",
+              last_name: "Doe",
+              locality: "Rotterdam",
+            },
+          ],
+        },
+      ],
+    };
+
+    const electionMock = electionMockData as Required<Election>;
+    const politicalGroupMock = politicalGroupMockData as Required<PoliticalGroup>;
+
+    const Component = (
+      <PollingStationFormController election={electionMock} pollingStationId={1} entryNumber={1}>
+        <CandidatesVotesForm group={politicalGroupMock} />
+      </PollingStationFormController>
+    );
+
+    const expectedRequest = {
+      data: {
+        ...rootRequest.data,
+        political_group_votes: [
           {
             number: 1,
-            initials: "E.",
-            first_name: "Eldor",
-            last_name: "Zilverlicht",
-            locality: "Amsterdam",
-          },
-          {
-            number: 2,
-            initials: "G.",
-            first_name: "Grom",
-            last_name: "Donderbrul",
-            locality: "Rotterdam",
-          },
-        ],
-      };
-
-      const electionMockData: Election = {
-        id: 1,
-        name: "Municipal Election",
-        category: "Municipal",
-        election_date: "2024-11-30",
-        nomination_date: "2024-11-01",
-        political_groups: [
-          politicalGroupMockData,
-          {
-            number: 2,
-            name: "Lijst 2 - Wijzen van Water en Wind",
-            candidates: [
+            total: 10,
+            candidate_votes: [
               {
                 number: 1,
-                initials: "A.",
-                first_name: "Alice",
-                last_name: "Foo",
-                locality: "Amsterdam",
-                gender: "Female",
+                votes: 5,
               },
               {
                 number: 2,
-                initials: "C.",
-                first_name: "Charlie",
-                last_name: "Doe",
-                locality: "Rotterdam",
+                votes: 5,
+              },
+            ],
+          },
+          {
+            number: 2,
+            total: 0,
+            candidate_votes: [
+              {
+                number: 1,
+                votes: 0,
+              },
+              {
+                number: 2,
+                votes: 0,
               },
             ],
           },
         ],
-      };
+      },
+    };
 
-      const electionMock = electionMockData as Required<Election>;
-      const politicalGroupMock = politicalGroupMockData as Required<PoliticalGroup>;
+    const user = userEvent.setup();
 
-      const Component = (
-        <PollingStationFormController election={electionMock} pollingStationId={1} entryNumber={1}>
-          <CandidatesVotesForm group={politicalGroupMock} />
-        </PollingStationFormController>
-      );
+    render(Component);
 
-      const expectedRequest = {
-        data: {
-          ...rootRequest.data,
-          political_group_votes: [
-            {
-              number: 1,
-              total: 10,
-              candidate_votes: [
-                {
-                  number: 1,
-                  votes: 5,
-                },
-                {
-                  number: 2,
-                  votes: 5,
-                },
-              ],
-            },
-            {
-              number: 2,
-              total: 0,
-              candidate_votes: [
-                {
-                  number: 1,
-                  votes: 0,
-                },
-                {
-                  number: 2,
-                  votes: 0,
-                },
-              ],
-            },
-          ],
-        },
-      };
+    await user.type(
+      screen.getByTestId("candidate_votes-0.votes"),
+      expectedRequest.data.political_group_votes[0]?.candidate_votes[0]?.votes.toString() ?? "0",
+    );
 
-      const user = userEvent.setup();
+    await user.type(
+      screen.getByTestId("candidate_votes-1.votes"),
+      expectedRequest.data.political_group_votes[0]?.candidate_votes[1]?.votes.toString() ?? "0",
+    );
 
-      render(Component);
+    await user.type(
+      screen.getByTestId("total"),
+      expectedRequest.data.political_group_votes[0]?.total.toString() ?? "0",
+    );
 
-      const candidateOne = screen.getByTestId("candidate_votes-0.votes");
-      await user.clear(candidateOne);
-      await user.type(
-        candidateOne,
-        expectedRequest.data.political_group_votes[0]?.candidate_votes[0]?.votes.toString() ?? "0",
-      );
+    const submitButton = screen.getByRole("button", { name: "Volgende" });
+    await user.click(submitButton);
 
-      const candidateTwo = screen.getByTestId("candidate_votes-1.votes");
-      await user.clear(candidateTwo);
-      await user.type(
-        candidateTwo,
-        expectedRequest.data.political_group_votes[0]?.candidate_votes[1]?.votes.toString() ?? "0",
-      );
+    expect(spy).toHaveBeenCalled();
+    const { url, method, body } = getUrlMethodAndBody(spy.mock.calls);
+    expect(url).toEqual("http://testhost/v1/api/polling_stations/1/data_entries/1");
+    expect(method).toEqual("POST");
+    expect(body).toEqual(expectedRequest);
 
-      const listTotal = screen.getByTestId("total");
-      await user.clear(listTotal);
-      await user.type(
-        listTotal,
-        expectedRequest.data.political_group_votes[0]?.total.toString() ?? "0",
-      );
-
-      const submitButton = screen.getByRole("button", { name: "Volgende" });
-      await user.click(submitButton);
-
-      expect(spy).toHaveBeenCalled();
-      const { url, method, body } = getUrlMethodAndBody(spy.mock.calls);
-      expect(url).toEqual("http://testhost/v1/api/polling_stations/1/data_entries/1");
-      expect(method).toEqual("POST");
-      expect(body).toEqual(expectedRequest);
-
-      const result = await screen.findByTestId("result");
-      expect(result).toHaveTextContent(/^Success$/);
-    });
+    const result = await screen.findByTestId("result");
+    expect(result).toHaveTextContent(/^Success$/);
   });
 
   test("422 response results in display of error message", async () => {
@@ -310,142 +293,52 @@ describe("Test CandidatesVotesForm", () => {
     expect(result).toHaveTextContent(/^Error422 error from mock$/);
   });
 
+  test("500 response results in display of error message", async () => {
+    overrideOnce("post", "/v1/api/polling_stations/1/data_entries/1", 500, {
+      message: "500 error from mock",
+      errorCode: "500_ERROR",
+    });
+
+    const user = userEvent.setup();
+
+    render(Component);
+
+    const submitButton = screen.getByRole("button", { name: "Volgende" });
+    await user.click(submitButton);
+    const result = await screen.findByTestId("feedback-server-error");
+    expect(result).toHaveTextContent(/^Error500 error from mock$/);
+  });
+
   describe("CandidatesVotesForm errors", () => {
-    test("500 response results in display of error message", async () => {
-      overrideOnce("post", "/v1/api/polling_stations/1/data_entries/1", 500, {
-        message: "500 error from mock",
-        errorCode: "500_ERROR",
+    test("F.31 IncorrectTotal group total", async () => {
+      overrideOnce("post", "/v1/api/polling_stations/1/data_entries/1", 200, {
+        saved: true,
+        message: "Data entry saved successfully",
+        validation_results: {
+          errors: [
+            {
+              fields: ["data.political_group_votes[0].total"],
+              code: "IncorrectTotal",
+            },
+          ],
+          warnings: [],
+        },
       });
 
+      render(Component);
       const user = userEvent.setup();
 
-      render(Component);
+      await user.type(screen.getByTestId("candidate_votes-0.votes"), "1");
+      await user.type(screen.getByTestId("candidate_votes-1.votes"), "2");
+      await user.type(screen.getByTestId("total"), "10");
 
       const submitButton = screen.getByRole("button", { name: "Volgende" });
       await user.click(submitButton);
-      const result = await screen.findByTestId("feedback-server-error");
-      expect(result).toHaveTextContent(/^Error500 error from mock$/);
+
+      const result = await screen.findByTestId("feedback-error");
+      expect(result).toHaveTextContent(/^IncorrectTotal$/);
+      expect(screen.queryByTestId("feedback-warning")).toBeNull();
+      expect(screen.queryByTestId("server-feedback-error")).toBeNull();
     });
-  });
-
-  test("Incorrect total is caught by validation", async () => {
-    overrideOnce("post", "/v1/api/polling_stations/1/data_entries/1", 200, {
-      saved: true,
-      message: "Data entry saved successfully",
-      validation_results: {
-        errors: [
-          {
-            fields: ["data.political_group_votes[0].total"],
-            code: "IncorrectTotal",
-          },
-        ],
-        warnings: [],
-      },
-    });
-
-    render(Component);
-    const user = userEvent.setup();
-
-    const candidateOne = screen.getByTestId("candidate_votes-0.votes");
-    await user.clear(candidateOne);
-    await user.type(candidateOne, "1");
-
-    const candidateTwo = screen.getByTestId("candidate_votes-1.votes");
-    await user.clear(candidateTwo);
-    await user.type(candidateTwo, "2");
-
-    const totalInput = screen.getByTestId("total");
-    await user.clear(totalInput);
-    await user.type(totalInput, "10");
-
-    const submitButton = screen.getByRole("button", { name: "Volgende" });
-    await user.click(submitButton);
-
-    const result = await screen.findByTestId("feedback-error");
-    expect(result).toHaveTextContent(/^IncorrectTotal$/);
-    expect(screen.queryByTestId("feedback-warning")).toBeNull();
-    expect(screen.queryByTestId("server-feedback-error")).toBeNull();
-  });
-});
-
-describe("CandidatesVotesForm warnings", () => {
-  test("W.21 AboveThreshold candidate votes", async () => {
-    overrideOnce("post", "/v1/api/polling_stations/1/data_entries/1", 200, {
-      saved: true,
-      message: "Data entry saved successfully",
-      validation_results: {
-        errors: [],
-        warnings: [
-          {
-            fields: ["data.political_group_votes[0].candidates[0].votes"],
-            code: "AboveThreshold",
-          },
-        ],
-      },
-    });
-
-    const user = userEvent.setup();
-
-    render(Component);
-
-    const candidateOne = screen.getByTestId("candidate_votes-0.votes");
-    await user.clear(candidateOne);
-    await user.type(candidateOne, "1");
-
-    const candidateTwo = screen.getByTestId("candidate_votes-1.votes");
-    await user.clear(candidateTwo);
-    await user.type(candidateTwo, "2");
-
-    const totalInput = screen.getByTestId("total");
-    await user.clear(totalInput);
-    await user.type(totalInput, "10");
-
-    const submitButton = screen.getByRole("button", { name: "Volgende" });
-    await user.click(submitButton);
-
-    const result = await screen.findByTestId("feedback-warning");
-    expect(result).toHaveTextContent(/^AboveThreshold$/);
-    expect(screen.queryByTestId("feedback-server-error")).toBeNull();
-    expect(screen.queryByTestId("feedback-error")).toBeNull();
-  });
-
-  test("W.21 AboveThreshold political group total votes", async () => {
-    overrideOnce("post", "/v1/api/polling_stations/1/data_entries/1", 200, {
-      saved: true,
-      message: "Data entry saved successfully",
-      validation_results: {
-        errors: [],
-        warnings: [
-          {
-            fields: ["data.political_group_votes[0].total"],
-            code: "AboveThreshold",
-          },
-        ],
-      },
-    });
-
-    const user = userEvent.setup();
-
-    render(Component);
-
-    const candidateOne = screen.getByTestId("candidate_votes-0.votes");
-    await user.clear(candidateOne);
-    await user.type(candidateOne, "1");
-
-    const candidateTwo = screen.getByTestId("candidate_votes-1.votes");
-    await user.clear(candidateTwo);
-    await user.type(candidateTwo, "2");
-
-    const totalInput = screen.getByTestId("total");
-    await user.clear(totalInput);
-    await user.type(totalInput, "10");
-
-    const submitButton = screen.getByRole("button", { name: "Volgende" });
-    await user.click(submitButton);
-
-    const result = await screen.findByTestId("feedback-warning");
-    expect(result).toHaveTextContent(/^AboveThreshold$/);
-    expect(screen.queryByTestId("feedback-server-error")).toBeNull();
-    expect(screen.queryByTestId("feedback-error")).toBeNull();
   });
 });

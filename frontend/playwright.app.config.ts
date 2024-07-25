f07cc09aa3b879c46f0ebf084b2cd55ebca5e585
Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>
2024-07-25 10:53:25 +0200

    splits e2e app and e2e lib-ui tests by using separate config files (#178)

---
+++ frontend/playwright.app.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig, devices, type PlaywrightTestConfig } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = defineConfig({
+  testDir: "./app/test/e2e",
+  testMatch: /\.e2e\.ts/,
+  // Fail the build on CI if you accidentally left test.only in the source code
+  forbidOnly: !!process.env.CI,
+  // Retry on CI only
+  retries: process.env.CI ? 2 : 0,
+  // use all available cores (2) on GitHub actions. default is 50%, use that locally
+  workers: process.env.CI ? "100%" : undefined,
+  timeout: 10 * 1000, // 10 seconds, because tests are small for now
+  fullyParallel: true,
+  use: {
+    trace: "retain-on-failure",
+    baseURL: "http://localhost:4009",
+    testIdAttribute: "id",
+  },
+  projects: [
+    {
+      name: "chrome",
+      use: {
+        contextOptions: {
+          permissions: ["clipboard-read", "clipboard-write"],
+        },
+        ...devices["Desktop Chrome"],
+      },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "safari",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+  // use different port, so it doesn't conflict with local dev server
+  webServer: [
+    {
+      command: "npm run start:msw -- --port 4009",
+      port: 4009,
+    },
+  ],
+});
+
+export default config;

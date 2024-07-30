c29cf0b48618eb1038af87ef3645ce7283f49593
Lionqueen94 <lionqueen94@gmail.com>
2024-07-30 11:36:34 +0300

    Improved rendering by waiting for pollingStation to exist

--- frontend/app/module/input/PollingStation/PollingStationLayout.tsx
+++ frontend/app/module/input/PollingStation/PollingStationLayout.tsx
@@ -31,50 +31,52 @@ export function PollingStationLayout() {
   }, [pollingStationId, pollingStations]);
 
   return (
-    <PollingStationFormController
-      election={election}
-      pollingStationId={parseInt(pollingStationId || "0")}
-      entryNumber={1}
-    >
-      <header>
-        <section>
-          <PollingStationNumber>{pollingStation?.number}</PollingStationNumber>
-          <h1>{pollingStation?.name}</h1>
-          <Badge type="first_entry" />
-        </section>
-        <section>
-          <Button variant="secondary" size="sm" onClick={changeDialog} rightIcon={<IconCross />}>
-            Invoer afbreken
-          </Button>
-          <WorkStationNumber>16</WorkStationNumber>
-        </section>
-      </header>
-      <main>
-        <nav>
-          <PollingStationProgress />
-        </nav>
-        <article>
-          <Outlet />
-        </article>
-      </main>
-      {openModal && (
-        <Modal onClose={changeDialog}>
-          <h2>Wat wil je doen met je invoer?</h2>
-          <p>
-            Ga je op een later moment verder met het invoeren van dit stembureau? Dan kan je de
-            invoer die je al hebt gedaan bewaren.
-            <br />
-            <br />
-            Twijfel je? Overleg dan met de coördinator.
-          </p>
-          <nav>
-            <Button size="lg">Invoer bewaren</Button>
-            <Button size="lg" variant="secondary">
-              Niet bewaren
+    pollingStation && (
+      <PollingStationFormController
+        election={election}
+        pollingStationId={parseInt(pollingStationId || "0")}
+        entryNumber={1}
+      >
+        <header>
+          <section>
+            <PollingStationNumber>{pollingStation.number}</PollingStationNumber>
+            <h1>{pollingStation.name}</h1>
+            <Badge type="first_entry" />
+          </section>
+          <section>
+            <Button variant="secondary" size="sm" onClick={changeDialog} rightIcon={<IconCross />}>
+              Invoer afbreken
             </Button>
+            <WorkStationNumber>16</WorkStationNumber>
+          </section>
+        </header>
+        <main>
+          <nav>
+            <PollingStationProgress />
           </nav>
-        </Modal>
-      )}
-    </PollingStationFormController>
+          <article>
+            <Outlet />
+          </article>
+        </main>
+        {openModal && (
+          <Modal onClose={changeDialog}>
+            <h2>Wat wil je doen met je invoer?</h2>
+            <p>
+              Ga je op een later moment verder met het invoeren van dit stembureau? Dan kan je de
+              invoer die je al hebt gedaan bewaren.
+              <br />
+              <br />
+              Twijfel je? Overleg dan met de coördinator.
+            </p>
+            <nav>
+              <Button size="lg">Invoer bewaren</Button>
+              <Button size="lg" variant="secondary">
+                Niet bewaren
+              </Button>
+            </nav>
+          </Modal>
+        )}
+      </PollingStationFormController>
+    )
   );
 }

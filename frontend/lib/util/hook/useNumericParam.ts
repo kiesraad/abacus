e3acf0b3c85ddd664ff2a870a7bf50e1bc6481f4
cikzh <marlonpeeters@tweedegolf.com>
2024-08-01 10:18:16 +0200

    Fixed regex also matching parts of a word

--- frontend/lib/util/hook/useNumericParam.ts
+++ frontend/lib/util/hook/useNumericParam.ts
@@ -4,11 +4,13 @@ import { useParams } from "react-router-dom";
 export function useNumericParam(p: string): number {
   const params = useParams();
   const param = params[p];
+
   if (!param) {
     throw Error(`Parameter ${p} does not exist`);
   }
 
-  if (!/\d+/.test(param)) {
+  // Matches only if the whole word is numeric.
+  if (!/\b\d+\b/.test(param)) {
     throw Error(`Parameter ${p} is not numeric`);
   }
 

df84617391867ac28f2c6f605eeb1fdee77688c6
cikzh <marlonpeeters@tweedegolf.com>
2024-08-02 09:38:55 +0200

    Better var names and added missing comment

--- frontend/lib/util/hook/useNumericParam.ts
+++ frontend/lib/util/hook/useNumericParam.ts
@@ -1,17 +1,19 @@
 import { useParams } from "react-router-dom";
 
-//
-export function useNumericParam(p: string): number {
+// Takes a parameter key, then tries to find it in the URL params.
+// When it is found, it tries to validate it as a number and returns it.
+// Throws an error when the paramater key is not found or not an integer
+export function useNumericParam(parameterKey: string): number {
   const params = useParams();
-  const param = params[p];
+  const param = params[parameterKey];
 
   if (!param) {
-    throw Error(`Parameter ${p} does not exist`);
+    throw Error(`Parameter ${parameterKey} does not exist`);
   }
 
   // Matches only if the whole string is numeric
   if (!/^\d+$/.test(param)) {
-    throw Error(`Parameter ${p} is not numeric`);
+    throw Error(`Parameter ${parameterKey} is not numeric`);
   }
 
   return parseInt(param, 10);

e3876a3386947252a5461bdb0dbbbfc9a9d94ef1
Ruben Nijveld <ruben@tweedegolf.com>
2024-08-07 09:46:08 +0200

    Add API command flag to seed data into the database on startup

--- backend/src/bin/api.rs
+++ backend/src/bin/api.rs
@@ -19,9 +19,18 @@ struct Args {
     /// Path to the frontend dist directory to serve through the API server
     #[arg(short, long)]
     frontend_dist: Option<PathBuf>,
-    // Server port, optional
+
+    /// Server port, optional
     #[arg(short, long, default_value_t = 8080)]
     port: u16,
+
+    /// Seed the database with initial data using the fixtures
+    #[arg(short, long)]
+    seed_data: bool,
+
+    /// Reset the database
+    #[arg(short, long)]
+    reset_database: bool,
 }
 
 /// Main entry point for the application, sets up the database and starts the
@@ -29,7 +38,7 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let pool = create_sqlite_pool().await?;
+    let pool = create_sqlite_pool(args.reset_database, args.seed_data).await?;
     let app = router(pool)?;
 
     let app = if let Some(fd) = args.frontend_dist {
@@ -49,9 +58,53 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 /// Create a SQLite database if needed, then connect to it and run migrations.
 /// Return a connection pool.
-async fn create_sqlite_pool() -> Result<SqlitePool, Box<dyn Error>> {
+async fn create_sqlite_pool(
+    reset_database: bool,
+    load_fixtures: bool,
+) -> Result<SqlitePool, Box<dyn Error>> {
     let opts = SqliteConnectOptions::from_str(DB_URL)?.create_if_missing(true);
+    if reset_database {
+        // remove the file, ignoring any errors that occured (such as the file not existing)
+        let _ = tokio::fs::remove_file(opts.get_filename()).await;
+    }
     let pool = SqlitePool::connect_with(opts).await?;
     sqlx::migrate!().run(&pool).await?;
+
+    if load_fixtures {
+        fixtures::seed_fixture_data(&pool).await?;
+    }
+
     Ok(pool)
+}
+
+mod fixtures {
+    macro_rules! load_fixture {
+        ($fixture:literal) => {
+            Fixture {
+                data: include_str!(concat!("../../fixtures/", $fixture, ".sql")),
+            }
+        };
+    }
+
+    macro_rules! load_fixtures {
+        ([$($fix:literal),* $(,)?]) => {
+            &[$(load_fixture!($fix),)*]
+        }
+    }
+
+    const FIXTURES: &[Fixture] = load_fixtures!(["elections", "polling_stations",]);
+
+    struct Fixture {
+        data: &'static str,
+    }
+
+    pub async fn seed_fixture_data(
+        pool: &sqlx::SqlitePool,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        for fixture in FIXTURES {
+            sqlx::raw_sql(fixture.data).execute(pool).await?;
+        }
+
+        Ok(())
+    }
 }

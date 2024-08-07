40ca42d22769971abf6c602d012e9d67729a9e98
cikzh <marlonpeeters@tweedegolf.com>
2024-08-07 15:30:36 +0200

    Fixed checking for non-existence

--- frontend/app/component/form/polling_station_choice/PollingStationChoiceForm.test.tsx
+++ frontend/app/component/form/polling_station_choice/PollingStationChoiceForm.test.tsx
@@ -159,7 +159,7 @@ describe("Test PollingStationChoiceForm", () => {
     await user.type(pollingStation, "abc");
 
     // Test that the alert disappeared
-    expect(pollingStationSubmitFeedback).toBeUndefined();
+    expect(screen.queryByTestId("pollingStationSubmitFeedback")).toBeNull();
 
     // Click submit again and see that the alert appeared again
     await user.click(submitButton);

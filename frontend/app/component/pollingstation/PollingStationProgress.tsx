eb6afe6765a3c98df6787c408ad653e01c9bbe1e
Lionqueen94 <lionqueen94@gmail.com>
2024-08-08 16:08:57 +0300

    Replaced link with span in nav when form is active

--- frontend/app/component/pollingstation/PollingStationProgress.tsx
+++ frontend/app/component/pollingstation/PollingStationProgress.tsx
@@ -15,30 +15,52 @@ export function PollingStationProgress() {
   return (
     <ProgressList>
       <ProgressList.Item key="recounted" status="accept" active={targetForm === "recounted"}>
-        <Link to={`/${election.id}/input/${pollingStationId}/recounted`}>Is er herteld?</Link>
+        {targetForm !== "recounted" ? (
+          <Link to={`/${election.id}/input/${pollingStationId}/recounted`}>Is er herteld?</Link>
+        ) : (
+          <span>Is er herteld?</span>
+        )}
       </ProgressList.Item>
       <ProgressList.Item key="numbers" status="idle" active={targetForm === "numbers"}>
-        <Link to={`/${election.id}/input/${pollingStationId}/numbers`}>
-          Aantal kiezers en stemmen
-        </Link>
+        {targetForm !== "numbers" ? (
+          <Link to={`/${election.id}/input/${pollingStationId}/numbers`}>
+            Aantal kiezers en stemmen
+          </Link>
+        ) : (
+          <span>Aantal kiezers en stemmen</span>
+        )}
       </ProgressList.Item>
       <ProgressList.Item key="differences" status="idle" active={targetForm === "differences"}>
-        <Link to={`/${election.id}/input/${pollingStationId}/differences`}>Verschillen</Link>
+        {targetForm !== "differences" ? (
+          <Link to={`/${election.id}/input/${pollingStationId}/differences`}>Verschillen</Link>
+        ) : (
+          <span>Verschillen</span>
+        )}
       </ProgressList.Item>
       <ProgressList.Ruler key="ruler1" />
       {lists.map((list, index) => {
         const listId = `${index + 1}`;
         return (
           <ProgressList.Item key={`list${listId}`} status="idle" active={listNumber === listId}>
-            <Link to={`/${election.id}/input/${pollingStationId}/list/${listId}`}>
-              Lijst {list.number} - {list.name}
-            </Link>
+            {listNumber !== listId ? (
+              <Link to={`/${election.id}/input/${pollingStationId}/list/${listId}`}>
+                Lijst {list.number} - {list.name}
+              </Link>
+            ) : (
+              <span>
+                Lijst {list.number} - {list.name}
+              </span>
+            )}
           </ProgressList.Item>
         );
       })}
       <ProgressList.Ruler key="ruler2" />
       <ProgressList.Item key="save" status="idle" active={targetForm === "save"}>
-        <Link to={`/${election.id}/input/${pollingStationId}/save`}>Controleren en opslaan</Link>
+        {targetForm !== "save" ? (
+          <Link to={`/${election.id}/input/${pollingStationId}/save`}>Controleren en opslaan</Link>
+        ) : (
+          <span>Controleren en opslaan</span>
+        )}
       </ProgressList.Item>
     </ProgressList>
   );

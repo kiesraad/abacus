1dd42fdc72c594f619d832004f26d2b7b97f45c5
Mark Janssen <20283+praseodym@users.noreply.github.com>
2024-08-08 18:24:39 +0200

    Clean up SQL migrations (#245)

--- backend/migrations/3_polling_station_data_entries.sql
+++ backend/migrations/3_polling_station_data_entries.sql
@@ -1,11 +1,11 @@
 CREATE TABLE polling_station_data_entries
 (
-    polling_station_id INTEGER,
-    entry_number       INTEGER,
+    polling_station_id INTEGER NOT NULL,
+    entry_number       INTEGER NOT NULL,
     data               BLOB,
-    CONSTRAINT pk
-        PRIMARY KEY (entry_number, polling_station_id)
-    FOREIGN KEY (polling_station_id) REFERENCES polling_stations(id)
+
+    PRIMARY KEY (entry_number, polling_station_id),
+    FOREIGN KEY (polling_station_id) REFERENCES polling_stations (id)
 );
 
 CREATE INDEX polling_station_id_index

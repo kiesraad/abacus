105677be5f84a8008c4926b8a939fdf0e0a2ca62
Mark Janssen <20283+praseodym@users.noreply.github.com>
2024-08-15 15:10:08 +0200

    Move NavBar styling to module CSS
    And change the NavBar component to a named function instead of a
    variable holding an arrow function.

--- frontend/app/component/navbar/NavBar.tsx
+++ frontend/app/component/navbar/NavBar.tsx
@@ -2,14 +2,16 @@ import * as React from "react";
 
 import { IconUser } from "@kiesraad/icon";
 
-export const NavBar = ({ children }: { children: React.ReactNode }) => {
+import styles from "./NavBar.module.css";
+
+export function NavBar({ children }: { children: React.ReactNode }) {
   return (
-    <nav aria-label="primary-navigation">
-      <div className="links">{children}</div>
-      <div className="userInfo">
+    <nav aria-label="primary-navigation" className={styles.navBar}>
+      <div className={styles.links}>{children}</div>
+      <div className={styles.userInfo}>
         <IconUser />
         <span>Invoerder</span>
       </div>
     </nav>
   );
-};
+}

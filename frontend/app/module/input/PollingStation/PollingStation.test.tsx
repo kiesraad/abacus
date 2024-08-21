63e89615a4a329f9e51e362416e94f651f610597
Mark Janssen <20283+praseodym@users.noreply.github.com>
2024-08-21 18:52:04 +0200

    Expand polling station data entry integration test

--- frontend/app/module/input/PollingStation/PollingStation.test.tsx
+++ frontend/app/module/input/PollingStation/PollingStation.test.tsx
@@ -7,6 +7,8 @@ import { describe, expect, test } from "vitest";
 import { routes } from "app/routes.tsx";
 import { Providers, screen, userTypeInputs, waitFor } from "app/test/unit";
 
+import { electionMockData } from "@kiesraad/api-mocks";
+
 const router = createMemoryRouter(routes, {
   future: {
     v7_normalizeFormMethod: true,
@@ -16,7 +18,7 @@ const router = createMemoryRouter(routes, {
 const render = () => rtlRender(<Providers router={router} />);
 
 describe("Polling Station data entry integration tests", () => {
-  test("It renders", async () => {
+  test("Can navigate through form", async () => {
     render();
 
     const user = userEvent.setup();
@@ -24,33 +26,72 @@ describe("Polling Station data entry integration tests", () => {
     expect(router.state.location.pathname).toEqual("/1/input/1");
 
     // this automatically redirects to /1/input/1/recounted
+    // → Recounted form ("Is er herteld?")
     await waitFor(() => {
       expect(router.state.location.pathname).toEqual("/1/input/1/recounted");
     });
 
-    await waitFor(() => {
-      expect(screen.getByTestId("recounted_form")).toBeInTheDocument();
-    });
-
+    expect(screen.getByTestId("recounted_form")).toBeInTheDocument();
     screen.getByTestId("no").click();
     expect(screen.getByTestId("no")).toBeChecked();
 
-    const submitButton = screen.getByRole("button", { name: "Volgende" });
-    await user.click(submitButton);
+    await user.click(screen.getByRole("button", { name: "Volgende" }));
 
+    // → Voters and votes form ("Aantal kiezers en stemmen" / "Toegelaten kiezers en uitgebrachte stemmen")
     await waitFor(() => {
       expect(screen.getByTestId("voters_and_votes_form")).toBeInTheDocument();
     });
 
+    const total_votes = electionMockData.political_groups.length * 10;
     await userTypeInputs(user, {
-      poll_card_count: 1,
-      proxy_certificate_count: 2,
-      voter_card_count: 3,
-      total_admitted_voters_count: 6,
-      votes_candidates_counts: 4,
-      blank_votes_count: 5,
-      invalid_votes_count: 6,
-      total_votes_cast_count: 15,
+      poll_card_count: total_votes,
+      proxy_certificate_count: 0,
+      voter_card_count: 0,
+      total_admitted_voters_count: total_votes,
+      votes_candidates_counts: total_votes,
+      blank_votes_count: 0,
+      invalid_votes_count: 0,
+      total_votes_cast_count: total_votes,
     });
+
+    await user.click(screen.getByRole("button", { name: "Volgende" }));
+
+    // → Differences form ("Verschillen" / "Verschil tussen aantal kiezers en getelde stemmen")
+    await waitFor(() => {
+      expect(screen.getByTestId("differences_form")).toBeInTheDocument();
+    });
+
+    await userTypeInputs(user, {
+      more_ballots_count: 0,
+      fewer_ballots_count: 0,
+      unreturned_ballots_count: 0,
+      too_few_ballots_handed_out_count: 0,
+      too_many_ballots_handed_out_count: 0,
+      other_explanation_count: 0,
+      no_explanation_count: 0,
+    });
+
+    await user.click(screen.getByRole("button", { name: "Volgende" }));
+
+    // → Political group candidates votes form (stemmen op kandidaten per politieke groepering)
+    for (const pg of electionMockData.political_groups) {
+      await waitFor(() => {
+        expect(screen.getByTestId(`candidates_form_${pg.number}`)).toBeInTheDocument();
+      });
+
+      await userTypeInputs(user, {
+        "candidate_votes[0].votes": 5,
+        "candidate_votes[1].votes": 5,
+        total: 10,
+      });
+
+      await user.click(screen.getByRole("button", { name: "Volgende" }));
+    }
+
+    // → Check and Save Page ("Controleren en opslaan")
+    // FIXME: the last political group page doesn't navigate to the Check and Save page
+    // await waitFor(() => {
+    //   expect(router.state.location.pathname).toEqual("/1/input/1/save");
+    // });
   });
 });

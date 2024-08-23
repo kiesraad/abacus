2056cbf3062053ef0417ea88bd60ca4875a63957
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2024-08-23 14:16:51 +0200

    extends d2d tests for aborting input

--- frontend/e2e-tests/dom-to-db-tests/abort-data-entry.e2e.ts
+++ frontend/e2e-tests/dom-to-db-tests/abort-data-entry.e2e.ts
@@ -5,7 +5,7 @@ import { RecountedPage } from "e2e-tests/page-objects/input/RecountedPgObj";
 import { VotersVotesPage } from "e2e-tests/page-objects/input/VotersVotesPgObj";
 
 test.describe("Abort data entry", () => {
-  test("Abort and save input from empty voters and votes page", async ({ page }) => {
+  test("Save input from empty voters and votes page", async ({ page }) => {
     await page.goto("/1/input/1/recounted");
 
     const recountedPage = new RecountedPage(page);
@@ -23,9 +23,11 @@ test.describe("Abort data entry", () => {
 
     const inputPage = new InputPage(page);
     await expect(inputPage.heading).toBeVisible();
+
+    // TODO: extend test as part of epic #137 resume data entry
   });
 
-  test("Abort and save input from voters and votes page with error", async ({ page }) => {
+  test("Save input from voters and votes page with error", async ({ page }) => {
     await page.goto("/1/input/1/recounted");
 
     const recountedPage = new RecountedPage(page);
@@ -44,12 +46,13 @@ test.describe("Abort data entry", () => {
     await abortInputModal.heading.waitFor();
     await abortInputModal.saveInput.click();
 
-    // TODO: uncomment once issue 212 implements the expected behavior
-    // const inputPage = new InputPage(page);
-    // await expect(inputPage.heading).toBeVisible();
+    const inputPage = new InputPage(page);
+    await expect(inputPage.heading).toBeVisible();
+
+    // TODO: extend test as part of epic #137 resume data entry
   });
 
-  test("Abort and save input from voters and votes page with warning", async ({ page }) => {
+  test("Save input from voters and votes page with warning", async ({ page }) => {
     await page.goto("/1/input/1/recounted");
 
     const recountedPage = new RecountedPage(page);
@@ -79,10 +82,110 @@ test.describe("Abort data entry", () => {
     await abortInputModal.heading.waitFor();
     await abortInputModal.saveInput.click();
 
-    // TODO: uncomment once issue 212 implements the expected behavior
-    // const inputPage = new InputPage(page);
-    // await expect(inputPage.heading).toBeVisible();
+    const inputPage = new InputPage(page);
+    await expect(inputPage.heading).toBeVisible();
+
+    // TODO: extend test as part of epic #137 resume data entry
   });
 
-  // TODO: same three tests but discard input instead of save input
+  test("Discard input from empty voters and votes page", async ({ page }) => {
+    await page.goto("/1/input/1/recounted");
+
+    const recountedPage = new RecountedPage(page);
+    await recountedPage.heading.waitFor();
+    await recountedPage.checkNoAndClickNext();
+
+    const votersVotesPage = new VotersVotesPage(page);
+    await votersVotesPage.heading.waitFor();
+
+    await votersVotesPage.abortInput.click();
+
+    const abortInputModal = new AbortInputModal(page);
+    await abortInputModal.heading.waitFor();
+
+    // TODO: replace checking API call with checking form is empty at the end of the test
+    // should be done as part of epic #137 resume data entry
+    const responsePromise = page.waitForResponse("**/polling_stations/1/data_entries/1");
+
+    await abortInputModal.discardInput.click();
+
+    const response = await responsePromise;
+    expect(response.request().method()).toBe("DELETE");
+
+    const inputPage = new InputPage(page);
+    await expect(inputPage.heading).toBeVisible();
+  });
+
+  test("Discard input from voters and votes page with error", async ({ page }) => {
+    await page.goto("/1/input/1/recounted");
+
+    const recountedPage = new RecountedPage(page);
+    await recountedPage.heading.waitFor();
+    await recountedPage.checkNoAndClickNext();
+
+    const votersVotesPage = new VotersVotesPage(page);
+    await votersVotesPage.heading.waitFor();
+    await votersVotesPage.voterCardCount.fill("1000");
+    await votersVotesPage.next.click();
+    await expect(votersVotesPage.error).toBeVisible();
+
+    await votersVotesPage.abortInput.click();
+
+    const abortInputModal = new AbortInputModal(page);
+    await abortInputModal.heading.waitFor();
+
+    // TODO: replace checking API call with checking form is empty at the end of the test
+    // should be done as part of epic #137 resume data entry
+    const responsePromise = page.waitForResponse("**/polling_stations/1/data_entries/1");
+
+    await abortInputModal.discardInput.click();
+
+    const response = await responsePromise;
+    expect(response.request().method()).toBe("DELETE");
+
+    const inputPage = new InputPage(page);
+    await expect(inputPage.heading).toBeVisible();
+  });
+
+  test("Discard input from voters and votes page with warning", async ({ page }) => {
+    await page.goto("/1/input/1/recounted");
+
+    const recountedPage = new RecountedPage(page);
+    await recountedPage.heading.waitFor();
+    await recountedPage.checkNoAndClickNext();
+
+    const votersVotesPage = new VotersVotesPage(page);
+    await votersVotesPage.heading.waitFor();
+    const voters = {
+      poll_card_count: "100",
+      proxy_certificate_count: "0",
+      voter_card_count: "0",
+      total_admitted_voters_count: "100",
+    };
+    const votes = {
+      votes_candidates_counts: "50",
+      blank_votes_count: "50", // exceeds threshold
+      invalid_votes_count: "0",
+      total_votes_cast_count: "100",
+    };
+    await votersVotesPage.fillInPageAndClickNext(voters, votes);
+    await expect(votersVotesPage.warning).toBeVisible();
+
+    await votersVotesPage.abortInput.click();
+
+    const abortInputModal = new AbortInputModal(page);
+    await abortInputModal.heading.waitFor();
+
+    // TODO: replace checking API call with checking form is empty at the end of the test
+    // should be done as part of epic #137 resume data entry
+    const responsePromise = page.waitForResponse("**/polling_stations/1/data_entries/1");
+
+    await abortInputModal.discardInput.click();
+
+    const response = await responsePromise;
+    expect(response.request().method()).toBe("DELETE");
+
+    const inputPage = new InputPage(page);
+    await expect(inputPage.heading).toBeVisible();
+  });
 });

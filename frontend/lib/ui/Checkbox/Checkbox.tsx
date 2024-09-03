cc5c01afcfe7a4060371f26f06dbbc141daf8a3e
Les <les.kleuver@gmail.com>
2024-09-03 12:40:15 +0200

    refactor checkbox to have custom design

--- frontend/lib/ui/Checkbox/Checkbox.tsx
+++ frontend/lib/ui/Checkbox/Checkbox.tsx
@@ -1,3 +1,6 @@
+import * as React from "react";
+
+import { IconCheckmark } from "@kiesraad/icon";
 import { cn } from "@kiesraad/util";
 
 import cls from "./Checkbox.module.css";
@@ -12,13 +15,26 @@ interface CheckboxProps {
 //TODO: you can't style a border for a checkbox, currently outline is used as a workaround but the border radius doesnt match
 
 export function Checkbox({ id, children, defaultChecked, hasError }: CheckboxProps) {
+  const [checked, setChecked] = React.useState(defaultChecked);
+
+  React.useEffect(() => {
+    setChecked(defaultChecked);
+  }, [defaultChecked]);
+
+  const toggleCheckbox = () => {
+    setChecked((prev) => !prev);
+  };
+
   return (
     <div
       className={cn(cls.checkbox, { "has-error": !!hasError })}
       aria-label="input"
       id={`checkbox-container-${id}`}
     >
-      <input type="checkbox" id={id} name={id} defaultChecked={defaultChecked} />
+      <div aria-hidden={true} onClick={toggleCheckbox} id={`checkbox-button-${id}`}>
+        {checked ? <IconCheckmark aria-label="Aangevinkt" /> : null}
+      </div>
+      <input type="checkbox" id={id} name={id} checked={checked} onChange={toggleCheckbox} />
       <label htmlFor={id}>{children}</label>
     </div>
   );

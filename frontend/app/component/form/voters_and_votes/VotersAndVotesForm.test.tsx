15195927c0ffe62f6b78b2aca797af8c5b3acfb5
Les <les.kleuver@gmail.com>
2024-09-09 14:48:07 +0200

    removed test code

--- frontend/app/component/form/voters_and_votes/VotersAndVotesForm.test.tsx
+++ frontend/app/component/form/voters_and_votes/VotersAndVotesForm.test.tsx
@@ -407,9 +407,11 @@ describe("Test VotersAndVotesForm", () => {
       expect(screen.getByTestId("blank_votes_count"), "100").toHaveValue("100");
 
       await waitFor(() => {
-        const bla = screen.getByRole("checkbox", {
-          name: "Ik heb de aantallen gecontroleerd met het papier en correct overgenomen.",
-        });
+        // const bla = screen.getByRole("checkbox", {
+        //   name: "Ik heb de aantallen gecontroleerd met het papier en correct overgenomen.",
+        // });
+        const bla = screen.getByTestId("voters_and_votes_form_ignore_warnings");
+        expect(bla).toBeInTheDocument();
         expect(bla).not.toBeVisible();
       });
     });

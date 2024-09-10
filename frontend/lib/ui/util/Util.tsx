f750d0f9c8ee67cfdf2629b2a6ccd95da7328fd8
Lionqueen94 <lionqueen94@gmail.com>
2024-09-10 11:22:34 +0200

    Added aria-labels to remaining error and warning icons and info and success icons

--- frontend/lib/ui/util/Util.tsx
+++ frontend/lib/ui/util/Util.tsx
@@ -4,12 +4,12 @@ import { AlertType } from "@kiesraad/ui";
 export function renderIconForType(type: AlertType) {
   switch (type) {
     case "error":
-      return <IconError />;
+      return <IconError aria-label={"bevat een fout"} />;
     case "warning":
-      return <IconWarning />;
+      return <IconWarning aria-label={"bevat een waarschuwing"} />;
     case "notify":
-      return <IconInfo />;
+      return <IconInfo aria-label={"bevat een kennisgeving"} />;
     case "success":
-      return <IconThumbsUp />;
+      return <IconThumbsUp aria-label={"bevat een bevestiging"} />;
   }
 }

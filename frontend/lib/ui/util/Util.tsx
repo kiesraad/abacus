fc2b06728ee8e283869520f02a9a0871000eaf66
Ellen <lionqueen94@gmail.com>
2024-09-13 10:46:25 +0200

    Apply suggestions from code review
    Co-authored-by: Joris Leker <joris.leker@valsplat.nl>

--- frontend/lib/ui/util/Util.tsx
+++ frontend/lib/ui/util/Util.tsx
@@ -4,12 +4,12 @@ import { AlertType } from "@kiesraad/ui";
 export function renderIconForType(type: AlertType) {
   switch (type) {
     case "error":
-      return <IconError aria-label={"bevat een fout"} />;
+      return <IconError aria-label={"Er gaat iets fout"} />;
     case "warning":
-      return <IconWarning aria-label={"bevat een waarschuwing"} />;
+      return <IconWarning aria-label={"Let op"} />;
     case "notify":
-      return <IconInfo aria-label={"bevat een kennisgeving"} />;
+      return <IconInfo aria-label={"Ter info"} />;
     case "success":
-      return <IconThumbsUp aria-label={"bevat een bevestiging"} />;
+      return <IconThumbsUp aria-label={"Gelukt!"} />;
   }
 }

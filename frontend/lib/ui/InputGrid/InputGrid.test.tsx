6c3c0799bad910ca95ad118509e15e1f1310d6b7
Les <les.kleuver@gmail.com>
2024-09-17 12:39:03 +0200

    remove focus tests

--- frontend/lib/ui/InputGrid/InputGrid.test.tsx
+++ frontend/lib/ui/InputGrid/InputGrid.test.tsx
@@ -1,4 +1,3 @@
-import { userEvent } from "@testing-library/user-event";
 import { describe, expect, test } from "vitest";
 
 import { render, screen } from "app/test/unit";
@@ -22,43 +21,5 @@ describe("InputGrid", () => {
     secondInput.focus();
     expect(firstInput.parentElement?.parentElement).not.toHaveClass("focused");
     expect(secondInput.parentElement?.parentElement).toHaveClass("focused");
-  });
-
-  test("Move focus arrow up and down and tab and enter", async () => {
-    render(<DefaultGrid />);
-
-    const firstInput = screen.getByTestId("input1");
-    const secondInput = screen.getByTestId("input2");
-    const thirdInput = screen.getByTestId("input3");
-
-    firstInput.focus();
-
-    await userEvent.keyboard("{arrowdown}");
-
-    expect(secondInput).toHaveFocus();
-
-    await userEvent.keyboard("{arrowup}");
-
-    expect(firstInput).toHaveFocus();
-
-    await userEvent.keyboard("{tab}");
-
-    expect(secondInput).toHaveFocus();
-
-    await userEvent.keyboard("{enter}");
-
-    expect(thirdInput).toHaveFocus();
-  });
-
-  test("Move to last input with shortcut", async () => {
-    render(<DefaultGrid />);
-
-    const firstInput = screen.getByTestId("input1");
-    const thirdInput = screen.getByTestId("input3");
-
-    firstInput.focus();
-
-    await userEvent.keyboard("{Shift>}{arrowdown}{/Shift}");
-    expect(thirdInput).toHaveFocus();
   });
 });

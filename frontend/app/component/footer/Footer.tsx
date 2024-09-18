b43d94827d77505f7c24254ed982fc828902f69c
Mark Janssen <20283+praseodym@users.noreply.github.com>
2024-09-18 13:48:55 +0200

    Add Git commit and branch details to footer
    Only for Vite builds. During Vite serve (e.g. `npm run dev`) the Git
    tree might change without a chance to reload the variables, and we do
    not want to show stale details.

--- frontend/app/component/footer/Footer.tsx
+++ frontend/app/component/footer/Footer.tsx
@@ -1,10 +1,36 @@
 export function Footer() {
+  const gitBranch = import.meta.env.GIT_BRANCH as string | undefined;
+  let gitBranchShort = gitBranch;
+  if (gitBranch && gitBranch.length > 32) {
+    gitBranchShort = gitBranch.substring(0, 32) + "…";
+  }
+  const gitCommit = import.meta.env.GIT_COMMIT as string | undefined;
+  const gitDirty = import.meta.env.GIT_DIRTY as boolean;
+  let mode = (import.meta.env.MODE as string | undefined) || "unknown";
+  mode = (mode[0]?.toUpperCase() ?? "") + mode.slice(1);
+
   return (
     <footer>
-      <section>Abacus</section>
       <section>
-        <strong>Server</strong> {process.env.MSW ? "Mocked" : "Live"} &nbsp;&nbsp;
-        <strong>Versie</strong> v{process.env.VERSION}
+        Abacus{" "}
+        {gitBranch && (
+          <>
+            — <a href={"https://github.com/kiesraad/abacus/tree/" + gitBranch}>{gitBranchShort}</a>
+          </>
+        )}
+      </section>
+      <section>
+        <strong>Server</strong> {process.env.MSW ? "Mock Service Worker" : "Live"} &nbsp;&nbsp;
+        <strong>Versie</strong>{" "}
+        {gitCommit ? (
+          gitDirty ? (
+            <>{gitCommit}-dirty</>
+          ) : (
+            <a href={"https://github.com/kiesraad/abacus/commit/" + gitCommit}>{gitCommit}</a>
+          )
+        ) : (
+          mode
+        )}
       </section>
     </footer>
   );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,60 +1,74 @@
 import react from "@vitejs/plugin-react-swc";
+import { execSync } from "node:child_process";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 
 import pkgjson from "./package.json";
 import tsConfig from "./tsconfig.json";
 
-const apiMode = process.env.API_MODE ?? "mock";
-const apiHost = process.env.API_HOST ?? (apiMode === "mock" ? "" : "http://localhost:8080");
-const viteHost = process.env.VITE_HOST ?? undefined;
-
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  build: {
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
-    sourcemap: true,
-    minify: false,
-    rollupOptions: {
-      input: {
-        app: "index.html",
-      },
-    },
-  },
-  define: {
-    "process.env.MSW": apiMode === "mock",
-    "process.env.VERSION": JSON.stringify(pkgjson.version),
-    "process.env.API_HOST": JSON.stringify(apiHost),
-  },
-  optimizeDeps: { exclude: ["msw"] },
-  plugins: [react()],
-  resolve: {
-    alias: mapObj(
-      tsConfig.compilerOptions.paths,
-      (k) => k.replace("/*", ""),
-      (paths) => {
-        if (!paths[0]) throw new Error("No path found");
-        return path.resolve(__dirname, paths[0].replace("/*", ""));
-      },
-    ),
-  },
-  server: {
-    port: 3000,
-    host: viteHost,
-    proxy: {
-      "/api": {
-        target: apiHost,
-        changeOrigin: true,
-      },
-    },
-  },
-  test: {
-    environment: "jsdom",
-    setupFiles: ["app/test/unit/setup.ts"],
-    includeSource: ["app/**/*.ts", "lib/**/*.ts"],
-  },
-}));
-
 const mapObj = <V0, V>(obj: Record<string, V0>, kf: (t: string) => string, vf: (t: V0) => V): Record<string, V> =>
   Object.fromEntries(Object.entries(obj).map(([k, v]) => [kf(k), vf(v)]));
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const apiMode = process.env.API_MODE ?? "mock";
+  const apiHost = process.env.API_HOST ?? (apiMode === "mock" ? "" : "http://localhost:8080");
+
+  let gitDetails = {};
+  if (command == "build") {
+    const gitDirty = execSync("git status --porcelain").toString().trimEnd().length > 0;
+    const gitBranch = execSync("git rev-parse --abbrev-ref HEAD").toString().trimEnd();
+    const gitCommit = execSync("git rev-parse --short HEAD").toString().trimEnd();
+    gitDetails = {
+      "import.meta.env.GIT_DIRTY": JSON.stringify(gitDirty),
+      "import.meta.env.GIT_BRANCH": JSON.stringify(gitBranch),
+      "import.meta.env.GIT_COMMIT": JSON.stringify(gitCommit),
+    };
+  }
+
+  return {
+    build: {
+      outDir: path.resolve(__dirname, "dist"),
+      emptyOutDir: true,
+      sourcemap: true,
+      minify: false,
+      rollupOptions: {
+        input: {
+          app: "index.html",
+        },
+      },
+    },
+    define: {
+      "process.env.MSW": apiMode === "mock",
+      "process.env.VERSION": JSON.stringify(pkgjson.version),
+      "process.env.API_HOST": JSON.stringify(apiHost),
+      ...gitDetails,
+    },
+    optimizeDeps: { exclude: ["msw"] },
+    plugins: [react()],
+    resolve: {
+      alias: mapObj(
+        tsConfig.compilerOptions.paths,
+        (k) => k.replace("/*", ""),
+        (paths) => {
+          if (!paths[0]) throw new Error("No path found");
+          return path.resolve(__dirname, paths[0].replace("/*", ""));
+        },
+      ),
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: apiHost,
+          changeOrigin: true,
+        },
+      },
+    },
+    test: {
+      environment: "jsdom",
+      setupFiles: ["app/test/unit/setup.ts"],
+      includeSource: ["app/**/*.ts", "lib/**/*.ts"],
+    },
+  } satisfies UserConfig;
+});

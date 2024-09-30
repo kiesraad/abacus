1f24ccc5ec57a57bf1da4cdd44f663dd18982950
Les <les.kleuver@gmail.com>
2024-09-30 18:34:02 +0200

    Disable autocomplete for inputs (#401)

--- frontend/lib/ui/NumberInput/NumberInput.tsx
+++ frontend/lib/ui/NumberInput/NumberInput.tsx
@@ -10,6 +10,7 @@ export interface NumberInputProps
 export function NumberInput({ id, ...inputProps }: NumberInputProps) {
   const props = {
     maxLength: 9,
+    autoComplete: "off",
     ...inputProps,
     defaultValue: inputProps.defaultValue ? formatNumber(inputProps.defaultValue) : "",
     type: "text",

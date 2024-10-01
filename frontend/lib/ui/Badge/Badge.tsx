9576278b7bdfc01f0be865bde9be55c21f3bdb3d
cikzh <marlonpeeters@tweedegolf.com>
2024-10-01 11:14:38 +0200

    Adding icon to badge

--- frontend/lib/ui/Badge/Badge.tsx
+++ frontend/lib/ui/Badge/Badge.tsx
@@ -1,17 +1,25 @@
-import { type PollingStationStatus } from "@kiesraad/api";
+import { ReactElement } from "react";
 
+import { type PollingStationStatus } from "@kiesraad/api";
+import { IconPencil } from "@kiesraad/icon";
+
+import { Icon } from "../Icon/Icon";
 import classes from "./badge.module.css";
 
-const typeToTextDict: { [S in PollingStationStatus]: string } = {
+const typeToLabel: { [S in PollingStationStatus]: string | ReactElement } = {
   first_entry: "1e invoer",
   definitive: "Definitief",
-  first_entry_in_progress: "1e invoer bezig",
+  first_entry_in_progress: (
+    <>
+      1e invoer bezig <Icon icon={<IconPencil />} />
+    </>
+  ),
 };
 
 export interface BadgeProps {
-  type: keyof typeof typeToTextDict;
+  type: keyof typeof typeToLabel;
 }
 
 export function Badge({ type }: BadgeProps) {
-  return <div className={`${classes[type]} ${classes.badge}`}>{typeToTextDict[type]}</div>;
+  return <div className={`${classes[type]} ${classes.badge}`}>{typeToLabel[type]}</div>;
 }

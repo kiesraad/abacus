ddcda7518c08885d1de9876cd1991493fec6da91
cikzh <marlonpeeters@tweedegolf.com>
2024-10-01 12:18:33 +0200

    Added test

--- frontend/lib/ui/Badge/Badge.test.tsx
+++ frontend/lib/ui/Badge/Badge.test.tsx
@@ -11,10 +11,23 @@ test("The definitive badge is visible", () => {
 
   expect(badgeElement).toBeVisible();
 });
+
 test("The first entry badge is visible", () => {
   render(<Badge type="first_entry" />);
 
   const badgeElement = screen.getByText("1e invoer");
 
   expect(badgeElement).toBeVisible();
+});
+
+test("The first entry in progress badge is visible", () => {
+  render(<Badge type="first_entry_in_progress" />);
+
+  const badgeElement = screen.getByText("1e invoer");
+  const badgeElementImg = screen.getByRole("img");
+
+  // Essentially points to the same element, but at least we test
+  // the presence of an icon this way
+  expect(badgeElement).toBeVisible();
+  expect(badgeElementImg).toBeVisible();
 });

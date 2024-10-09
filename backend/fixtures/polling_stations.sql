4044eca283dfaac11f4991e1252e9287ad3308a3
Mark Janssen <20283+praseodym@users.noreply.github.com>
2024-10-09 16:23:01 +0200

    Add demo election
    Co-authored-by: ring-ring-ring <76487016+ring-ring-ring@users.noreply.github.com>

--- backend/fixtures/polling_stations.sql
+++ backend/fixtures/polling_stations.sql
@@ -1,5 +1,9 @@
-INSERT INTO polling_stations (id, election_id, name, number, number_of_voters, polling_station_type, street, house_number, house_number_addition, postal_code, locality)
-VALUES
-(1, 1, 'Op Rolletjes', 33, NULL, 'mobiel', 'Rijksweg A12', '1', NULL, '1234 YQ', 'Den Haag'),
-(2, 1, 'Testplek', 34, 1000, 'bijzonder', 'Teststraat', '2', 'b', '1234 QY', 'Testdorp'),
-(3, 2, 'Testgebouw', 35, NULL, 'vaste_locatie', 'Testweg', '3', NULL, '1234 QA', 'Teststad');
+INSERT INTO polling_stations (id, election_id, name, number, number_of_voters, polling_station_type, street,
+                              house_number, house_number_addition, postal_code, locality)
+VALUES (1, 1, 'Op Rolletjes', 33, NULL, 'mobiel', 'Rijksweg A12', '1', NULL, '1234 YQ', 'Den Haag'),
+       (2, 1, 'Testplek', 34, 1000, 'bijzonder', 'Teststraat', '2', 'b', '1234 QY', 'Testdorp'),
+       (3, 2, 'Testgebouw', 35, NULL, 'vaste_locatie', 'Testweg', '3', NULL, '1234 QA', 'Teststad'),
+       (4, 3, 'Studio The Rules', 1, NULL, 'vaste_locatie', 'Gerontoplein', '1', NULL, '1337 YQ', 'Juinen'),
+       (5, 3, 'Buurtcentrum de Mattenklopper', 2, 1000, 'bijzonder', 'Complexiteitslaan', '2', 'b', '1337 QY',
+        'Juinen'),
+       (6, 3, 'Positivo Zaal', 3, NULL, 'vaste_locatie', 'Kerkweg', '3', NULL, '1337 QA', 'Juinen');

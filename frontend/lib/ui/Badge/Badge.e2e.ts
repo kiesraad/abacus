c1ddc4fd0fe13b5f7665b3c70acd2963df07937d
Lionqueen94 <lionqueen94@gmail.com>
2024-10-15 10:16:03 +0200

    Fixed e2e test

--- frontend/lib/ui/Badge/Badge.e2e.ts
+++ frontend/lib/ui/Badge/Badge.e2e.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 test("all badges are visible", async ({ page }) => {
   await page.goto("http://localhost:61000/?story=badge--all-badges");
 
-  const firstEntryBadge = page.getByText("1e invoer").filter({ hasText: /^1e invoer$/ });
+  const firstEntryBadge = page.getByText("1e invoer").filter({ hasNot: page.getByRole("img") });
   const firstEntryInProgressBadgeWithIcon = page.getByText("1e invoer").filter({ has: page.getByRole("img") });
   const definitiveBadge = page.getByText("Definitief");
   await expect(firstEntryBadge).toBeVisible();

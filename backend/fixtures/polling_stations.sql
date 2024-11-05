9aa737d1033219f23e209d27efc1f33ace78ee46
cikzh <marlonpeeters@tweedegolf.com>
2024-11-05 09:14:02 +0100

    Fixed casing in fixture

--- backend/fixtures/polling_stations.sql
+++ backend/fixtures/polling_stations.sql
@@ -2,8 +2,8 @@ INSERT INTO polling_stations (id, election_id, name, number, number_of_voters, p
                               house_number, house_number_addition, postal_code, locality)
 VALUES (1, 1, 'Op Rolletjes', 33, NULL, 'Mobile', 'Rijksweg A12', '1', NULL, '1234 YQ', 'Den Haag'),
        (2, 1, 'Testplek', 34, 1000, 'Special', 'Teststraat', '2', 'b', '1234 QY', 'Testdorp'),
-       (3, 2, 'Testgebouw', 35, NULL, 'Fixedlocation', 'Testweg', '3', NULL, '1234 QA', 'Teststad'),
-       (4, 3, 'Studio The Rules', 1, NULL, 'Fixedlocation', 'Gerontoplein', '1', NULL, '1337 YQ', 'Juinen'),
+       (3, 2, 'Testgebouw', 35, NULL, 'FixedLocation', 'Testweg', '3', NULL, '1234 QA', 'Teststad'),
+       (4, 3, 'Studio The Rules', 1, NULL, 'FixedLocation', 'Gerontoplein', '1', NULL, '1337 YQ', 'Juinen'),
        (5, 3, 'Buurtcentrum de Mattenklopper', 2, 1000, 'Special', 'Complexiteitslaan', '2', 'b', '1337 QY',
         'Juinen'),
        (6, 3, 'Positivo Zaal', 3, NULL, 'FixedLocation', 'Kerkweg', '3', NULL, '1337 QA', 'Juinen'),

2bcd2ce295e3bd8901be06484d36e5747411b198
Lionqueen94 <lionqueen94@gmail.com>
2024-11-05 16:31:16 +0100

    Updated ElectionReportPage with text and status as on Figma design

--- frontend/app/module/election/page/ElectionReportPage.tsx
+++ frontend/app/module/election/page/ElectionReportPage.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import { NavBar } from "app/component/navbar/NavBar";
 
 import { useElection, useElectionStatus } from "@kiesraad/api";
-import { Button, PageTitle } from "@kiesraad/ui";
+import { t, tx } from "@kiesraad/i18n";
+import { IconLock } from "@kiesraad/icon";
+import { Button, Icon, PageTitle } from "@kiesraad/ui";
 
 export function ElectionReportPage() {
   const { election } = useElection();
@@ -63,12 +65,24 @@ export function ElectionReportPage() {
       </NavBar>
       <header>
         <section>
-          <h1>{election.name}</h1>
+          <h1>{t("election_status.first_session")}</h1>
+        </section>
+        <section>
+          <div className="election_status">
+            <span>{tx("election_status.finish_first_session_data_entry_status")}</span>{" "}
+            <Icon size="md" color="error" icon={<IconLock />} />
+          </div>
         </section>
       </header>
       <main>
         <article>
-          <Button onClick={downloadResults}>Download proces-verbaal</Button>
+          <h2 className="form_title">{t("election_report.finish_data_entry_phase")}</h2>
+          <p className="md ul-larger-padding">
+            {t("election_report.about_to_stop_data_entry")}
+            {tx("election_report.data_entry_finish_steps_explanation")}
+            {t("election_report.for_recount_new_session_needed")}
+          </p>
+          <Button onClick={downloadResults}>{t("election_report.download_report")}</Button>
         </article>
       </main>
     </>

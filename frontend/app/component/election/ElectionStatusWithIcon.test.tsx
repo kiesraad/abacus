8740dbb4384d9046eba4e0d84b0c75f712a05dbb
Lionqueen94 <lionqueen94@gmail.com>
2024-11-14 12:01:46 +0100

    Added check for specific icon

--- frontend/app/component/election/ElectionStatusWithIcon.test.tsx
+++ frontend/app/component/election/ElectionStatusWithIcon.test.tsx
@@ -9,72 +9,72 @@ describe("ElectionStatusWithIcon", () => {
 
     expect(screen.getByText("Steminvoer bezig")).toBeVisible();
     expect(screen.getByText("(eerste zitting)")).toBeVisible();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckHeart");
 
     rerender(ElectionStatusWithIcon("FinishDataEntry", true, true));
 
     expect(screen.getByText("Steminvoer afronden")).toBeVisible();
     expect(screen.getByText("(eerste zitting)")).toBeVisible();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconLock");
 
     rerender(ElectionStatusWithIcon("DataEntryFinished", true, true));
 
     expect(screen.getByText("Steminvoer afgerond")).toBeVisible();
     expect(screen.getByText("(eerste zitting)")).toBeVisible();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckVerified");
 
     rerender(ElectionStatusWithIcon("DataEntryInProgress", true, false));
 
     expect(screen.getByText("Invoer gestart")).toBeVisible();
     expect(screen.getByText("(eerste zitting)")).toBeVisible();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckHeart");
 
     rerender(ElectionStatusWithIcon("FinishDataEntry", true, false));
 
     expect(screen.getByText("Steminvoer afronden")).toBeVisible();
     expect(screen.getByText("(eerste zitting)")).toBeVisible();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconLock");
 
     rerender(ElectionStatusWithIcon("DataEntryFinished", true, false));
 
     expect(screen.getByText("Steminvoer voltooid")).toBeVisible();
     expect(screen.getByText("(eerste zitting)")).toBeVisible();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckVerified");
 
     rerender(ElectionStatusWithIcon("DataEntryInProgress", false, true));
 
     expect(screen.getByText("Steminvoer bezig")).toBeVisible();
     expect(screen.queryByText("(eerste zitting)")).not.toBeInTheDocument();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckHeart");
 
     rerender(ElectionStatusWithIcon("FinishDataEntry", false, true));
 
     expect(screen.getByText("Steminvoer afronden")).toBeVisible();
     expect(screen.queryByText("(eerste zitting)")).not.toBeInTheDocument();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconLock");
 
     rerender(ElectionStatusWithIcon("DataEntryFinished", false, true));
 
     expect(screen.getByText("Steminvoer afgerond")).toBeVisible();
     expect(screen.queryByText("(eerste zitting)")).not.toBeInTheDocument();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckVerified");
 
     rerender(ElectionStatusWithIcon("DataEntryInProgress", false, false));
 
     expect(screen.getByText("Invoer gestart")).toBeVisible();
     expect(screen.queryByText("(eerste zitting)")).not.toBeInTheDocument();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckHeart");
 
     rerender(ElectionStatusWithIcon("FinishDataEntry", false, false));
 
     expect(screen.getByText("Steminvoer afronden")).toBeVisible();
     expect(screen.queryByText("(eerste zitting)")).not.toBeInTheDocument();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconLock");
 
     rerender(ElectionStatusWithIcon("DataEntryFinished", false, false));
 
     expect(screen.getByText("Steminvoer voltooid")).toBeVisible();
     expect(screen.queryByText("(eerste zitting)")).not.toBeInTheDocument();
-    expect(screen.getByRole("img")).toBeVisible();
+    expect(screen.getByRole("img")).toHaveAttribute("data-icon", "IconCheckVerified");
   });
 });

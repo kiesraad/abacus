70f72e696d1a60a07ae3ebcce16e46b532748ee7
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2024-11-27 12:46:10 +0100

    refactors code for data entry candidate selector

--- frontend/app/test/unit/test-utils.ts
+++ frontend/app/test/unit/test-utils.ts
@@ -7,6 +7,8 @@ import { expect } from "vitest";
 
 import { routes } from "app/routes";
 
+import { PoliticalGroup } from "@kiesraad/api";
+
 import { Providers } from "./Providers";
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
@@ -50,6 +52,15 @@ export async function userTypeInputs(user: UserEvent, inputs: { [key: string]: s
     await user.type(input, value.toString());
     expect(input).toHaveValue(value.toString());
   }
+}
+
+export function getCandidateFullNamesFromMockData(politicalGroupMockData: PoliticalGroup): string[] {
+  const candidateNames = politicalGroupMockData.candidates.map((candidate) => {
+    return candidate.first_name
+      ? `${candidate.last_name}, ${candidate.initials} (${candidate.first_name})`
+      : `${candidate.last_name}, ${candidate.initials}`;
+  });
+  return candidateNames;
 }
 
 export const setupTestRouter = () => {

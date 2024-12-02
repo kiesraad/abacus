016d22995b7f5e396aab619ecedd6726a9cf9e6d
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2024-12-02 12:26:22 +0100

    WIP: progress on eslint.config.new.js

--- frontend/eslint.config.new.js
+++ frontend/eslint.config.new.js
@@ -1,13 +1,33 @@
 import pluginJs from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
+import reactPlugin from "eslint-plugin-react";
+import react from "eslint-plugin-react";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{ts,tsx}"] }, // not js
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"], // not js
+    ignores: ["**/dist", "**/.eslintrc.cjs", "!**/.ladle/"],
+    // ...reactRecommended,
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      react,
+    },
+    languageOptions: { globals: globals.browser },
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat["jsx-runtime"],
+  },
+  pluginJs.configs.recommended, // should this bere here?
+  ...tseslint.configs.recommended, // should this bere here?
 ];

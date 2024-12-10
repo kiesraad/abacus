189b9aca1493a4a37034f36dc865dce6f1b89519
Oliver <oliver@sensibly.nl>
2024-12-10 10:49:53 +0100

    Delete polling station (#689)

--- frontend/app/module/polling_stations/page/PollingStationUpdatePage.test.tsx
+++ frontend/app/module/polling_stations/page/PollingStationUpdatePage.test.tsx
@@ -1,37 +1,34 @@
 import * as Router from "react-router";
 
+import { within } from "@testing-library/dom";
 import { screen, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { describe, expect, test, vi } from "vitest";
 
-import { overrideOnce, render } from "app/test/unit";
+import { overrideOnce, render, server } from "app/test/unit";
 
 import { ElectionProvider, PollingStation } from "@kiesraad/api";
 
 import { PollingStationUpdatePage } from "./PollingStationUpdatePage";
 
 describe("PollingStationCreatePage", () => {
+  const testPollingStation: PollingStation = {
+    id: 1,
+    election_id: 1,
+    number: 1,
+    name: "test",
+    street: "test",
+    postal_code: "1234",
+    locality: "test",
+    polling_station_type: "FixedLocation",
+    number_of_voters: 1,
+    house_number: "test",
+  };
+
   test("Shows form", async () => {
     vi.spyOn(Router, "useParams").mockReturnValue({ electionId: "1", pollingStationId: "1" });
 
-    const testPollingStation: PollingStation = {
-      id: 1,
-      election_id: 1,
-      number: 1,
-      name: "test",
-      street: "test",
-      postal_code: "1234",
-      locality: "test",
-      polling_station_type: "FixedLocation",
-      number_of_voters: 1,
-      house_number: "test",
-    };
-
-    overrideOnce(
-      "get",
-      `/api/polling_stations/${testPollingStation.id}`,
-      200,
-      testPollingStation satisfies PollingStation,
-    );
+    overrideOnce("get", `/api/polling_stations/${testPollingStation.id}`, 200, testPollingStation);
 
     render(
       <ElectionProvider electionId={1}>
@@ -51,25 +48,7 @@ describe("PollingStationCreatePage", () => {
     const navigate = vi.fn();
     vi.spyOn(Router, "useNavigate").mockReturnValue(navigate);
 
-    const testPollingStation: PollingStation = {
-      id: 1,
-      election_id: 1,
-      number: 1,
-      name: "test",
-      street: "test",
-      postal_code: "1234",
-      locality: "test",
-      polling_station_type: "FixedLocation",
-      number_of_voters: 1,
-      house_number: "test",
-    };
-
-    overrideOnce(
-      "get",
-      `/api/polling_stations/${testPollingStation.id}`,
-      200,
-      testPollingStation satisfies PollingStation,
-    );
+    overrideOnce("get", `/api/polling_stations/${testPollingStation.id}`, 200, testPollingStation);
 
     render(
       <ElectionProvider electionId={1}>
@@ -82,6 +61,83 @@ describe("PollingStationCreatePage", () => {
 
     await waitFor(() => {
       expect(navigate).toHaveBeenCalledWith("../?updated=1");
+    });
+  });
+
+  describe("Delete polling station", () => {
+    test("Returns to list page with a message", async () => {
+      const user = userEvent.setup();
+
+      vi.spyOn(Router, "useParams").mockReturnValue({ electionId: "1", pollingStationId: "1" });
+      const navigate = vi.fn();
+      vi.spyOn(Router, "useNavigate").mockReturnValue(navigate);
+
+      overrideOnce("get", `/api/polling_stations/${testPollingStation.id}`, 200, testPollingStation);
+
+      render(
+        <ElectionProvider electionId={1}>
+          <PollingStationUpdatePage />
+        </ElectionProvider>,
+      );
+
+      const deleteButton = await screen.findByRole("button", { name: "Stembureau verwijderen" });
+      await user.click(deleteButton);
+
+      const modal = await screen.findByTestId("modal-dialog");
+      expect(modal).toHaveTextContent("Stembureau verwijderen");
+
+      let request_method: string;
+      let request_url: string;
+
+      overrideOnce("delete", `/api/polling_stations/${testPollingStation.id}`, 200, "");
+      server.events.on("request:start", ({ request }) => {
+        request_method = request.method;
+        request_url = request.url;
+      });
+
+      const confirmButton = await within(modal).findByRole("button", { name: "Verwijderen" });
+      await user.click(confirmButton);
+
+      await waitFor(() => {
+        expect(request_method).toEqual("DELETE");
+        expect(request_url).toContain(`/api/polling_stations/${testPollingStation.id}`);
+      });
+
+      await waitFor(() => {
+        expect(navigate).toHaveBeenCalledWith("../?deleted=1%20(test)");
+      });
+    });
+
+    test("Shows an error message when delete was not possible", async () => {
+      const user = userEvent.setup();
+
+      vi.spyOn(Router, "useParams").mockReturnValue({ electionId: "1", pollingStationId: "1" });
+      const navigate = vi.fn();
+      vi.spyOn(Router, "useNavigate").mockReturnValue(navigate);
+
+      overrideOnce("get", `/api/polling_stations/${testPollingStation.id}`, 200, testPollingStation);
+      overrideOnce("delete", `/api/polling_stations/${testPollingStation.id}`, 422, {
+        error: "Invalid data",
+        fatal: false,
+        reference: "InvalidData",
+      });
+
+      render(
+        <ElectionProvider electionId={1}>
+          <PollingStationUpdatePage />
+        </ElectionProvider>,
+      );
+
+      const deleteButton = await screen.findByRole("button", { name: "Stembureau verwijderen" });
+      await user.click(deleteButton);
+
+      const modal = await screen.findByTestId("modal-dialog");
+      expect(modal).toHaveTextContent("Stembureau verwijderen");
+
+      const confirmButton = await within(modal).findByRole("button", { name: "Verwijderen" });
+      await user.click(confirmButton);
+
+      expect(await screen.findByRole("alert")).toHaveTextContent("Het stembureau kan niet meer verwijderd worden.");
     });
   });
 });

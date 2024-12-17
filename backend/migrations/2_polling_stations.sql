4922182c54ed183bf224ae49a50b4e0faa9eed6b
cikzh <marlonpeeters@tweedegolf.com>
2024-12-17 10:59:07 +0100

    Removed finished_at

--- backend/migrations/2_polling_stations.sql
+++ backend/migrations/2_polling_stations.sql
@@ -11,7 +11,6 @@ CREATE TABLE polling_stations
     house_number_addition TEXT,
     postal_code           TEXT                              NOT NULL,
     locality              TEXT                              NOT NULL,
-    finished_at           INTEGER,                                      
 
     FOREIGN KEY (election_id) REFERENCES elections (id),
     CONSTRAINT number UNIQUE (election_id, number)

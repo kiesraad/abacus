fea0fec47af69848f22a0744166a1cb99d35241d
Lionqueen94 <lionqueen94@gmail.com>
2024-12-24 22:08:13 +0100

    Fixed badge css classes and added missing tests

--- frontend/lib/ui/Badge/Badge.test.tsx
+++ frontend/lib/ui/Badge/Badge.test.tsx
@@ -12,6 +12,14 @@ test("The definitive badge is visible", () => {
   expect(badgeElement).toBeVisible();
 });
 
+test("The entries different badge is visible", () => {
+  render(<Badge type="entries_different" />);
+
+  const badgeElement = screen.getByText("Verschil invoer 1 en 2");
+
+  expect(badgeElement).toBeVisible();
+});
+
 test("The first entry badge is visible", () => {
   render(<Badge type="first_entry_not_started" />);
 
@@ -24,6 +32,26 @@ test("The first entry in progress badge is visible", () => {
   render(<Badge type="first_entry_in_progress" showIcon />);
 
   const badgeElement = screen.getByText("1e invoer");
+  const badgeElementImg = screen.getByRole("img");
+
+  // Essentially points to the same element, but at least we test
+  // the presence of an icon this way
+  expect(badgeElement).toBeVisible();
+  expect(badgeElementImg).toBeVisible();
+});
+
+test("The second entry badge is visible", () => {
+  render(<Badge type="second_entry_not_started" />);
+
+  const badgeElement = screen.getByText("2e invoer");
+
+  expect(badgeElement).toBeVisible();
+});
+
+test("The second entry in progress badge is visible", () => {
+  render(<Badge type="second_entry_in_progress" showIcon />);
+
+  const badgeElement = screen.getByText("2e invoer");
   const badgeElementImg = screen.getByRole("img");
 
   // Essentially points to the same element, but at least we test

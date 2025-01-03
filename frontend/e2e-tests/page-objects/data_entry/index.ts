322ba1501255d37e7acc5043c605856555d1d95d
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-01-03 16:26:03 +0100

    adds TODOs to index.ts of page objects

--- frontend/e2e-tests/page-objects/data_entry/index.ts
+++ frontend/e2e-tests/page-objects/data_entry/index.ts
@@ -1,10 +1,10 @@
 export * from "./AbortInputModalPgObj";
 export * from "./CandidatesListPgObj";
 export * from "./CheckAndSavePgObj";
-export * from "./DataEntryBasePgObj";
+export * from "./DataEntryBasePgObj"; // TODO: should go via actual page page object
 export * from "./DifferencesPgObj";
-export * from "./NavigationPanelPgObj";
+export * from "./NavigationPanelPgObj"; // TODO: should go via DataEntryBasePgObj
 export * from "./PollingStationChoicePgObj";
 export * from "./RecountedPgObj";
-export * from "./UnsavedChangesModalPgObj";
+export * from "./UnsavedChangesModalPgObj"; // TODO: should go via DataEntryBasePgObj
 export * from "./VotersAndVotesPgObj";

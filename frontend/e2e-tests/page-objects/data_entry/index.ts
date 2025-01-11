969fb94c0133fbc08e5faeafff693e47af44515f
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-01-11 17:42:16 +0100

    remove out-of-scope TODOs

--- frontend/e2e-tests/page-objects/data_entry/index.ts
+++ frontend/e2e-tests/page-objects/data_entry/index.ts
@@ -1,10 +1,10 @@
 export * from "./AbortInputModalPgObj";
 export * from "./CandidatesListPgObj";
 export * from "./CheckAndSavePgObj";
-export * from "./DataEntryBasePgObj"; // TODO: should go via actual page page object
+export * from "./DataEntryBasePgObj";
 export * from "./DifferencesPgObj";
-export * from "./NavigationPanelPgObj"; // TODO: should go via DataEntryBasePgObj
+export * from "./NavigationPanelPgObj";
 export * from "./PollingStationChoicePgObj";
 export * from "./RecountedPgObj";
-export * from "./UnsavedChangesModalPgObj"; // TODO: should go via DataEntryBasePgObj
+export * from "./UnsavedChangesModalPgObj";
 export * from "./VotersAndVotesPgObj";

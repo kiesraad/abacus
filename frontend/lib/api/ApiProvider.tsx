f6961761bfe377434222559f8651aa3baba8776e
Marlon Baeten <marlon@tweedegolf.com>
2025-01-28 20:59:35 +0100

    Subscribe to unauthorized events and unset the user when sessions expire

--- frontend/lib/api/ApiProvider.tsx
+++ frontend/lib/api/ApiProvider.tsx
@@ -1,7 +1,8 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 import { ApiState } from "./api.types";
 import { ApiClient } from "./ApiClient";
+import { ApiError } from "./ApiError";
 import { ApiProviderContext } from "./ApiProviderContext";
 import useSessionState from "./useSessionState";
 
@@ -10,9 +11,22 @@ export interface ApiProviderProps {
   initialUser?: boolean;
 }
 
+const client = new ApiClient();
+
 export function ApiProvider({ children, initialUser = true }: ApiProviderProps) {
-  const client = new ApiClient();
   const { user, setUser } = useSessionState(initialUser);
+
+  // Unset the current user when the API returns a 401,
+  // indicating that the sessions has expired or the user is not authenticated anymore
+  useEffect(() => {
+    const callback = (error: ApiError) => {
+      if (error.code === 401 && user) {
+        setUser(null);
+      }
+    };
+
+    return client.subscribeToApiErrors(callback);
+  }, [user, setUser]);
 
   const apiState: ApiState = {
     client,

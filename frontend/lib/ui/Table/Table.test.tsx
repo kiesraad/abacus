7ef0e2884fbe77f75a80ab25b74623cf8a388388
Oliver <oliver@sensibly.nl>
2025-01-30 15:09:38 +0100

    Vitest custom matcher toHaveTableContent() (#927)
    Co-authored-by: Mark Janssen <20283+praseodym@users.noreply.github.com>

--- frontend/lib/ui/Table/Table.test.tsx
+++ frontend/lib/ui/Table/Table.test.tsx
@@ -11,17 +11,12 @@ describe("Table", () => {
     render(<BasicTable />);
 
     const table = await screen.findByTestId("basic_table");
-    const rows = within(table).getAllByRole("row");
-
-    expect(rows.length).toBe(4);
-
-    expect(rows[0]).toHaveTextContent(/Number/);
-    expect(rows[0]).toHaveTextContent(/Fixed width/);
-    expect(rows[0]).toHaveTextContent(/Some value/);
-
-    expect(rows[1]).toHaveTextContent(/1/);
-    expect(rows[1]).toHaveTextContent(/some/);
-    expect(rows[1]).toHaveTextContent(/value/);
+    expect(table).toHaveTableContent([
+      ["Number", "Fixed width", "Some value"],
+      ["1", "some", "value"],
+      ["2", "other", "value"],
+      ["3", "another", "thing"],
+    ]);
   });
 
   test("LinkRow navigates to url when clicked", async () => {

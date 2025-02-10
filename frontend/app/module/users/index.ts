7c4c8cfe8185330b22ce97a97074e0d1cbca76f7
Oliver <oliver@sensibly.nl>
2025-02-10 11:22:50 +0100

    Extra index.ts for the users/create folder

--- frontend/app/module/users/index.ts
+++ frontend/app/module/users/index.ts
@@ -1,5 +1,2 @@
 export * from "./UserListPage";
-export * from "./create/UserCreateLayout";
-export * from "./create/UserCreateRolePage";
-export * from "./create/UserCreateTypePage";
-export * from "./create/UserCreateDetailsPage";
+export * from "./create";

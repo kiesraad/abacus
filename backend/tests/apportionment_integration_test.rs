90ffd8c5fff6d3087fc4944a2b6c3973fcf1c708
Lionqueen94 <lionqueen94@gmail.com>
2025-02-11 22:51:43 +0100

    Added integration test for apportionment endpoint for election without polling stations

--- backend/tests/apportionment_integration_test.rs
+++ backend/tests/apportionment_integration_test.rs
@@ -5,9 +5,12 @@ use sqlx::SqlitePool;
 use test_log::test;
 
 use crate::{
-    shared::{create_result, create_result_with_non_example_data_entry},
+    shared::{
+        create_and_finalise_data_entry, create_result, create_result_with_non_example_data_entry,
+    },
     utils::serve_api,
 };
+use abacus::election::Election;
 use abacus::{
     apportionment::{
         get_total_seats_from_apportionment_result, ElectionApportionmentResponse, Fraction,
@@ -136,11 +139,80 @@ async fn test_election_apportionment_error_drawing_of_lots_not_implemented(pool:
     assert_eq!(body.error, "Drawing of lots is required");
 }
 
-#[test(sqlx::test(fixtures(path = "../fixtures", scripts("election_3"))))]
-async fn test_election_apportionment_error_apportionment_not_available_until_data_entry_finalised(
+#[test(sqlx::test)]
+async fn test_election_apportionment_error_apportionment_not_available_no_polling_stations(
     pool: SqlitePool,
 ) {
     let addr = serve_api(pool).await;
+
+    // Create election without polling stations
+    let response = reqwest::Client::new()
+        .post(format!("http://{addr}/api/elections"))
+        .json(&serde_json::json!({
+            "name": "Test Election",
+            "location": "Test Location",
+            "number_of_voters": 100,
+            "category": "Municipal",
+            "number_of_seats": 29,
+            "election_date": "2026-01-01",
+            "nomination_date": "2026-01-01",
+            "status": "DataEntryInProgress",
+            "political_groups": [
+          {
+            "number": 1,
+            "name": "Political Group A",
+            "candidates": [
+              {
+                "number": 1,
+                "initials": "A.",
+                "first_name": "Alice",
+                "last_name": "Foo",
+                "locality": "Amsterdam",
+                "gender": "Female"
+              },
+              {
+                "number": 2,
+                "initials": "C.",
+                "first_name": "Charlie",
+                "last_name": "Doe",
+                "locality": "Rotterdam",
+                "gender": null
+              }
+            ]
+          }
+        ]
+        }))
+        .send()
+        .await
+        .unwrap();
+
+    // Ensure the response is what we expect
+    assert_eq!(response.status(), StatusCode::CREATED);
+    let election: Election = response.json().await.unwrap();
+
+    let url = format!(
+        "http://{}/api/elections/{}/apportionment",
+        addr, election.id
+    );
+    let response = reqwest::Client::new().post(&url).send().await.unwrap();
+
+    // Ensure the response is what we expect
+    assert_eq!(response.status(), StatusCode::PRECONDITION_FAILED);
+    let body: ErrorResponse = response.json().await.unwrap();
+    assert_eq!(
+        body.error,
+        "Election data entry first needs to be finalised"
+    );
+}
+
+#[test(sqlx::test(fixtures(path = "../fixtures", scripts("election_3"))))]
+async fn test_election_apportionment_error_apportionment_not_available_until_data_entries_finalised(
+    pool: SqlitePool,
+) {
+    let addr = serve_api(pool).await;
+
+    // Add and finalise first data entry
+    create_and_finalise_data_entry(&addr, 3, 1).await;
 
     let url = format!("http://{addr}/api/elections/3/apportionment");
     let response = reqwest::Client::new().post(&url).send().await.unwrap();

25221614e0c76817fa1aaf522a00577ac6aca442
Oliver <oliver@sensibly.nl>
2025-02-12 16:31:41 +0100

    Custom matcher toHaveTableContent spaces between elements (#1024)

--- frontend/lib/test/matchers.ts
+++ frontend/lib/test/matchers.ts
@@ -1,5 +1,22 @@
 import type { ExpectationResult } from "@vitest/expect";
 
+function isTextNode(node: Node): boolean {
+  return node.nodeType === Node.TEXT_NODE;
+}
+
+/**
+ * Get the text content, adding spaces between different child elements to make sure contents such as
+ * `<span>text</span><Badge />` and `<DisplayFractions />` can be tested in a more readable way.
+ */
+function getTextContent(node: Node): string {
+  const childNodes = Array.from(node.childNodes);
+  if (isTextNode(node) || childNodes.every(isTextNode)) {
+    return node.textContent?.trim() || "";
+  }
+
+  return childNodes.map(getTextContent).filter(Boolean).join(" ").trim();
+}
+
 function toHaveTableContent(htmlElement: unknown, expected: string[][]): ExpectationResult {
   if (!(htmlElement instanceof HTMLTableElement)) {
     return {
@@ -9,7 +26,7 @@ function toHaveTableContent(htmlElement: unknown, expected: string[][]): Expecta
   }
 
   const actual = Array.from(htmlElement.querySelectorAll("tr")).map((row) =>
-    Array.from(row.querySelectorAll("th, td")).map((cell) => cell.textContent?.trim() || ""),
+    Array.from(row.querySelectorAll("th, td")).map(getTextContent),
   );
 
   const pass = JSON.stringify(actual) === JSON.stringify(expected);

cc2983c4bc4ca4f03c99fbb335c8cb9b51eb5da0
Lionqueen94 <lionqueen94@gmail.com>
2025-02-14 15:29:46 +0100

    Fixed check for yesterday if it's a different month

--- frontend/lib/util/format.ts
+++ frontend/lib/util/format.ts
@@ -30,22 +30,35 @@ export function validateNumberString(s: string | null | undefined) {
   return !!result.match(/^(\d*\.?)$|^(\d{1,3}(\.\d{3})+\.?)$/g);
 }
 
+function isToday(date: Date): boolean {
+  const today = new Date();
+  return (
+    date.getDate() === today.getDate() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear()
+  );
+}
+
+function isYesterday(date: Date): boolean {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  return (
+    date.getDate() === yesterday.getDate() &&
+    date.getMonth() === yesterday.getMonth() &&
+    date.getFullYear() === yesterday.getFullYear()
+  );
+}
+
 export function formatDateTime(date: Date) {
   const today = new Date();
-  const yesterday = new Date();
-  yesterday.setDate(today.getDate() - 1);
-
   const timeString = date.toLocaleTimeString(t("date_locale"), { hour: "numeric", minute: "numeric" });
-  if (date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear()) {
-    if (date.getDate() === today.getDate()) {
-      // Today
-      return `${t("today")} ${timeString}`;
-    } else if (date.getDate() === yesterday.getDate()) {
-      // Yesterday
-      return `${t("yesterday")} ${timeString}`;
-    }
-  }
-  if (Math.round(Math.abs(Number(today) - Number(date)) / (24 * 60 * 60 * 1000)) < 7) {
+  if (isToday(date)) {
+    // Today
+    return `${t("today")} ${timeString}`;
+  } else if (isYesterday(date)) {
+    // Yesterday
+    return `${t("yesterday")} ${timeString}`;
+  } else if (Math.round(Math.abs(Number(today) - Number(date)) / (24 * 60 * 60 * 1000)) < 7) {
     // Within the past 3-6 days
     return date.toLocaleString(t("date_locale"), {
       weekday: "long",

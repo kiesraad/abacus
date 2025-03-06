ea541688609fca39f4b008d14a0d001e2e0ba91e
Les <les.kleuver@gmail.com>
2025-03-06 15:44:21 +0100

    changed reset to proceed due to abort control using navigate

--- frontend/app/component/form/data_entry/DataEntryNavigation.tsx
+++ frontend/app/component/form/data_entry/DataEntryNavigation.tsx
@@ -135,7 +135,7 @@ export function DataEntryNavigation({ onSubmit, currentValues }: DataEntryNaviga
     <Modal
       title={t("data_entry.abort.title")}
       onClose={() => {
-        blocker.reset();
+        blocker.proceed();
       }}
     >
       {tx("data_entry.abort.description")}

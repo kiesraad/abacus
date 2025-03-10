da58ca4909dfc7dfb8992c2b5658d86c22aa2835
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-03-10 08:45:49 +0100

    makes new e2e tests use storageState

--- frontend/e2e-tests/data-entry.warning.model.e2e.ts
+++ frontend/e2e-tests/data-entry.warning.model.e2e.ts
@@ -98,6 +98,10 @@ const votesEmpty: VotesCounts = {
   total_votes_cast_count: 0,
 };
 
+test.use({
+  storageState: "e2e-tests/state/typist.json",
+});
+
 test.describe("Data entry model test", () => {
   createTestModel(machine)
     .getSimplePaths()

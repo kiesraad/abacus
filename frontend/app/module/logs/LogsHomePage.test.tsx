73efea910a15f40f5a59823dede6c4fd5dd46a99
Marlon Baeten <marlon@tweedegolf.com>
2025-03-11 18:32:39 +0100

    Remove audit log message from table test

--- frontend/app/module/logs/LogsHomePage.test.tsx
+++ frontend/app/module/logs/LogsHomePage.test.tsx
@@ -18,17 +18,17 @@ describe("LogsHomePage", () => {
     const table = await screen.findByRole("table");
     expect(table).toBeVisible();
     expect(table).toHaveTableContent([
-      ["Nummer", "Tijdstip", "Werkplek", "Type", "Gebeurtenis", "Toelichting", "Gebruiker"],
-      ["24", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Sanne Molenaar (Beheerder)"],
-      ["22", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Sanne Molenaar (Beheerder)"],
-      ["23", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Sanne Molenaar (Beheerder)"],
-      ["21", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Sam Kuijpers (Invoerder)"],
-      ["19", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Mohammed van der Velden (Coördinator)"],
-      ["20", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Sam Kuijpers (Invoerder)"],
-      ["18", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Mohammed van der Velden (Coördinator)"],
-      ["16", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Mohammed van der Velden (Coördinator)"],
-      ["17", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Mohammed van der Velden (Coördinator)"],
-      ["14", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Sam Kuijpers (Invoerder)"],
+      ["Nummer", "Tijdstip", "Werkplek", "Type", "Gebeurtenis", "Gebruiker"],
+      ["24", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Sanne Molenaar (Beheerder)"],
+      ["22", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Sanne Molenaar (Beheerder)"],
+      ["23", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Sanne Molenaar (Beheerder)"],
+      ["21", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Sam Kuijpers (Invoerder)"],
+      ["19", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Mohammed van der Velden (Coördinator)"],
+      ["20", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Sam Kuijpers (Invoerder)"],
+      ["18", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Mohammed van der Velden (Coördinator)"],
+      ["16", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Mohammed van der Velden (Coördinator)"],
+      ["17", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Mohammed van der Velden (Coördinator)"],
+      ["14", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Sam Kuijpers (Invoerder)"],
     ]);
 
     const user = userEvent.setup();
@@ -36,17 +36,17 @@ describe("LogsHomePage", () => {
     await user.click(nextButton);
 
     expect(table).toHaveTableContent([
-      ["Nummer", "Tijdstip", "Werkplek", "Type", "Gebeurtenis", "Toelichting", "Gebruiker"],
-      ["15", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Sam Kuijpers (Invoerder)"],
-      ["13", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Sanne Molenaar (Beheerder)"],
-      ["11", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Sam Kuijpers (Invoerder)"],
-      ["12", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Sanne Molenaar (Beheerder)"],
-      ["10", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Sam Kuijpers (Invoerder)"],
-      ["8", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Mohammed van der Velden (Coördinator)"],
-      ["9", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Mohammed van der Velden (Coördinator)"],
-      ["6", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "", "Sam Kuijpers (Invoerder)"],
-      ["7", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Sam Kuijpers (Invoerder)"],
-      ["5", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "", "Sanne Molenaar (Beheerder)"],
+      ["Nummer", "Tijdstip", "Werkplek", "Type", "Gebeurtenis", "Gebruiker"],
+      ["15", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Sam Kuijpers (Invoerder)"],
+      ["13", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Sanne Molenaar (Beheerder)"],
+      ["11", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Sam Kuijpers (Invoerder)"],
+      ["12", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Sanne Molenaar (Beheerder)"],
+      ["10", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Sam Kuijpers (Invoerder)"],
+      ["8", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Mohammed van der Velden (Coördinator)"],
+      ["9", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Mohammed van der Velden (Coördinator)"],
+      ["6", "11 mrt 10:02", "-", "Succes", "Gebruiker ingelogd", "Sam Kuijpers (Invoerder)"],
+      ["7", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Sam Kuijpers (Invoerder)"],
+      ["5", "11 mrt 10:02", "-", "Succes", "Gebruiker uitgelogd", "Sanne Molenaar (Beheerder)"],
     ]);
   });
 });

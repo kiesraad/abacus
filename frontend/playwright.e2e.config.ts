747ca6a6d5a0bf9cdfcc9dfc0e9b61ae5eb8d72a
cikzh <marlonpeeters@tweedegolf.com>
2025-03-17 11:59:55 +0100

    Only pipe WebServer stdout locally

--- frontend/playwright.e2e.config.ts
+++ frontend/playwright.e2e.config.ts
@@ -34,7 +34,7 @@ const config: PlaywrightTestConfig = defineConfig({
     {
       command: returnWebserverCommand(),
       url: "http://127.0.0.1:8081",
-      stdout: "pipe",
+      stdout: process.env.LOCAL_CI ? "pipe" : "ignore",
     },
   ],
 });

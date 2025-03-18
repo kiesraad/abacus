2115db3bd2a1b5e59699442300cf1247f7f64084
Mark Janssen <20283+praseodym@users.noreply.github.com>
2025-03-18 14:33:14 +0100

    Clean up `utoipa_axum` import

--- backend/src/report/api.rs
+++ backend/src/report/api.rs
@@ -13,8 +13,7 @@ use crate::{
 };
 use axum::extract::{Path, State};
 use axum_extra::response::Attachment;
-use utoipa_axum::router::OpenApiRouter;
-use utoipa_axum::routes;
+use utoipa_axum::{router::OpenApiRouter, routes};
 use zip::{result::ZipError, write::SimpleFileOptions};
 
 pub fn router() -> OpenApiRouter<AppState> {

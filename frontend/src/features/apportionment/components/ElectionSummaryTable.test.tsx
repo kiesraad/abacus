1b981bb3f10a75d897d04232a4988462eb6aa667
Lionqueen94 <lionqueen94@gmail.com>
2025-03-22 01:41:17 +0100

    Updated frontend test data to make sure there is no list exhaustion

--- frontend/src/features/apportionment/components/ElectionSummaryTable.test.tsx
+++ frontend/src/features/apportionment/components/ElectionSummaryTable.test.tsx
@@ -21,8 +21,8 @@ describe("ElectionSummaryTable", () => {
     expect(table).toHaveTableContent([
       ["Kiesgerechtigden", "2.000", ""],
       ["Getelde stembiljetten", "1.205", "Opkomst: 60.25%"],
-      ["Blanco stemmen", "3", "0.25%"],
-      ["Ongeldige stemmen", "2", "0.17%"],
+      ["Blanco stemmen", "2", "0.17%"],
+      ["Ongeldige stemmen", "3", "0.25%"],
       ["Stemmen op kandidaten", "1.200", ""],
       ["Aantal raadszetels", "23", ""],
       ["Kiesdeler", "52 4/23", "Benodigde stemmen per volle zetel"],
@@ -44,8 +44,8 @@ describe("ElectionSummaryTable", () => {
     expect(table).toHaveTableContent([
       ["Kiesgerechtigden", "", ""],
       ["Getelde stembiljetten", "1.205", ""],
-      ["Blanco stemmen", "3", "0.25%"],
-      ["Ongeldige stemmen", "2", "0.17%"],
+      ["Blanco stemmen", "2", "0.17%"],
+      ["Ongeldige stemmen", "3", "0.25%"],
       ["Stemmen op kandidaten", "1.200", ""],
       ["Aantal raadszetels", "23", ""],
       ["Kiesdeler", "52 4/23", "Benodigde stemmen per volle zetel"],

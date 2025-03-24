72edd50e5cec470228e40a338a323e16af559e61
cikzh <marlonpeeters@tweedegolf.com>
2025-03-24 17:22:22 +0100

    Added page layout

--- frontend/src/features/election_management/components/ElectionCreatePage.tsx
+++ frontend/src/features/election_management/components/ElectionCreatePage.tsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router";
 
 import { Footer } from "@/components/footer/Footer";
 import { NavBar } from "@/components/navbar/NavBar";
-import { FileInput, PageTitle } from "@/components/ui";
+import { FileInput, PageTitle, ProgressList, StickyNav } from "@/components/ui";
 
 import { t } from "@kiesraad/i18n";
 
@@ -19,6 +19,35 @@ export function ElectionCreatePage() {
         </section>
       </header>
       <main>
+        <StickyNav>
+          <ProgressList>
+            <ProgressList.Fixed>
+              <ProgressList.Item key="election_definition" status="idle" active>
+                <span>{t("election_definition")}</span>
+              </ProgressList.Item>
+              <ProgressList.Item key="polling_station_role" status="idle">
+                <span>{t("polling_station.role")}</span>
+              </ProgressList.Item>
+              <ProgressList.Item key="list_of_candidates" status="idle">
+                <span>{t("list_of_candidates.plural")}</span>
+              </ProgressList.Item>
+              <ProgressList.Item key="polling_stations" status="idle">
+                <span>{t("polling_stations")}</span>
+              </ProgressList.Item>
+              <ProgressList.Item key="counting_method_type" status="idle">
+                <span>{t("counting_method_type")}</span>
+              </ProgressList.Item>
+            </ProgressList.Fixed>
+            <div className="mt-md">
+              <ProgressList.Fixed>
+                <ProgressList.Item key="check_and_save" status="idle">
+                  <span>{t("check_and_save.title")}</span>
+                </ProgressList.Item>
+              </ProgressList.Fixed>
+            </div>
+          </ProgressList>
+        </StickyNav>
+
         <article>
           <h2>Importeer verkiezingsdefinitie</h2>
           <p className="mt-lg mb-lg">

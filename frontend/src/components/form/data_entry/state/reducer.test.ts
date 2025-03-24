95c10c897af207ec734e93efd42eea569f121c2a
Mark Janssen <20283+praseodym@users.noreply.github.com>
2025-03-24 09:27:30 +0100

    Data entry reducer cleanup (#1217)

--- frontend/src/components/form/data_entry/state/reducer.test.ts
+++ frontend/src/components/form/data_entry/state/reducer.test.ts
@@ -2,9 +2,8 @@ import { expect, test } from "vitest";
 
 import { mockElection } from "@/components/election/status/mockData";
 
-import { ApiResponseStatus } from "@kiesraad/api";
+import { ApiResponseStatus, Election, PollingStationResults } from "@kiesraad/api";
 
-import { getInitialValues as _getInitialValues } from "./dataEntryUtils";
 import dataEntryReducer, { getInitialState as _getInitialState } from "./reducer";
 import { DataEntryAction, DataEntryState } from "./types";
 
@@ -12,13 +11,53 @@ function getInitialState(): DataEntryState {
   return _getInitialState(mockElection, 1, 1);
 }
 
+export function _getInitialValues(
+  election: Required<Election>,
+  defaultValues?: Partial<PollingStationResults>,
+): PollingStationResults {
+  return {
+    recounted: undefined,
+    voters_counts: {
+      poll_card_count: 0,
+      proxy_certificate_count: 0,
+      voter_card_count: 0,
+      total_admitted_voters_count: 0,
+    },
+    votes_counts: {
+      votes_candidates_count: 0,
+      blank_votes_count: 0,
+      invalid_votes_count: 0,
+      total_votes_cast_count: 0,
+    },
+    voters_recounts: undefined,
+    differences_counts: {
+      more_ballots_count: 0,
+      fewer_ballots_count: 0,
+      unreturned_ballots_count: 0,
+      too_few_ballots_handed_out_count: 0,
+      too_many_ballots_handed_out_count: 0,
+      other_explanation_count: 0,
+      no_explanation_count: 0,
+    },
+    political_group_votes: election.political_groups.map((pg) => ({
+      number: pg.number,
+      total: 0,
+      candidate_votes: pg.candidates.map((c) => ({
+        number: c.number,
+        votes: 0,
+      })),
+    })),
+    ...defaultValues,
+  };
+}
+
 function getInitialValues() {
   return _getInitialValues(mockElection);
 }
 
-test("should handle DATA_ENTRY_LOADED with client_state", () => {
+test("should handle DATA_ENTRY_CLAIMED with client_state", () => {
   const action: DataEntryAction = {
-    type: "DATA_ENTRY_LOADED",
+    type: "DATA_ENTRY_CLAIMED",
     dataEntry: {
       client_state: null,
       data: getInitialValues(),
@@ -35,19 +74,9 @@ test("should handle DATA_ENTRY_LOADED with client_state", () => {
   expect(state.error).toBeNull();
 });
 
-test("should handle DATA_ENTRY_NOT_FOUND", () => {
+test("should handle DATA_ENTRY_CLAIM_FAILED", () => {
   const action: DataEntryAction = {
-    type: "DATA_ENTRY_NOT_FOUND",
-  };
-
-  const state = dataEntryReducer(getInitialState(), action);
-  expect(state.error).toBeNull();
-  expect(state.pollingStationResults).toStrictEqual(getInitialValues());
-});
-
-test("should handle DATA_ENTRY_LOAD_FAILED", () => {
-  const action: DataEntryAction = {
-    type: "DATA_ENTRY_LOAD_FAILED",
+    type: "DATA_ENTRY_CLAIM_FAILED",
     error: {
       message: "error",
       code: 1,

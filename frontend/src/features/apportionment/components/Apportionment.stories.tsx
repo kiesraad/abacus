93d7be050b9d669ca6c49e5ba755de7205be1db9
Oliver <oliver@sensibly.nl>
2025-03-25 16:37:24 +0100

    Shorten imports, use correct dataset for each table, add remaining tables

--- frontend/src/features/apportionment/components/Apportionment.stories.tsx
+++ frontend/src/features/apportionment/components/Apportionment.stories.tsx
@@ -1,14 +1,16 @@
 import { Story } from "@ladle/react";
 
 import { PoliticalGroup } from "@/api";
-import { ElectionSummaryTable } from "@/features/apportionment/components/ElectionSummaryTable";
-import { LargestAveragesFor19OrMoreSeatsTable } from "@/features/apportionment/components/residual_seats/LargestAveragesFor19OrMoreSeatsTable";
-import { LargestAveragesForLessThan19SeatsTable } from "@/features/apportionment/components/residual_seats/LargestAveragesForLessThan19SeatsTable";
-import { LargestRemaindersTable } from "@/features/apportionment/components/residual_seats/LargestRemaindersTable";
-import { largest_average_steps, largest_remainder_steps } from "@/features/apportionment/testing/less-than-19-seats";
 
-import { election, election_summary, seat_assignment } from "../testing/19-or-more-seats";
+import * as equalOrMore from "../testing/19-or-more-seats";
+import * as lessThan from "../testing/less-than-19-seats";
 import { ApportionmentTable } from "./ApportionmentTable";
+import { ElectionSummaryTable } from "./ElectionSummaryTable";
+import { FullSeatsTable } from "./full_seats/FullSeatsTable";
+import { ResidualSeatsCalculationTable } from "./full_seats/ResidualSeatsCalculationTable";
+import { LargestAveragesFor19OrMoreSeatsTable } from "./residual_seats/LargestAveragesFor19OrMoreSeatsTable";
+import { LargestAveragesForLessThan19SeatsTable } from "./residual_seats/LargestAveragesForLessThan19SeatsTable";
+import { LargestRemaindersTable } from "./residual_seats/LargestRemaindersTable";
 
 export default {
   title: "App / Apportionment",
@@ -16,48 +18,66 @@ export default {
 
 export const DefaultApportionmentTable: Story = () => (
   <ApportionmentTable
-    finalStanding={seat_assignment.final_standing}
-    politicalGroups={election.political_groups as PoliticalGroup[]}
-    fullSeats={seat_assignment.full_seats}
-    residualSeats={seat_assignment.residual_seats}
-    seats={seat_assignment.seats}
+    finalStanding={equalOrMore.seat_assignment.final_standing}
+    politicalGroups={equalOrMore.election.political_groups as PoliticalGroup[]}
+    fullSeats={equalOrMore.seat_assignment.full_seats}
+    residualSeats={equalOrMore.seat_assignment.residual_seats}
+    seats={equalOrMore.seat_assignment.seats}
   />
 );
 DefaultApportionmentTable.storyName = "ApportionmentTable";
 
 export const DefaultElectionSummaryTable: Story = () => (
   <ElectionSummaryTable
-    votesCounts={election_summary.votes_counts}
-    seats={seat_assignment.seats}
-    quota={seat_assignment.quota}
-    numberOfVoters={election.number_of_voters}
+    votesCounts={equalOrMore.election_summary.votes_counts}
+    seats={equalOrMore.seat_assignment.seats}
+    quota={equalOrMore.seat_assignment.quota}
+    numberOfVoters={equalOrMore.election.number_of_voters}
   />
 );
 DefaultElectionSummaryTable.storyName = "ElectionSummaryTable";
 
 export const DefaultLargestRemaindersTable: Story = () => (
   <LargestRemaindersTable
-    largestRemainderSteps={largest_remainder_steps}
-    finalStanding={seat_assignment.final_standing}
-    politicalGroups={election.political_groups as PoliticalGroup[]}
+    largestRemainderSteps={lessThan.largest_remainder_steps}
+    finalStanding={lessThan.seat_assignment.final_standing}
+    politicalGroups={lessThan.election.political_groups as PoliticalGroup[]}
   />
 );
 DefaultLargestRemaindersTable.storyName = "LargestRemaindersTable";
 
 export const DefaultLargestAveragesForLessThan19SeatsTable: Story = () => (
   <LargestAveragesForLessThan19SeatsTable
-    largestAverageSteps={largest_average_steps}
-    finalStanding={seat_assignment.final_standing}
-    politicalGroups={election.political_groups as PoliticalGroup[]}
+    largestAverageSteps={lessThan.largest_average_steps}
+    finalStanding={lessThan.seat_assignment.final_standing}
+    politicalGroups={lessThan.election.political_groups as PoliticalGroup[]}
   />
 );
 DefaultLargestAveragesForLessThan19SeatsTable.storyName = "Largest averages for less than 19 seats table";
 
 export const DefaultLargestAveragesFor19OrMoreSeatsTable: Story = () => (
   <LargestAveragesFor19OrMoreSeatsTable
-    largestAverageSteps={seat_assignment.steps}
-    finalStanding={seat_assignment.final_standing}
-    politicalGroups={election.political_groups as PoliticalGroup[]}
+    largestAverageSteps={equalOrMore.seat_assignment.steps}
+    finalStanding={equalOrMore.seat_assignment.final_standing}
+    politicalGroups={equalOrMore.election.political_groups as PoliticalGroup[]}
   />
 );
 DefaultLargestAveragesFor19OrMoreSeatsTable.storyName = "Largest averages for 19 or more seats table";
+
+export const DefaultResidualSeatsCalculationTable: Story = () => (
+  <ResidualSeatsCalculationTable
+    seats={equalOrMore.seat_assignment.seats}
+    fullSeats={equalOrMore.seat_assignment.full_seats}
+    residualSeats={equalOrMore.seat_assignment.residual_seats}
+  />
+);
+DefaultResidualSeatsCalculationTable.storyName = "ResidualSeatsCalculationTable";
+
+export const DefaultFullSeatsTable: Story = () => (
+  <FullSeatsTable
+    finalStanding={equalOrMore.seat_assignment.final_standing}
+    politicalGroups={equalOrMore.election.political_groups ?? []}
+    quota={equalOrMore.seat_assignment.quota}
+  />
+);
+DefaultFullSeatsTable.storyName = "FullSeatsTable";

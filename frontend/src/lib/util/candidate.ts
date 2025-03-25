51dc9d93db91e15bf8179db941cc7777de6f0ad9
Lionqueen94 <lionqueen94@gmail.com>
2025-03-25 16:56:25 +0100

    Made gender rendering optional and not rendered in data entry form

--- frontend/src/lib/util/candidate.ts
+++ frontend/src/lib/util/candidate.ts
@@ -2,7 +2,7 @@ import { Candidate } from "@/api";
 
 import { t } from "@kiesraad/i18n";
 
-export function getCandidateFullName(candidate: Candidate): string {
+export function getCandidateFullName(candidate: Candidate, withGender: boolean): string {
   const gender = candidate.gender ? t(`candidate.${candidate.gender}`) : undefined;
   const last_name = candidate.last_name_prefix
     ? `${candidate.last_name_prefix} ${candidate.last_name}`
@@ -10,5 +10,5 @@ export function getCandidateFullName(candidate: Candidate): string {
   const full_name = candidate.first_name
     ? `${last_name}, ${candidate.initials} (${candidate.first_name})`
     : `${last_name}, ${candidate.initials}`;
-  return candidate.gender ? full_name + ` (${gender})` : full_name;
+  return withGender && candidate.gender ? full_name + ` (${gender})` : full_name;
 }

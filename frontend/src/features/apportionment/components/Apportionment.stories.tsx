97ce156a15db5e0e5028e43fd9658d4a4d6a217a
Oliver <oliver@sensibly.nl>
2025-03-26 09:08:11 +0100

    Rename imports

--- frontend/src/features/apportionment/components/Apportionment.stories.tsx
+++ frontend/src/features/apportionment/components/Apportionment.stories.tsx
@@ -2,8 +2,8 @@ import { Story } from "@ladle/react";
 
 import { PoliticalGroup } from "@/api";
 
-import * as equalOrMore from "../testing/19-or-more-seats";
-import * as lessThan from "../testing/less-than-19-seats";
+import * as gte19Seats from "../testing/19-or-more-seats";
+import * as lt19Seats from "../testing/less-than-19-seats";
 import { ApportionmentTable } from "./ApportionmentTable";
 import { ElectionSummaryTable } from "./ElectionSummaryTable";
 import { FullSeatsTable } from "./full_seats/FullSeatsTable";
@@ -18,66 +18,66 @@ export default {
 
 export const DefaultApportionmentTable: Story = () => (
   <ApportionmentTable
-    finalStanding={equalOrMore.seat_assignment.final_standing}
-    politicalGroups={equalOrMore.election.political_groups as PoliticalGroup[]}
-    fullSeats={equalOrMore.seat_assignment.full_seats}
-    residualSeats={equalOrMore.seat_assignment.residual_seats}
-    seats={equalOrMore.seat_assignment.seats}
+    finalStanding={gte19Seats.seat_assignment.final_standing}
+    politicalGroups={gte19Seats.election.political_groups as PoliticalGroup[]}
+    fullSeats={gte19Seats.seat_assignment.full_seats}
+    residualSeats={gte19Seats.seat_assignment.residual_seats}
+    seats={gte19Seats.seat_assignment.seats}
   />
 );
 DefaultApportionmentTable.storyName = "ApportionmentTable";
 
 export const DefaultElectionSummaryTable: Story = () => (
   <ElectionSummaryTable
-    votesCounts={equalOrMore.election_summary.votes_counts}
-    seats={equalOrMore.seat_assignment.seats}
-    quota={equalOrMore.seat_assignment.quota}
-    numberOfVoters={equalOrMore.election.number_of_voters}
+    votesCounts={gte19Seats.election_summary.votes_counts}
+    seats={gte19Seats.seat_assignment.seats}
+    quota={gte19Seats.seat_assignment.quota}
+    numberOfVoters={gte19Seats.election.number_of_voters}
   />
 );
 DefaultElectionSummaryTable.storyName = "ElectionSummaryTable";
 
 export const DefaultLargestRemaindersTable: Story = () => (
   <LargestRemaindersTable
-    largestRemainderSteps={lessThan.largest_remainder_steps}
-    finalStanding={lessThan.seat_assignment.final_standing}
-    politicalGroups={lessThan.election.political_groups as PoliticalGroup[]}
+    largestRemainderSteps={lt19Seats.largest_remainder_steps}
+    finalStanding={lt19Seats.seat_assignment.final_standing}
+    politicalGroups={lt19Seats.election.political_groups as PoliticalGroup[]}
   />
 );
 DefaultLargestRemaindersTable.storyName = "LargestRemaindersTable";
 
 export const DefaultLargestAveragesForLessThan19SeatsTable: Story = () => (
   <LargestAveragesForLessThan19SeatsTable
-    largestAverageSteps={lessThan.largest_average_steps}
-    finalStanding={lessThan.seat_assignment.final_standing}
-    politicalGroups={lessThan.election.political_groups as PoliticalGroup[]}
+    largestAverageSteps={lt19Seats.largest_average_steps}
+    finalStanding={lt19Seats.seat_assignment.final_standing}
+    politicalGroups={lt19Seats.election.political_groups as PoliticalGroup[]}
   />
 );
 DefaultLargestAveragesForLessThan19SeatsTable.storyName = "Largest averages for less than 19 seats table";
 
 export const DefaultLargestAveragesFor19OrMoreSeatsTable: Story = () => (
   <LargestAveragesFor19OrMoreSeatsTable
-    largestAverageSteps={equalOrMore.seat_assignment.steps}
-    finalStanding={equalOrMore.seat_assignment.final_standing}
-    politicalGroups={equalOrMore.election.political_groups as PoliticalGroup[]}
+    largestAverageSteps={gte19Seats.seat_assignment.steps}
+    finalStanding={gte19Seats.seat_assignment.final_standing}
+    politicalGroups={gte19Seats.election.political_groups as PoliticalGroup[]}
   />
 );
 DefaultLargestAveragesFor19OrMoreSeatsTable.storyName = "Largest averages for 19 or more seats table";
 
 export const DefaultResidualSeatsCalculationTable: Story = () => (
   <ResidualSeatsCalculationTable
-    seats={equalOrMore.seat_assignment.seats}
-    fullSeats={equalOrMore.seat_assignment.full_seats}
-    residualSeats={equalOrMore.seat_assignment.residual_seats}
+    seats={gte19Seats.seat_assignment.seats}
+    fullSeats={gte19Seats.seat_assignment.full_seats}
+    residualSeats={gte19Seats.seat_assignment.residual_seats}
   />
 );
 DefaultResidualSeatsCalculationTable.storyName = "ResidualSeatsCalculationTable";
 
 export const DefaultFullSeatsTable: Story = () => (
   <FullSeatsTable
-    finalStanding={equalOrMore.seat_assignment.final_standing}
-    politicalGroups={equalOrMore.election.political_groups ?? []}
-    quota={equalOrMore.seat_assignment.quota}
+    finalStanding={gte19Seats.seat_assignment.final_standing}
+    politicalGroups={gte19Seats.election.political_groups ?? []}
+    quota={gte19Seats.seat_assignment.quota}
   />
 );
 DefaultFullSeatsTable.storyName = "FullSeatsTable";

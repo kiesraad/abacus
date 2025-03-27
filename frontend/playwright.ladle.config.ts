38bc009730321fd840f1ae06247b45b87a6620f8
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-03-27 15:04:04 +0100

    increases local timeout of e2e tests to 20 seconds
    The timeout of 10 seconds was not sufficient when running all
    e2e tests against a single browser engine. Increasing the timeout
    to 15 seconds still resulted in an occassional timeout. So
    increased timeout to 20 seconds.

--- frontend/playwright.ladle.config.ts
+++ frontend/playwright.ladle.config.ts
@@ -4,6 +4,8 @@ import commonConfig from "./playwright.common.config";
 
 const config: PlaywrightTestConfig = defineConfig({
   ...commonConfig,
+  // Increase the test timeout on CI, which is usually slower
+  timeout: process.env.CI ? 30_000 : 10_000,
   // Use the list reporter even on CI, to get immediate feedback
   reporter: process.env.CI ? [["list"], ["github"], ["junit", { outputFile: "playwright.ladle.junit.xml" }]] : "list",
   testDir: "./src/components/ui",

316d1b968c8da2e2880849255e8b985d4d44bc48
Lionqueen94 <lionqueen94@gmail.com>
2025-04-01 13:29:21 +0300

    Updated api endpoint descriptions

--- backend/src/apportionment/api.rs
+++ backend/src/apportionment/api.rs
@@ -41,9 +41,8 @@ pub struct ElectionApportionmentResponse {
         (status = 200, description = "Election Apportionment", body = ElectionApportionmentResponse),
         (status = 401, description = "Unauthorized", body = ErrorResponse),
         (status = 404, description = "Not found", body = ErrorResponse),
-        (status = 412, description = "Election data entry first needs to be finalised", body = ErrorResponse),
-        (status = 422, description = "All lists are exhausted, not enough candidates to fill all seats", body = ErrorResponse),
-        (status = 422, description = "Drawing of lots is required", body = ErrorResponse),
+        (status = 412, description = "Election apportionment is not yet available", body = ErrorResponse),
+        (status = 422, description = "Election apportionment is not possible", body = ErrorResponse),
         (status = 500, description = "Internal server error", body = ErrorResponse),
   ),
   params(

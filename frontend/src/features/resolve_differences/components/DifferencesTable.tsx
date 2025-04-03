4b844f744cbcf2c68540e03141e2f55f6a041ef5
Oliver <oliver@sensibly.nl>
2025-04-03 16:11:05 +0200

    DifferencesTable to show rows that are different, ResolveDifferencesTables to show data entry differences within sections and political groups

---
+++ frontend/src/features/resolve_differences/components/DifferencesTable.tsx
@@ -0,0 +1,79 @@
+import { Fragment, useId } from "react";
+
+import cls from "@/features/resolve_differences/components/ResolveDifferences.module.css";
+
+import { Table } from "@kiesraad/ui";
+
+interface DifferencesTableProps {
+  title: string;
+  headers: string[];
+  rows: DifferencesRow[];
+}
+
+const zeroDash = <span className={cls.zeroDash}>&mdash;</span>;
+
+export interface DifferencesRow {
+  code?: number | string;
+  first?: number | string;
+  second?: number | string;
+  description?: string;
+}
+
+export function DifferencesTable({ title, headers, rows }: DifferencesTableProps) {
+  const id = useId();
+  // An array of indices for rows that are different, also used to detect row gaps.
+  // Two falsy values are considered equal (e.g. 0 and undefined)
+  const differences = rows.reduce<number[]>(
+    (show, row, index) => (row.first === row.second || (!row.first && !row.second) ? show : [...show, index]),
+    [],
+  );
+
+  if (differences.length === 0) {
+    return null;
+  }
+
+  return (
+    <section className="mt-lg mb-xl">
+      <h2 id={id}>{title}</h2>
+      <div>
+        <Table className={cls.differencesTable} aria-labelledby={id}>
+          <Table.Header>
+            <Table.HeaderCell className="w-6 text-align-r">{headers[0]}</Table.HeaderCell>
+            <Table.HeaderCell className="w-13 text-align-r">{headers[1]}</Table.HeaderCell>
+            <Table.HeaderCell className="w-13 text-align-r">{headers[2]}</Table.HeaderCell>
+            <Table.HeaderCell>{headers[3]}</Table.HeaderCell>
+          </Table.Header>
+          <Table.Body>
+            {differences.map((rowIndex, differenceIndex) => {
+              // There is a gap if the previous row is not index - 1
+              const gapRow = differenceIndex > 0 && differences[differenceIndex - 1] !== rowIndex - 1;
+
+              return (
+                <Fragment key={differenceIndex}>
+                  {gapRow && (
+                    <Table.Row>
+                      <Table.Cell className={cls.gapRow} colSpan={4}></Table.Cell>
+                    </Table.Row>
+                  )}
+
+                  <Table.Row>
+                    <Table.HeaderCell scope="row" className="text-align-r normal">
+                      {rows[rowIndex]?.code}
+                    </Table.HeaderCell>
+                    <Table.Cell className="text-align-r font-number bold">
+                      {rows[rowIndex]?.first || zeroDash}
+                    </Table.Cell>
+                    <Table.Cell className="text-align-r font-number bold">
+                      {rows[rowIndex]?.second || zeroDash}
+                    </Table.Cell>
+                    <Table.Cell>{rows[rowIndex]?.description}</Table.Cell>
+                  </Table.Row>
+                </Fragment>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      </div>
+    </section>
+  );
+}
---
+++ frontend/src/features/resolve_differences/utils/dataEntry.ts
@@ -0,0 +1,67 @@
+import { PollingStationResults } from "@/api";
+import { FormSectionId } from "@/components/form/data_entry/state/types";
+
+type ObjectPath<T> = {
+  // For each entry, if it is an object (including optional)
+  [K in keyof T]: NonNullable<T[K]> extends object
+    ? T[K] extends unknown[]
+      ? never // Skip arrays, which are also objects
+      : `${K & string}.${Extract<keyof NonNullable<T[K]>, string>}` // "parent.child" template literal for string keys
+    : K & string; // else, the key itself when it is a string
+}[keyof T]; // Get all values together
+
+type SectionId = Extract<FormSectionId, "recounted" | "voters_votes_counts" | "differences_counts">;
+
+export type PollingStationResultsPath = NonNullable<ObjectPath<PollingStationResults>>;
+
+export interface DataEntrySection {
+  id: SectionId;
+  fields: { code?: string; path: PollingStationResultsPath }[];
+}
+
+export const sections: DataEntrySection[] = [
+  {
+    id: "recounted",
+    fields: [{ path: "recounted" }],
+  },
+  {
+    id: "voters_votes_counts",
+    fields: [
+      { code: "A", path: "voters_counts.poll_card_count" },
+      { code: "B", path: "voters_counts.proxy_certificate_count" },
+      { code: "C", path: "voters_counts.voter_card_count" },
+      { code: "D", path: "voters_counts.total_admitted_voters_count" },
+      { code: "E", path: "votes_counts.votes_candidates_count" },
+      { code: "F", path: "votes_counts.blank_votes_count" },
+      { code: "G", path: "votes_counts.invalid_votes_count" },
+      { code: "H", path: "votes_counts.total_votes_cast_count" },
+      { code: "A.2", path: "voters_recounts.poll_card_count" },
+      { code: "B.2", path: "voters_recounts.proxy_certificate_count" },
+      { code: "C.2", path: "voters_recounts.voter_card_count" },
+      { code: "D.2", path: "voters_recounts.total_admitted_voters_count" },
+    ],
+  },
+  {
+    id: "differences_counts",
+    fields: [
+      { code: "I", path: "differences_counts.more_ballots_count" },
+      { code: "J", path: "differences_counts.fewer_ballots_count" },
+      { code: "K", path: "differences_counts.unreturned_ballots_count" },
+      { code: "L", path: "differences_counts.too_few_ballots_handed_out_count" },
+      { code: "M", path: "differences_counts.too_many_ballots_handed_out_count" },
+      { code: "N", path: "differences_counts.other_explanation_count" },
+      { code: "O", path: "differences_counts.no_explanation_count" },
+    ],
+  },
+];
+
+export function getFromResults(results: PollingStationResults, path: PollingStationResultsPath) {
+  const segments = path.split(".");
+  return segments.reduce((o: unknown, k: string) => {
+    if (o && typeof o === "object" && k in o) {
+      return o[k as keyof typeof o] as string | number | boolean | undefined;
+    } else {
+      return undefined;
+    }
+  }, results) as string | number | boolean | undefined;
+}

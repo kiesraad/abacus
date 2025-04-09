0dddf541e86dbac25cb35311beede82a6abc4146
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-04-09 17:37:18 +0200

    adds test coverage for two remaining paths of ErrorModal

--- frontend/src/components/error/ErrorModal.stories.tsx
+++ frontend/src/components/error/ErrorModal.stories.tsx
@@ -2,7 +2,7 @@ import { Story } from "@ladle/react";
 
 import { ErrorModal } from "@/components/error/ErrorModal";
 
-import { ApiError, ApiResponseStatus } from "@kiesraad/api";
+import { ApiError, ApiResponseStatus, NetworkError } from "@kiesraad/api";
 
 export const ServerErrorModal: Story = () => {
   return (
@@ -10,4 +10,19 @@ export const ServerErrorModal: Story = () => {
       error={new ApiError(ApiResponseStatus.ServerError, 500, "Internal Server Error", "InternalServerError")}
     />
   );
+};
+
+export const UnknownServerErrorModal: Story = () => {
+  return (
+    <ErrorModal
+      // we need to disable the linter to test the method with an unknown translation key
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      error={new ApiError(ApiResponseStatus.ServerError, 543, "Unknown Server Error", "UnknownServerError")}
+    />
+  );
+};
+
+export const NetworkErrorModal: Story = () => {
+  return <ErrorModal error={new NetworkError("Network errors are the worst")} />;
 };
--- frontend/src/components/error/ErrorModal.test.tsx
+++ frontend/src/components/error/ErrorModal.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, test } from "vitest";
 
 import { render, screen, within } from "@/testing";
 
-import { ServerErrorModal } from "./ErrorModal.stories";
+import { NetworkErrorModal, ServerErrorModal, UnknownServerErrorModal } from "./ErrorModal.stories";
 
 describe("Component: ErrorModal", () => {
   test("Server error has expected children", () => {
@@ -13,6 +13,27 @@ describe("Component: ErrorModal", () => {
     const modalBody = screen.getByTestId("error-modal");
     expect(modalBody).toHaveTextContent("Er is een interne fout opgetreden");
     expect(modalBody).not.toHaveTextContent("Foutcode: 500");
+    expect(within(modalBody).getByRole("button")).toHaveTextContent("Melding sluiten");
+  });
+
+  test("Unknown server error has expected children", () => {
+    render(<UnknownServerErrorModal />);
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent("Sorry, er ging iets mis");
+
+    const modalBody = screen.getByTestId("error-modal");
+    expect(modalBody).toHaveTextContent("Foutcode: 543");
+    expect(modalBody).toHaveTextContent("Unknown Server Error");
+    expect(within(modalBody).getByRole("button")).toHaveTextContent("Melding sluiten");
+  });
+
+  test("Network error has expected children", () => {
+    render(<NetworkErrorModal />);
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent("Sorry, er ging iets mis");
+
+    const modalBody = screen.getByTestId("error-modal");
+    expect(modalBody).toHaveTextContent("Network errors are the worst");
     expect(within(modalBody).getByRole("button")).toHaveTextContent("Melding sluiten");
   });
 });

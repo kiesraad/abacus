fa307eb3e68310d16fd405783bcd54725988d036
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-04-10 13:58:00 +0200

    removes unneeded explicit type

--- frontend/src/features/data_entry/utils/dataEntryUtils.test.ts
+++ frontend/src/features/data_entry/utils/dataEntryUtils.test.ts
@@ -1,7 +1,5 @@
 import { describe, expect, test } from "vitest";
 
-import { PollingStationResults } from "@/api";
-
 import {
   errorWarningMocks,
   getDefaultDataEntryState,
@@ -73,14 +71,14 @@ describe("getNextSectionID", () => {
 describe("isFormSectionEmpty", () => {
   test("political group form is empty", () => {
     const section = getDefaultFormSection("political_group_votes_1", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
 
     expect(isFormSectionEmpty(section, values)).toBeTruthy();
   });
 
   test("political group form: total is not empty", () => {
     const section = getDefaultFormSection("political_group_votes_1", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
     values.political_group_votes[0]!.total = 100;
 
     expect(isFormSectionEmpty(section, values)).toBeFalsy();
@@ -88,7 +86,7 @@ describe("isFormSectionEmpty", () => {
 
   test("political group formL: candidate votes is not empty", () => {
     const section = getDefaultFormSection("political_group_votes_1", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
     values.political_group_votes[0]!.candidate_votes[0]!.votes = 100;
 
     expect(isFormSectionEmpty(section, values)).toBeFalsy();
@@ -96,14 +94,14 @@ describe("isFormSectionEmpty", () => {
 
   test("voters and votes form is empty", () => {
     const section = getDefaultFormSection("political_group_votes_1", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
 
     expect(isFormSectionEmpty(section, values)).toBeTruthy();
   });
 
   test("voters and votes form: votes is not empty", () => {
     const section = getDefaultFormSection("voters_votes_counts", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
     values.votes_counts.invalid_votes_count = 3;
 
     expect(isFormSectionEmpty(section, values)).toBeFalsy();
@@ -111,7 +109,7 @@ describe("isFormSectionEmpty", () => {
 
   test("voters and votes form: voters is not empty", () => {
     const section = getDefaultFormSection("voters_votes_counts", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
     values.voters_counts.total_admitted_voters_count = 6;
 
     expect(isFormSectionEmpty(section, values)).toBeFalsy();
@@ -119,14 +117,14 @@ describe("isFormSectionEmpty", () => {
 
   test("differences form is empty", () => {
     const section = getDefaultFormSection("differences_counts", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
 
     expect(isFormSectionEmpty(section, values)).toBeTruthy();
   });
 
   test("differences form is not empty", () => {
     const section = getDefaultFormSection("differences_counts", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
     values.differences_counts.more_ballots_count = 5;
 
     expect(isFormSectionEmpty(section, values)).toBeFalsy();
@@ -134,14 +132,14 @@ describe("isFormSectionEmpty", () => {
 
   test("recounted form is empty", () => {
     const section = getDefaultFormSection("recounted", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
 
     expect(isFormSectionEmpty(section, values)).toBeTruthy();
   });
 
   test("recounted form is not empty", () => {
     const section = getDefaultFormSection("recounted", 0);
-    const values: PollingStationResults = getInitialValues();
+    const values = getInitialValues();
     values.recounted = false;
 
     expect(isFormSectionEmpty(section, values)).toBeFalsy();

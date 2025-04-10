d884003289090f7b3d7b4d2e329d6a21a013161f
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-04-10 08:53:42 +0200

    adds closing of modal to ErrorModal tests

--- frontend/src/components/error/ErrorModal.test.tsx
+++ frontend/src/components/error/ErrorModal.test.tsx
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { describe, expect, test } from "vitest";
 
 import { render, screen, within } from "@/testing";
@@ -5,35 +6,47 @@ import { render, screen, within } from "@/testing";
 import { NetworkErrorModal, ServerErrorModal, UnknownServerErrorModal } from "./ErrorModal.stories";
 
 describe("Component: ErrorModal", () => {
-  test("Server error has expected children", () => {
+  test("Server error modal is shown and can be closed", async () => {
     render(<ServerErrorModal />);
 
-    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent("Sorry, er ging iets mis");
+    expect(screen.getByRole("heading", { level: 2, name: "Sorry, er ging iets mis" })).toBeVisible();
 
     const modalBody = screen.getByTestId("error-modal");
     expect(modalBody).toHaveTextContent("Er is een interne fout opgetreden");
     expect(modalBody).not.toHaveTextContent("Foutcode: 500");
     expect(within(modalBody).getByRole("button")).toHaveTextContent("Melding sluiten");
+
+    const user = userEvent.setup();
+    await user.click(within(modalBody).getByRole("button", { name: "Melding sluiten" }));
+    expect(screen.queryByRole("heading", { level: 2, name: "Sorry, er ging iets mis" })).not.toBeInTheDocument();
   });
 
-  test("Unknown server error has expected children", () => {
+  test("Unknown server modal is shown and can be closed", async () => {
     render(<UnknownServerErrorModal />);
 
-    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent("Sorry, er ging iets mis");
+    expect(screen.getByRole("heading", { level: 2, name: "Sorry, er ging iets mis" })).toBeVisible();
 
     const modalBody = screen.getByTestId("error-modal");
     expect(modalBody).toHaveTextContent("Foutcode: 543");
     expect(modalBody).toHaveTextContent("Unknown Server Error");
     expect(within(modalBody).getByRole("button")).toHaveTextContent("Melding sluiten");
+
+    const user = userEvent.setup();
+    await user.click(within(modalBody).getByRole("button", { name: "Melding sluiten" }));
+    expect(screen.queryByRole("heading", { level: 2, name: "Sorry, er ging iets mis" })).not.toBeInTheDocument();
   });
 
-  test("Network error has expected children", () => {
+  test("Network error modal is shown and can be closed", async () => {
     render(<NetworkErrorModal />);
 
-    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent("Sorry, er ging iets mis");
+    expect(screen.getByRole("heading", { level: 2, name: "Sorry, er ging iets mis" })).toBeVisible();
 
     const modalBody = screen.getByTestId("error-modal");
     expect(modalBody).toHaveTextContent("Network errors are the worst");
     expect(within(modalBody).getByRole("button")).toHaveTextContent("Melding sluiten");
+
+    const user = userEvent.setup();
+    await user.click(within(modalBody).getByRole("button", { name: "Melding sluiten" }));
+    expect(screen.queryByRole("heading", { level: 2, name: "Sorry, er ging iets mis" })).not.toBeInTheDocument();
   });
 });

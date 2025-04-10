0e81c8e188881199bdcae1f85ffee7d7dce4c768
Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>
2025-04-10 10:12:14 +0200

    restore code coverage of ErrorModal.tsx (#1344)
    Co-authored-by: Mark Janssen <20283+praseodym@users.noreply.github.com>

--- frontend/src/components/error/ErrorModal.stories.tsx
+++ frontend/src/components/error/ErrorModal.stories.tsx
@@ -2,18 +2,27 @@ import { Story } from "@ladle/react";
 
 import { ErrorModal } from "@/components/error/ErrorModal";
 
-import { ApiError, ApiResponseStatus } from "@kiesraad/api";
+import { ApiError, ApiResponseStatus, NetworkError } from "@kiesraad/api";
 
 export const ServerErrorModal: Story = () => {
   return (
     <ErrorModal
-      error={
-        {
-          status: ApiResponseStatus.ServerError,
-          code: 500,
-          message: "Internal Server Error",
-        } as ApiError
-      }
+      error={new ApiError(ApiResponseStatus.ServerError, 500, "Internal Server Error", "InternalServerError")}
     />
   );
+};
+
+export const UnknownServerErrorModal: Story = () => {
+  return (
+    <ErrorModal
+      // we need to disable the linter to test the method with an unknown translation key
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      error={new ApiError(ApiResponseStatus.ServerError, 543, "Unknown Server Error", "UnknownServerError")}
+    />
+  );
+};
+
+export const NetworkErrorModal: Story = () => {
+  return <ErrorModal error={new NetworkError("Network errors are the worst")} />;
 };

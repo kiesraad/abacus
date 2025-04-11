1b7ab17da52b15554424b612fe2a7943ebb4a7b6
Marlon Peeters <marlonpeeters@tweedegolf.com>
2025-04-11 09:14:47 +0200

    Added inline Alert component (#1350)

--- frontend/src/components/ui/Alert/Alert.tsx
+++ frontend/src/components/ui/Alert/Alert.tsx
@@ -9,22 +9,35 @@ import { AlertType, IconButton, renderIconForType } from "@kiesraad/ui";
 import cls from "./Alert.module.css";
 
 export interface AlertProps {
+  title?: string;
   type: AlertType;
   variant?: "default" | "small" | "no-icon";
+  inline?: boolean;
   margin?: "mb-md" | "mb-md-lg" | "mb-lg";
   children: React.ReactNode;
   onClose?: () => void;
 }
 
-export function Alert({ type, onClose, children, margin, variant = "default" }: AlertProps) {
+export function Alert({ type, onClose, children, margin, title, inline, variant = "default" }: AlertProps) {
   const id = React.useId();
-
   return (
-    <div className={cn(cls.alert, cls[type], margin, variant)} role="alert" aria-describedby={id}>
+    <div
+      className={cn(inline ? cls.inlineAlert : cls.alert, cls[type], margin, variant)}
+      role="alert"
+      aria-describedby={id}
+    >
       {onClose && (
         <IconButton icon={<IconCross />} title={t("close_message")} variant="tertiary" size="lg" onClick={onClose} />
       )}
-      {variant !== "no-icon" && <aside>{renderIconForType(type)}</aside>}
+
+      {inline ? (
+        <header>
+          {variant !== "no-icon" && <aside>{renderIconForType(type)}</aside>}
+          {title && <h2>{title}</h2>}
+        </header>
+      ) : (
+        variant !== "no-icon" && <aside>{renderIconForType(type)}</aside>
+      )}
       <section id={id}>{children}</section>
     </div>
   );

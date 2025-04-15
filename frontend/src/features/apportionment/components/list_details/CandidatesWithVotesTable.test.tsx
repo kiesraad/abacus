5cca3ae4079db8975e1dbcc0802cf3184c279cdf
Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>
2025-04-15 14:47:59 +0200

    fix two instances of identical test names (#1379)

--- frontend/src/features/apportionment/components/list_details/CandidatesWithVotesTable.test.tsx
+++ frontend/src/features/apportionment/components/list_details/CandidatesWithVotesTable.test.tsx
@@ -10,7 +10,7 @@ import {
 import { CandidatesWithVotesTable } from "./CandidatesWithVotesTable";
 
 describe("CandidatesWithVotesTable", () => {
-  test("renders a table with the candidates, localities and number of votes", async () => {
+  test("renders a table with the candidates and localities", async () => {
     render(
       <CandidatesWithVotesTable
         id="test-table"
@@ -37,7 +37,7 @@ describe("CandidatesWithVotesTable", () => {
     ]);
   });
 
-  test("renders a table with the candidates, localities and number of votes", async () => {
+  test("renders a table with the candidates and number of votes", async () => {
     render(
       <CandidatesWithVotesTable
         id="test-table"

2cb6ca5880c857473acd374ac1c7958aa83b5931
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-04-17 10:50:20 +0200

    adds second WIP test for DataEntryProgress

--- frontend/src/features/data_entry/components/DataEntryProgress.test.tsx
+++ frontend/src/features/data_entry/components/DataEntryProgress.test.tsx
@@ -6,8 +6,11 @@ import { ElectionProvider } from "@/api/election/ElectionProvider";
 import { electionMockData } from "@/testing/api-mocks/ElectionMockData";
 import { ElectionRequestHandler, PollingStationDataEntryClaimHandler } from "@/testing/api-mocks/RequestHandlers";
 import { server } from "@/testing/server";
-import { render, screen, waitFor } from "@/testing/test-utils";
+import { render, screen, waitFor, within } from "@/testing/test-utils";
 
+import { getDefaultFormSection } from "../testing/mock-data";
+import { overrideServerClaimDataEntryResponse } from "../testing/test.utils";
+import { DataEntryState } from "../types/types";
 import { DataEntryProgress } from "./DataEntryProgress";
 import { DataEntryProvider } from "./DataEntryProvider";
 
@@ -15,7 +18,14 @@ import { DataEntryProvider } from "./DataEntryProvider";
 //      It's for forms that have not been visited yet.
 //      Reason: the active attribute can also set an "idle" class
 
-// How to change the values of useDataEntryContext()? Through the PollingStationDataEntryClaimHandler?
+// What needs testing?
+// status class and icon -> result of menuStatusForFormSection()
+// link if not current form and before current form
+
+// ProgressList: does not have tests
+// status -> class and icon
+// disabled -> class
+// active-> aria-current (step or false) and icon and class (active or idle)
 
 vi.mock("react-router");
 
@@ -28,6 +38,27 @@ function renderForm() {
     </ElectionProvider>,
   );
 }
+
+const defaultDataEntryState: DataEntryState = {
+  election: electionMockData,
+  pollingStationId: 1,
+  error: null,
+  pollingStationResults: null,
+  entryNumber: 1,
+  formState: {
+    current: "differences_counts",
+    furthest: "differences_counts",
+    sections: {
+      recounted: getDefaultFormSection("recounted", 1),
+      voters_votes_counts: getDefaultFormSection("voters_votes_counts", 2),
+      differences_counts: getDefaultFormSection("differences_counts", 3),
+      save: getDefaultFormSection("save", 4),
+    },
+  },
+  targetFormSectionId: "recounted",
+  status: "idle",
+  cache: null,
+};
 
 describe("Test DataEntryProgress", () => {
   beforeEach(() => {
@@ -57,6 +88,47 @@ describe("Test DataEntryProgress", () => {
 
     expect(differences).toHaveClass("idle idle disabled");
     expect(differences).toHaveAttribute("aria-current", "false");
+
+    expect(list1).toHaveClass("idle idle disabled");
+    expect(list1).toHaveAttribute("aria-current", "false");
+
+    expect(list2).toHaveClass("idle idle disabled");
+    expect(list2).toHaveAttribute("aria-current", "false");
+
+    expect(checkAndSave).toHaveClass("idle idle disabled");
+    expect(checkAndSave).toHaveAttribute("aria-current", "false");
+  });
+
+  test("second test", async () => {
+    overrideServerClaimDataEntryResponse({
+      formState: defaultDataEntryState.formState,
+      pollingStationResults: {
+        recounted: true,
+      },
+    });
+    renderForm();
+
+    await waitFor(() => {
+      expect(screen.getByText("Is er herteld?")).toBeVisible();
+    });
+
+    const recounted = screen.getByTestId("list-item-recounted");
+    const votersAndVotes = screen.getByTestId("list-item-voters-and-votes");
+    const differences = screen.getByTestId("list-item-differences");
+    const list1 = screen.getByTestId("list-item-pg-1");
+    const list2 = screen.getByTestId("list-item-pg-2");
+    const checkAndSave = screen.getByTestId("list-item-save");
+
+    expect(recounted).toHaveClass("accept idle");
+    expect(recounted).toHaveAttribute("aria-current", "false");
+    const icon = within(recounted).getByRole("img");
+    expect(icon).toHaveAccessibleName("opgeslagen");
+
+    expect(votersAndVotes).toHaveClass("idle empty");
+    expect(votersAndVotes).toHaveAttribute("aria-current", "false");
+
+    expect(differences).toHaveClass("active unsaved");
+    expect(differences).toHaveAttribute("aria-current", "step");
 
     expect(list1).toHaveClass("idle idle disabled");
     expect(list1).toHaveAttribute("aria-current", "false");

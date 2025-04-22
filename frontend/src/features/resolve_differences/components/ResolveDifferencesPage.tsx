452d83396abc37effa4b8b90ae646ed93b0edb8b
Marlon Baeten <marlon@tweedegolf.com>
2025-04-22 16:48:25 +0200

    WIP resolve differences page

--- frontend/src/features/resolve_differences/components/ResolveDifferencesPage.tsx
+++ frontend/src/features/resolve_differences/components/ResolveDifferencesPage.tsx
@@ -1,23 +1,75 @@
+import { Badge } from "@/components/ui/Badge/Badge";
+import { PollingStationNumber } from "@/components/ui/Badge/PollingStationNumber";
+import { ChoiceList } from "@/components/ui/CheckboxAndRadio/ChoiceList";
 import { Loader } from "@/components/ui/Loader/Loader";
 import { useNumericParam } from "@/hooks/useNumericParam";
+import { t } from "@/lib/i18n";
 
 import { usePollingStationDataEntryDifferences } from "../hooks/usePollingStationDataEntryDifferences";
+import cls from "./ResolveDifferences.module.css";
 import { ResolveDifferencesTables } from "./ResolveDifferencesTables";
 
-// TODO: Implement the actual page layout
 export function ResolveDifferencesPage() {
   const pollingStationId = useNumericParam("pollingStationId");
-  const { election, loading, status } = usePollingStationDataEntryDifferences(pollingStationId);
+  const { pollingStation, election, loading, status, choice, setChoice } =
+    usePollingStationDataEntryDifferences(pollingStationId);
 
   if (loading || status === null) {
     return <Loader />;
   }
 
   return (
-    <ResolveDifferencesTables
-      first={status.state.first_entry}
-      second={status.state.second_entry}
-      politicalGroups={election.political_groups}
-    />
+    <>
+      <header>
+        <section className="smaller-gap">
+          <PollingStationNumber>{pollingStation.number}</PollingStationNumber>
+          <h1>{pollingStation.name}</h1>
+          <Badge type="entries_different" />
+        </section>
+      </header>
+      <main className={cls.resolveDifferences}>
+        <aside>Wololo</aside>
+        <article>
+          <h2 className="">{t("resolve_differences.page_title")}</h2>
+          <p>{t("resolve_differences.page_content")}</p>
+          <ResolveDifferencesTables
+            first={status.state.first_entry}
+            second={status.state.second_entry}
+            politicalGroups={election.political_groups}
+            previewChoice={choice}
+          />
+          <form action="">
+            <h2>{t("resolve_differences.form_question")}</h2>
+            <p>{t("resolve_differences.form_content")}</p>
+            <ChoiceList>
+              <ChoiceList.Radio
+                id="keep_first_entry"
+                label={t("resolve_differences.options.keep_first_entry", { name: status.first_user })}
+                checked={choice === "keep_first_entry"}
+                onChange={() => {
+                  setChoice("keep_first_entry");
+                }}
+              />
+              <ChoiceList.Radio
+                id="keep_second_entry"
+                label={t("resolve_differences.options.keep_second_entry", { name: status.second_user })}
+                checked={choice === "keep_second_entry"}
+                onChange={() => {
+                  setChoice("keep_second_entry");
+                }}
+              />
+              <ChoiceList.Radio
+                id="discard_both_entries"
+                label={t("resolve_differences.options.discard_both_entries")}
+                checked={choice === "discard_both_entries"}
+                onChange={() => {
+                  setChoice("discard_both_entries");
+                }}
+              />
+            </ChoiceList>
+          </form>
+        </article>
+      </main>
+    </>
   );
 }
--- frontend/src/features/resolve_differences/hooks/usePollingStationDataEntryDifferences.ts
+++ frontend/src/features/resolve_differences/hooks/usePollingStationDataEntryDifferences.ts
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 import { NotFoundError } from "@/api/ApiResult";
-import { useInitialApiGet } from "@/api/useInitialApiGet";
+import { useInitialApiGet, useInitialApiGetWithErrors } from "@/api/useInitialApiGet";
 import { useElection } from "@/hooks/election/useElection";
 import {
   DataEntryStatus,
@@ -7,17 +9,33 @@ import {
   EntriesDifferent,
   PoliticalGroup,
   POLLING_STATION_DATA_ENTRY_STATUS_REQUEST_PATH,
+  PollingStation,
+  ResolveAction,
+  USER_LIST_REQUEST_PATH,
+  UserListResponse,
 } from "@/types/generated/openapi";
 
+type EntriesDifferentStatus = {
+  state: EntriesDifferent;
+  status: "EntriesDifferent";
+  first_user: string;
+  second_user: string;
+};
+
 interface PollingStationDataEntryStatus {
+  choice: ResolveAction | null;
+  setChoice: (choice: ResolveAction | null) => void;
+  pollingStation: PollingStation;
   election: Election & { political_groups: PoliticalGroup[] };
   loading: boolean;
-  status: { state: EntriesDifferent; status: "EntriesDifferent" } | null;
+  status: EntriesDifferentStatus | null;
 }
 
 export function usePollingStationDataEntryDifferences(pollingStationId: number): PollingStationDataEntryStatus {
   const { election, pollingStations } = useElection();
+
   const pollingStation = pollingStations.find((ps) => ps.id === pollingStationId);
+  const [choice, setChoice] = useState<ResolveAction | null>(null);
 
   if (!pollingStation) {
     throw new NotFoundError("error.polling_station_not_found");
@@ -31,15 +49,40 @@ export function usePollingStationDataEntryDifferences(pollingStationId: number):
     throw requestState.error;
   }
 
+  const usersPath: USER_LIST_REQUEST_PATH = "/api/user";
+  const { requestState: usersRequestState } = useInitialApiGetWithErrors<UserListResponse>(usersPath);
+
+  if (usersRequestState.status === "api-error") {
+    throw usersRequestState.error;
+  }
+
   // only allow polling stations with status "EntriesDifferent" to be resolved
   if (requestState.status === "success" && requestState.data.status !== "EntriesDifferent") {
     throw new NotFoundError("error.polling_station_not_found");
   }
 
+  let status: EntriesDifferentStatus | null = null;
+
+  if (
+    requestState.status === "success" &&
+    usersRequestState.status === "success" &&
+    requestState.data.status === "EntriesDifferent"
+  ) {
+    const users = usersRequestState.data.users;
+    const state = requestState.data.state;
+    status = {
+      ...requestState.data,
+      first_user: users.find((u) => u.id === state.first_entry_user_id)?.fullname || "",
+      second_user: users.find((u) => u.id === state.second_entry_user_id)?.fullname || "",
+    };
+  }
+
   return {
+    choice,
+    setChoice,
+    pollingStation,
     election,
-    loading: requestState.status === "loading",
-    status:
-      requestState.status === "success" && requestState.data.status === "EntriesDifferent" ? requestState.data : null,
+    loading: requestState.status === "loading" || usersRequestState.status === "loading",
+    status,
   };
 }

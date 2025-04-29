e709716c3114f4aaa3b002efb9237a836b4a34aa
Les <les.kleuver@gmail.com>
2025-04-29 11:32:22 +0200

    add tests for component

---
+++ frontend/src/features/data_entry/components/AbortDataEntryControl.test.tsx
@@ -0,0 +1,50 @@
+import { RouterProvider } from "react-router";
+
+import { render } from "@testing-library/react";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+
+import { electionMockData } from "@/testing/api-mocks/ElectionMockData";
+import { screen, setupTestRouter } from "@/testing/test-utils";
+
+import { useElection } from "../../../hooks/election/useElection";
+import { AbortDataEntryControl } from "./AbortDataEntryControl";
+
+vi.mock("@/hooks/election/useElection");
+
+function renderComponent() {
+  const router = setupTestRouter([
+    {
+      path: "/",
+      element: <AbortDataEntryControl />,
+    },
+    {
+      path: "/elections/:election-id/data-entry",
+      element: <div>test</div>,
+    },
+  ]);
+
+  render(<RouterProvider router={router} />);
+  return router;
+}
+
+describe("AbortDataEntryControl", () => {
+  beforeEach(() => {
+    vi.mocked(useElection).mockReturnValue({
+      election: electionMockData,
+      pollingStations: [],
+      pollingStation: undefined,
+    });
+  });
+  test("renders correctly", () => {
+    renderComponent();
+    expect(screen.getByRole("button", { name: "Invoer afbreken" })).toBeVisible();
+  });
+
+  test("redirects correctly", () => {
+    const router = renderComponent();
+    const button = screen.getByRole("button", { name: "Invoer afbreken" });
+    button.click();
+
+    expect(router.state.location.pathname).toBe(`/elections/${electionMockData.id}/data-entry`);
+  });
+});

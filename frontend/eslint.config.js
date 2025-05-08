63a8a1df242ab97e765a854da01565a93717cf64
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-05-08 16:56:23 +0200

    adds eslint import plugin

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,18 @@
 // @ts-check
 import eslint from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 
 // https://typescript-eslint.io/getting-started/#step-2-configuration
 // https://typescript-eslint.io/packages/typescript-eslint#config
 export default tseslint.config({
   files: ["**/*.ts", "**/*.tsx"],
-  extends: [eslint.configs.recommended, tseslint.configs.recommended],
+  ignores: ["dist", ".eslintrc.cjs", "!.ladle/"],
+  extends: [eslint.configs.recommended, tseslint.configs.recommended, importPlugin.flatConfigs.recommended],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
 });

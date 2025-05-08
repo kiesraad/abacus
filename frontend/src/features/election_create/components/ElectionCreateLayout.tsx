14e1da4313e355676f649bec3c325f2f278bbede
Marlon Peeters <marlonpeeters@tweedegolf.com>
2025-05-08 17:22:46 +0200

    Election Create routing (#1462)

--- frontend/src/features/election_create/components/ElectionCreateLayout.tsx
+++ frontend/src/features/election_create/components/ElectionCreateLayout.tsx
@@ -1,21 +1,22 @@
-import { useState } from "react";
-import { useLocation } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 
 import { Footer } from "@/components/footer/Footer";
 import { NavBar } from "@/components/navbar/NavBar";
 import { PageTitle } from "@/components/page_title/PageTitle";
 import { StickyNav } from "@/components/ui/AppLayout/StickyNav";
 import { ProgressList } from "@/components/ui/ProgressList/ProgressList";
+import { useUserRole } from "@/hooks/user/useUserRole";
 import { t } from "@/lib/i18n";
-import { ElectionDefinitionUploadResponse } from "@/types/generated/openapi";
 
-import { CheckElectionDefinition } from "./CheckElectionDefinition";
-import { UploadElectionDefinition } from "./UploadElectionDefinition";
+import { ElectionCreateContextProvider } from "./ElectionCreateContextProvider";
 
-export function ElectionCreatePage() {
+export function ElectionCreateLayout() {
+  const { isAdministrator } = useUserRole();
   const location = useLocation();
-  const [file, setFile] = useState<File | undefined>(undefined);
-  const [data, setData] = useState<ElectionDefinitionUploadResponse | undefined>(undefined);
+
+  if (!isAdministrator) {
+    return <Navigate to="/account/login" state={{ unauthorized: true }} />;
+  }
 
   return (
     <>
@@ -33,22 +34,25 @@ export function ElectionCreatePage() {
               <ProgressList.Item key="election_definition" status="idle" active>
                 <span>{t("election_definition")}</span>
               </ProgressList.Item>
-              <ProgressList.Item key="polling_station_role" status="idle">
+              <ProgressList.Item key="polling_station_role" status="idle" disabled>
                 <span>{t("polling_station.role")}</span>
               </ProgressList.Item>
-              <ProgressList.Item key="list_of_candidates" status="idle">
+              <ProgressList.Item key="list_of_candidates" status="idle" disabled>
                 <span>{t("candidate.list.plural")}</span>
               </ProgressList.Item>
-              <ProgressList.Item key="polling_stations" status="idle">
+              <ProgressList.Item key="polling_stations" status="idle" disabled>
                 <span>{t("polling_stations")}</span>
               </ProgressList.Item>
-              <ProgressList.Item key="counting_method_type" status="idle">
+              <ProgressList.Item key="counting_method_type" status="idle" disabled>
                 <span>{t("counting_method_type")}</span>
+              </ProgressList.Item>
+              <ProgressList.Item key="number_of_voters" status="idle" disabled>
+                <span>{t("polling_station.number_of_voters")}</span>
               </ProgressList.Item>
             </ProgressList.Fixed>
             <div className="mt-md">
               <ProgressList.Fixed>
-                <ProgressList.Item key="check_and_save" status="idle">
+                <ProgressList.Item key="check_and_save" status="idle" disabled>
                   <span>{t("check_and_save.title")}</span>
                 </ProgressList.Item>
               </ProgressList.Fixed>
@@ -56,11 +60,9 @@ export function ElectionCreatePage() {
           </ProgressList>
         </StickyNav>
         <article>
-          {file && data ? (
-            <CheckElectionDefinition file={file} election={data.election} hash={data.hash} />
-          ) : (
-            <UploadElectionDefinition file={file} setFile={setFile} setData={setData}></UploadElectionDefinition>
-          )}
+          <ElectionCreateContextProvider>
+            <Outlet />
+          </ElectionCreateContextProvider>
         </article>
       </main>
       <Footer />

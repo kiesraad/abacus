2252750f4fb065b508754d0716f68d6f1f2581ed
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-05-08 17:02:50 +0200

    adds section for test files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,22 +5,31 @@ import tseslint from "typescript-eslint";
 
 // https://typescript-eslint.io/getting-started/#step-2-configuration
 // https://typescript-eslint.io/packages/typescript-eslint#config
-export default tseslint.config({
-  files: ["**/*.ts", "**/*.tsx"],
-  ignores: ["dist", ".eslintrc.cjs", "!.ladle/"],
-  extends: [eslint.configs.recommended, tseslint.configs.recommended, importPlugin.flatConfigs.recommended],
-  rules: {
-    "@typescript-eslint/no-unsafe-type-assertion": "error",
-  },
-  settings: {
-    "import/resolver": {
-      typescript: true,
-      node: true,
+export default tseslint.config(
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    ignores: ["dist", ".eslintrc.cjs", "!.ladle/"],
+    extends: [eslint.configs.recommended, tseslint.configs.recommended, importPlugin.flatConfigs.recommended],
+    rules: {
+      "@typescript-eslint/no-unsafe-type-assertion": "error",
+    },
+    settings: {
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+    languageOptions: {
+      parserOptions: {
+        project: "**/tsconfig.json",
+      },
     },
   },
-  languageOptions: {
-    parserOptions: {
-      project: "**/tsconfig.json",
+  {
+    files: ["**/*.test.ts{,x}", "src/testing/**/*.ts{,x}"],
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unsafe-type-assertion": "off",
     },
   },
-});
+);

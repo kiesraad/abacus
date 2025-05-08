bfa314daa7139f2bdcd40bbceaabf01151fcf58a
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-05-08 16:50:56 +0200

    limits linting to ts and tsx files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,4 +2,9 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+// https://typescript-eslint.io/getting-started/#step-2-configuration
+// https://typescript-eslint.io/packages/typescript-eslint#config
+export default tseslint.config({
+  files: ["**/*.ts", "**/*.tsx"],
+  extends: [eslint.configs.recommended, tseslint.configs.recommended],
+});

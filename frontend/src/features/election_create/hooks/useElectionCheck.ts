1b4ad6d804405078f2ba05cabe2b8ec2505d441e
Marlon Peeters <marlonpeeters@tweedegolf.com>
2025-05-09 16:02:44 +0200

    Errors for hash validation (#1470)
    Co-authored-by: Mark Janssen <20283+praseodym@users.noreply.github.com>

---
+++ frontend/src/features/election_create/hooks/useElectionCheck.ts
@@ -0,0 +1,48 @@
+import { FormEvent, useState } from "react";
+
+import { t } from "@/lib/i18n";
+import { ElectionDefinitionUploadResponse } from "@/types/generated/openapi";
+
+import { Stub } from "../components/RedactedHash";
+
+export function useElectionCheck(data: ElectionDefinitionUploadResponse) {
+  const [stubs, setStubs] = useState<Stub[]>(
+    data.hash.redacted_indexes.map((redacted_index: number) => ({
+      selected: false,
+      index: redacted_index,
+      error: "",
+    })),
+  );
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    stubs.forEach((stub, i) => {
+      const value = formData.get(stub.index.toString());
+      const newStubs = [...stubs];
+      const newStub = newStubs[i];
+      if (newStub) {
+        newStub.error = "";
+        if (typeof value !== "string" || value.length !== 4) {
+          newStub.error = t("election.check_eml.check_hash.hint");
+        }
+        setStubs(newStubs);
+      }
+    });
+  }
+
+  function highlightStub(stubIndex: number, highlight: boolean) {
+    const newStubs = [...stubs];
+    const stub = newStubs[stubIndex];
+    if (stub) {
+      stub.selected = highlight;
+    }
+    setStubs(newStubs);
+  }
+
+  return {
+    stubs,
+    highlightStub,
+    handleSubmit,
+  };
+}

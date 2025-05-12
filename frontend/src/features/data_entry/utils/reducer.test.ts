9f2c56a425bdeb0a764b8e45578419ff1c8c1351
Les <les.kleuver@gmail.com>
2025-05-12 14:02:32 +0200

    expand tests for onSubmitForm action

--- frontend/src/features/data_entry/utils/reducer.test.ts
+++ frontend/src/features/data_entry/utils/reducer.test.ts
@@ -1,11 +1,16 @@
-import { expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 
+import { ApiClient } from "@/api/ApiClient";
 import { ApiResponseStatus } from "@/api/ApiResult";
 import { electionMockData } from "@/testing/api-mocks/ElectionMockData";
+import { overrideOnce } from "@/testing/server";
 import { Election, PollingStationResults } from "@/types/generated/openapi";
 
+import { errorWarningMocks, getDefaultDataEntryState } from "../testing/mock-data";
 import { DataEntryAction, DataEntryState } from "../types/types";
+import { onSubmitForm } from "./actions";
 import dataEntryReducer, { getInitialState as _getInitialState } from "./reducer";
+import { ValidationResultSet } from "./ValidationResults";
 
 function getInitialState(): DataEntryState {
   return _getInitialState(electionMockData, 1, 1);
@@ -193,4 +198,110 @@ test("should handle REGISTER_CURRENT_FORM", () => {
   expect(state.formState.current).toEqual(action.formSectionId);
   expect(state.formState.sections.recounted.isSubmitted).toBeDefined();
   expect(state.formState.sections.recounted.isSubmitted).toEqual(false);
+});
+
+describe("onSubmitForm", () => {
+  test("No current section", async () => {
+    const dispatch = vi.fn();
+    const client = {} as ApiClient;
+
+    const submit = onSubmitForm(client, "", dispatch, getDefaultDataEntryState());
+
+    const result = await submit({}, { showAcceptWarnings: true });
+    expect(result).toBe(false);
+    expect(dispatch).toHaveBeenCalledTimes(0);
+  });
+
+  test("Unaccepted warnings", async () => {
+    const dispatch = vi.fn();
+    const client = {} as ApiClient;
+    const defaultState = getDefaultDataEntryState();
+    const state: DataEntryState = {
+      ...defaultState,
+      pollingStationResults: getInitialValues(),
+      formState: {
+        current: "voters_votes_counts",
+        furthest: "voters_votes_counts",
+        sections: {
+          ...defaultState.formState.sections,
+          voters_votes_counts: {
+            ...defaultState.formState.sections.voters_votes_counts,
+            acceptWarnings: false,
+            warnings: new ValidationResultSet([errorWarningMocks.W201]),
+          },
+        },
+      },
+    };
+
+    const submit = onSubmitForm(client, "", dispatch, state);
+
+    const result = await submit({}, { showAcceptWarnings: true });
+    expect(result).toBe(false);
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "UPDATE_FORM_SECTION",
+      partialFormSection: { acceptWarningsError: true },
+    } satisfies DataEntryAction);
+  });
+
+  test.only("Aborting", async () => {
+    const dispatch = vi.fn();
+
+    const defaultState = getDefaultDataEntryState();
+    const state: DataEntryState = {
+      ...defaultState,
+      pollingStationResults: getInitialValues(),
+      cache: {
+        key: "voters_votes_counts",
+        data: {
+          voters_counts: {
+            poll_card_count: 1,
+            proxy_certificate_count: 2,
+            voter_card_count: 3,
+            total_admitted_voters_count: 4,
+          },
+          votes_counts: {
+            votes_candidates_count: 5,
+            blank_votes_count: 6,
+            invalid_votes_count: 7,
+            total_votes_cast_count: 8,
+          },
+        },
+      },
+    };
+
+    const client = new ApiClient();
+
+    const requestPath = "/api/polling_stations/1/data_entries/1";
+    const submit = onSubmitForm(client, requestPath, dispatch, state);
+
+    overrideOnce("post", requestPath, 200, {
+      validation_results: {
+        errors: [],
+        warnings: [],
+      },
+    });
+
+    const result = await submit({}, { aborting: true });
+
+    expect(dispatch).toHaveBeenCalledTimes(3);
+
+    expect(dispatch.mock.calls[0]).toStrictEqual([{ type: "SET_STATUS", status: "saving" } satisfies DataEntryAction]);
+    expect(dispatch.mock.calls[1]).toStrictEqual([{ type: "SET_STATUS", status: "aborted" } satisfies DataEntryAction]);
+
+    const data: PollingStationResults = {
+      ...getInitialValues(),
+      ...state.cache!.data,
+    };
+
+    expect(dispatch.mock.calls[2]).toStrictEqual([
+      {
+        type: "FORM_SAVED",
+        data,
+        validationResults: { errors: [], warnings: [] },
+        aborting: true,
+        continueToNextSection: true,
+      } satisfies DataEntryAction,
+    ]);
+    expect(result).toBe(true);
+  });
 });

6186c178d9aa984935e5bd26156c39105a26ec1c
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-05-16 09:39:40 +0200

    splits NumberInput e2e test in two and only does the clear() for chromium

--- frontend/src/components/ui/NumberInput/NumberInput.e2e.ts
+++ frontend/src/components/ui/NumberInput/NumberInput.e2e.ts
@@ -14,10 +14,22 @@ test.describe("NumberInput", () => {
 
     await input.blur();
     await expect(input).toHaveValue("12.300");
+  });
+
+  test("change formatted number", async ({ page, browserName }) => {
+    await page.goto("/?story=number-input--default-number-input");
+    await page.waitForSelector("[data-storyloaded]");
+
+    const input = page.getByTestId("test");
+    await expect(input).toBeVisible();
+    await expect(input).toHaveValue("12.300");
 
     // BUG: Playwright with Chrome will append the new value instead of replacing the old value
     // without `clear()` or `selectText()` or `focus()`
-    await input.clear();
+    // eslint-disable-next-line playwright/no-conditional-in-test
+    if (browserName == "chromium") {
+      await input.clear();
+    }
 
     await input.fill("9999");
     await expect(input).toHaveValue("9999");

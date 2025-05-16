348bfadb108428ed47fcbe6a1896a321ebaf56fd
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-05-16 10:58:20 +0200

    adds global eslint ignores

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,15 +1,20 @@
 // @ts-check
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
-import globals from "globals";
+// import globals from "globals";
 import tseslint from "typescript-eslint";
 
 // https://typescript-eslint.io/getting-started/#step-2-configuration
 // https://typescript-eslint.io/packages/typescript-eslint#config
+
 export default tseslint.config(
   {
+    // global ignores
+    ignores: ["dist/**", ".eslintrc.cjs"],
+  },
+  {
     files: ["**/*.ts", "**/*.tsx"],
-    ignores: ["dist", ".eslintrc.cjs", "!.ladle/"],
+    ignores: ["!.ladle/**"],
     extends: [eslint.configs.recommended, tseslint.configs.recommended, importPlugin.flatConfigs.recommended],
     rules: {
       "@typescript-eslint/no-unsafe-type-assertion": "error",

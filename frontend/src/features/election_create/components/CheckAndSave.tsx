558d904e98f4f59c62e1670caeb5e2d81f8717f7
cikzh <marlonpeeters@tweedegolf.com>
2025-05-20 17:45:04 +0200

    Added check and save election page

---
+++ frontend/src/features/election_create/components/CheckAndSave.tsx
@@ -0,0 +1,34 @@
+import { Navigate } from "react-router";
+
+import { Button } from "@/components/ui/Button/Button";
+import { t } from "@/i18n/translate";
+
+import { useElectionCreateContext } from "../hooks/useElectionCreateContext";
+
+export function CheckAndSave() {
+  const { data } = useElectionCreateContext();
+
+  if (!data) {
+    return <Navigate to="/elections/create" />;
+  }
+
+  return (
+    <section className="md">
+      <h2>{t("election.check_and_save.title")}</h2>
+      <p className="mt-lg mb-xl">
+        {t("election.check_and_save.description")}
+        <ul>
+          <li>
+            <strong>{t("election.singular")}:</strong> {data.election.name}
+          </li>
+          <li>
+            <strong>{t("area_designation")}:</strong> {data.election.location}
+          </li>
+        </ul>
+      </p>
+      <div className="mt-lg">
+        <Button>{t("save")}</Button>
+      </div>
+    </section>
+  );
+}

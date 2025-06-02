02ef64048e20fc3ad8cd998e9fd7af7e19db5c76
Marlon Peeters <marlonpeeters@tweedegolf.com>
2025-06-02 10:14:11 +0200

    Refactor election creation state management and add save call (#1549)

--- frontend/src/features/election_create/components/ElectionCreateContextProvider.tsx
+++ frontend/src/features/election_create/components/ElectionCreateContextProvider.tsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
-import { ElectionDefinitionValidateResponse } from "@/types/generated/openapi";
+import { ElectionDefinitionValidateResponse, NewElection, RedactedEmlHash } from "@/types/generated/openapi";
 
 import { ElectionCreateContext, IElectionCreateContext } from "../hooks/ElectionCreateContext";
 
-export function ElectionCreateContextProvider({ children }: { children: React.ReactNode }) {
-  const [file, setFile] = useState<File | undefined>(undefined);
-  const [data, setData] = useState<ElectionDefinitionValidateResponse | undefined>(undefined);
+export type ElectionCreateAction =
+  | {
+      type: "SELECT_ELECTION_DEFINITION";
+      response: ElectionDefinitionValidateResponse;
+      electionDefinitionData: string;
+      electionDefinitionFileName: string;
+    }
+  | {
+      type: "SET_ELECTION_DEFINITION_HASH";
+      electionDefinitionHash: string[];
+    };
 
-  const context: IElectionCreateContext = { file, setFile, data, setData };
+export interface ElectionCreateState {
+  electionDefinitionRedactedHash?: RedactedEmlHash;
+  election?: NewElection;
+  electionDefinitionHash?: string[];
+  electionDefinitionData?: string;
+  electionDefinitionFileName?: string;
+}
+
+function reducer(state: ElectionCreateState, action: ElectionCreateAction): ElectionCreateState {
+  switch (action.type) {
+    case "SELECT_ELECTION_DEFINITION":
+      return {
+        ...state,
+        election: action.response.election,
+        electionDefinitionRedactedHash: action.response.hash,
+        electionDefinitionData: action.electionDefinitionData,
+        electionDefinitionFileName: action.electionDefinitionFileName,
+      };
+    case "SET_ELECTION_DEFINITION_HASH":
+      return {
+        ...state,
+        electionDefinitionHash: action.electionDefinitionHash,
+      };
+  }
+}
+
+export function ElectionCreateContextProvider({ children }: { children: React.ReactNode }) {
+  const [state, dispatch] = useReducer(reducer, {});
+  const context: IElectionCreateContext = { state, dispatch };
   return <ElectionCreateContext.Provider value={context}>{children}</ElectionCreateContext.Provider>;
 }

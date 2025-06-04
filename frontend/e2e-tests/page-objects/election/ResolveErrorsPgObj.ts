18c1723bcf7ebeeb14aad444635703265fdc7379
Lionqueen94 <lionqueen94@gmail.com>
2025-06-04 17:02:52 +0200

    Added some more tests

--- frontend/e2e-tests/page-objects/election/ResolveErrorsPgObj.ts
+++ frontend/e2e-tests/page-objects/election/ResolveErrorsPgObj.ts
@@ -1,16 +1,12 @@
 import { type Locator, type Page } from "@playwright/test";
 
 export class ResolveErrorsPgObj {
-  readonly firstValue: Locator;
-  readonly secondValue: Locator;
   readonly validationError: Locator;
   readonly resumeFirstEntry: Locator;
   readonly discardFirstEntry: Locator;
   readonly save: Locator;
 
   constructor(protected readonly page: Page) {
-    this.firstValue = this.page.getByRole("cell").nth(0);
-    this.secondValue = this.page.getByRole("cell").nth(1);
     this.validationError = page.getByText(/Dit is een verplichte vraag/);
     this.resumeFirstEntry = page.getByLabel(/Invoer bewaren/);
     this.discardFirstEntry = page.getByLabel(/Invoer verwijderen/);

5afd4a2582f739db8d4950b37224e3c7ddb342d0
Bram Tweedegolf <bram@tweedegolf.com>
2025-06-23 15:53:30 +0200

    Add create election abort button and confirmation modal on navigation

--- frontend/e2e-tests/page-objects/election/create/CheckElectionDefinitionPgObj.ts
+++ frontend/e2e-tests/page-objects/election/create/CheckElectionDefinitionPgObj.ts
@@ -1,12 +1,13 @@
 import { type Locator, type Page } from "@playwright/test";
 
-export class CheckDefinitionPgObj {
+export class CheckElectionDefinitionPgObj {
   readonly header: Locator;
   readonly hash: Locator;
   readonly hashInput1: Locator;
   readonly hashInput2: Locator;
   readonly next: Locator;
   readonly error: Locator;
+  readonly backToOverviewButton: Locator;
 
   constructor(protected readonly page: Page) {
     this.header = page.getByRole("heading", { level: 2, name: "Controleer verkiezingsdefinitie" });
@@ -15,5 +16,6 @@ export class CheckDefinitionPgObj {
     this.hashInput2 = page.getByLabel("Controle deel 2");
     this.next = page.getByRole("button", { name: "Volgende" });
     this.error = page.getByRole("heading", { level: 3, name: "Controle digitale vingerafdruk niet gelukt" });
+    this.backToOverviewButton = page.getByRole("link", { name: "Verkiezingen" });
   }
 }
--- frontend/e2e-tests/tests/election-create.e2e.ts
+++ frontend/e2e-tests/tests/election-create.e2e.ts
@@ -1,7 +1,8 @@
 import { expect } from "@playwright/test";
+import { AbortModalPgObj } from "e2e-tests/page-objects/election/create/AbortModalPgObj";
 import { CheckAndSavePgObj } from "e2e-tests/page-objects/election/create/CheckAndSavePgObj";
 import { CheckCandidateDefinitionPgObj } from "e2e-tests/page-objects/election/create/CheckCandidateDefinitionPgObj";
-import { CheckDefinitionPgObj } from "e2e-tests/page-objects/election/create/CheckDefinitionPgObj";
+import { CheckElectionDefinitionPgObj } from "e2e-tests/page-objects/election/create/CheckElectionDefinitionPgObj";
 import { UploadCandidateDefinitionPgObj } from "e2e-tests/page-objects/election/create/UploadCandidateDefinitionPgObj";
 import { UploadDefinitionPgObj } from "e2e-tests/page-objects/election/create/UploadDefinitionPgObj";
 import { OverviewPgObj } from "e2e-tests/page-objects/election/OverviewPgObj";
@@ -24,7 +25,7 @@ test.describe("Election creation", () => {
     await expect(uploadDefinitionPage.header).toBeVisible();
     await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
 
-    const checkDefinitionPage = new CheckDefinitionPgObj(page);
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
     await expect(checkDefinitionPage.header).toBeVisible();
     // Check that the uploaded file name is present somewhere on the page
     await expect(page.getByText("eml110a_test.eml.xml")).toBeVisible();
@@ -74,7 +75,7 @@ test.describe("Election creation", () => {
     await expect(uploadDefinitionPage.header).toBeVisible();
     await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
 
-    const checkDefinitionPage = new CheckDefinitionPgObj(page);
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
     await expect(checkDefinitionPage.hashInput1).toBeFocused();
     // Wrong hash
     await checkDefinitionPage.hashInput1.fill("1234");
@@ -103,7 +104,7 @@ test.describe("Election creation", () => {
     await expect(uploadDefinitionPage.header).toBeVisible();
     await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
 
-    const checkDefinitionPage = new CheckDefinitionPgObj(page);
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
     await expect(checkDefinitionPage.header).toBeVisible();
     await expect(page.getByText("eml110a_test.eml.xml")).toBeVisible();
     await expect(page.getByText("Woensdag 16 maart 2022")).toBeVisible();
@@ -139,7 +140,7 @@ test.describe("Election creation", () => {
     await expect(uploadDefinitionPage.header).toBeVisible();
     await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
 
-    const checkDefinitionPage = new CheckDefinitionPgObj(page);
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
     await expect(checkDefinitionPage.header).toBeVisible();
     await expect(page.getByText("eml110a_test.eml.xml")).toBeVisible();
     await expect(page.getByText("Woensdag 16 maart 2022")).toBeVisible();
@@ -153,5 +154,142 @@ test.describe("Election creation", () => {
     await expect(uploadCandidateDefinitionPage.header).toBeVisible();
     await uploadCandidateDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110b_test.eml.xml");
     await expect(uploadCandidateDefinitionPage.error).toBeVisible();
+  });
+
+  test("while uploading an election, navigating away should trigger the warning modal", async ({ page }) => {
+    await page.goto("/elections");
+    const overviewPage = new OverviewPgObj(page);
+    await overviewPage.create.click();
+
+    const uploadDefinitionPage = new UploadDefinitionPgObj(page);
+    await expect(uploadDefinitionPage.header).toBeVisible();
+    await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
+
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
+    await expect(checkDefinitionPage.header).toBeVisible();
+
+    // Menu button back to election overview
+    await expect(checkDefinitionPage.backToOverviewButton).toBeVisible();
+    await checkDefinitionPage.backToOverviewButton.click();
+
+    // Abort modal should have stopped navigation
+    const AbortModal = new AbortModalPgObj(page);
+    await expect(AbortModal.header).toBeVisible();
+  });
+
+  test("warning modal close button should stay on page", async ({ page }) => {
+    await page.goto("/elections");
+    const overviewPage = new OverviewPgObj(page);
+    await overviewPage.create.click();
+
+    const uploadDefinitionPage = new UploadDefinitionPgObj(page);
+    await expect(uploadDefinitionPage.header).toBeVisible();
+    await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
+
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
+    await expect(checkDefinitionPage.header).toBeVisible();
+
+    // Menu button back to election overview
+    await expect(checkDefinitionPage.backToOverviewButton).toBeVisible();
+    await checkDefinitionPage.backToOverviewButton.click();
+
+    // Abort modal should have stopped navigation
+    const abortModal = new AbortModalPgObj(page);
+    await expect(abortModal.header).toBeVisible();
+
+    // Click cancel, assert we are still on the election create page
+    await expect(abortModal.closeButton).toBeVisible();
+    await abortModal.closeButton.click();
+    const updatedCheckDefinitionPage = new CheckElectionDefinitionPgObj(page);
+    await expect(updatedCheckDefinitionPage.header).toBeVisible();
+  });
+
+  test("warning modal cancel button should stay on page", async ({ page }) => {
+    await page.goto("/elections");
+    const overviewPage = new OverviewPgObj(page);
+    await overviewPage.create.click();
+
+    const uploadDefinitionPage = new UploadDefinitionPgObj(page);
+    await expect(uploadDefinitionPage.header).toBeVisible();
+    await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
+
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
+    await expect(checkDefinitionPage.header).toBeVisible();
+
+    // Menu button back to election overview
+    await expect(checkDefinitionPage.backToOverviewButton).toBeVisible();
+    await checkDefinitionPage.backToOverviewButton.click();
+
+    // Abort modal should have stopped navigation
+    const abortModal = new AbortModalPgObj(page);
+    await expect(abortModal.header).toBeVisible();
+
+    // Click cancel, assert we are still on the election create page
+    await expect(abortModal.cancelButton).toBeVisible();
+    await abortModal.cancelButton.click();
+    const updatedCheckDefinitionPage = new CheckElectionDefinitionPgObj(page);
+    await expect(updatedCheckDefinitionPage.header).toBeVisible();
+  });
+
+  test("warning modal delete button should continue navigation", async ({ page }) => {
+    await page.goto("/elections");
+    const overviewPage = new OverviewPgObj(page);
+    await overviewPage.create.click();
+
+    const uploadDefinitionPage = new UploadDefinitionPgObj(page);
+    await expect(uploadDefinitionPage.header).toBeVisible();
+    await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
+
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
+    await expect(checkDefinitionPage.header).toBeVisible();
+
+    // Menu button back to election overview
+    await expect(checkDefinitionPage.backToOverviewButton).toBeVisible();
+    await checkDefinitionPage.backToOverviewButton.click();
+
+    // Abort modal should have stopped navigation
+    const abortModal = new AbortModalPgObj(page);
+    await expect(abortModal.header).toBeVisible();
+
+    // Click delete, assert we are bcak at the overview
+    await expect(abortModal.deleteButton).toBeVisible();
+    await abortModal.deleteButton.click();
+    const updatedOverviewPage = new OverviewPgObj(page);
+    await expect(updatedOverviewPage.header).toBeVisible();
+  });
+
+  test("uploading a candidate list, then navigating should trigger the modal", async ({ page }) => {
+    await page.goto("/elections");
+    const overviewPage = new OverviewPgObj(page);
+    await overviewPage.create.click();
+
+    const uploadDefinitionPage = new UploadDefinitionPgObj(page);
+    await expect(uploadDefinitionPage.header).toBeVisible();
+    await uploadDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml110a_test.eml.xml");
+
+    const checkDefinitionPage = new CheckElectionDefinitionPgObj(page);
+    await expect(checkDefinitionPage.header).toBeVisible();
+    // Check that the uploaded file name is present somewhere on the page
+    await expect(page.getByText("eml110a_test.eml.xml")).toBeVisible();
+    // Election date
+    await expect(page.getByText("Woensdag 16 maart 2022")).toBeVisible();
+
+    await expect(checkDefinitionPage.hashInput1).toBeFocused();
+    await checkDefinitionPage.hashInput1.fill("9825");
+    await checkDefinitionPage.hashInput2.fill("8af1");
+    await checkDefinitionPage.next.click();
+
+    // Candidate page
+    const uploadCandidateDefinitionPage = new UploadCandidateDefinitionPgObj(page);
+    await expect(uploadCandidateDefinitionPage.header).toBeVisible();
+    await uploadCandidateDefinitionPage.uploadFile(page, "../backend/src/eml/tests/eml230b_test.eml.xml");
+
+    // Menu button back to election overview
+    await expect(checkDefinitionPage.backToOverviewButton).toBeVisible();
+    await checkDefinitionPage.backToOverviewButton.click();
+
+    // Abort modal should have stopped navigation
+    const AbortModal = new AbortModalPgObj(page);
+    await expect(AbortModal.header).toBeVisible();
   });
 });

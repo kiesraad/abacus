ff26564b8e6f1a1bf53eb630e919db97c1812fba
Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>
2025-06-25 13:10:24 +0200

    Refactor election create e2e test (#1671)

--- frontend/e2e-tests/page-objects/election/create/CheckCandidateDefinitionPgObj.ts
+++ frontend/e2e-tests/page-objects/election/create/CheckCandidateDefinitionPgObj.ts
@@ -2,7 +2,6 @@ import { type Locator, type Page } from "@playwright/test";
 
 export class CheckCandidateDefinitionPgObj {
   readonly header: Locator;
-  readonly hash: Locator;
   readonly hashInput1: Locator;
   readonly hashInput2: Locator;
   readonly next: Locator;
@@ -10,7 +9,6 @@ export class CheckCandidateDefinitionPgObj {
 
   constructor(protected readonly page: Page) {
     this.header = page.getByRole("heading", { level: 2, name: "Controleer kandidatenlijst" });
-    this.hash = page.getByTestId("hash");
     this.hashInput1 = page.getByLabel("Controle deel 1");
     this.hashInput2 = page.getByLabel("Controle deel 2");
     this.next = page.getByRole("button", { name: "Volgende" });
--- frontend/e2e-tests/page-objects/election/create/CheckDefinitionPgObj.ts
+++ frontend/e2e-tests/page-objects/election/create/CheckDefinitionPgObj.ts
@@ -2,7 +2,6 @@ import { type Locator, type Page } from "@playwright/test";
 
 export class CheckDefinitionPgObj {
   readonly header: Locator;
-  readonly hash: Locator;
   readonly hashInput1: Locator;
   readonly hashInput2: Locator;
   readonly next: Locator;
@@ -10,7 +9,6 @@ export class CheckDefinitionPgObj {
 
   constructor(protected readonly page: Page) {
     this.header = page.getByRole("heading", { level: 2, name: "Controleer verkiezingsdefinitie" });
-    this.hash = page.getByTestId("hash");
     this.hashInput1 = page.getByLabel("Controle deel 1");
     this.hashInput2 = page.getByLabel("Controle deel 2");
     this.next = page.getByRole("button", { name: "Volgende" });

9332bca3a62561733de8c57d41fba985eb6272ad
Lionqueen94 <lionqueen94@gmail.com>
2025-07-18 16:09:31 +0200

    Added tests for committee session status.rs

--- backend/src/committee_session/status.rs
+++ backend/src/committee_session/status.rs
@@ -7,7 +7,7 @@ use utoipa::ToSchema;
 use crate::{
     APIError,
     audit_log::{AuditEvent, AuditService},
-    committee_session::{CommitteeSession, repository::CommitteeSessions},
+    committee_session::repository::CommitteeSessions,
     polling_station::repository::PollingStations,
 };
 
@@ -62,7 +62,7 @@ pub async fn change_committee_session_status(
         CommitteeSessionStatus::DataEntryNotStarted => {
             committee_session
                 .status
-                .ready_for_data_entry(committee_session, polling_stations_repo)
+                .ready_for_data_entry(committee_session.election_id, polling_stations_repo)
                 .await?
         }
         CommitteeSessionStatus::DataEntryInProgress => {
@@ -70,7 +70,7 @@ pub async fn change_committee_session_status(
         }
         CommitteeSessionStatus::DataEntryPaused => committee_session.status.pause_data_entry()?,
         CommitteeSessionStatus::DataEntryFinished => {
-            committee_session.status.finish_data_entry().await?
+            committee_session.status.finish_data_entry()?
         }
     };
 
@@ -103,14 +103,12 @@ impl CommitteeSessionStatus {
 
     pub async fn ready_for_data_entry(
         self,
-        committee_session: CommitteeSession,
+        election_id: u32,
         polling_stations_repo: PollingStations,
     ) -> Result<Self, CommitteeSessionTransitionError> {
         match self {
             CommitteeSessionStatus::Created => {
-                let polling_stations = polling_stations_repo
-                    .list(committee_session.election_id)
-                    .await?;
+                let polling_stations = polling_stations_repo.list(election_id).await?;
                 if polling_stations.is_empty() {
                     Err(CommitteeSessionTransitionError::Invalid)
                 } else {
@@ -162,7 +160,7 @@ impl CommitteeSessionStatus {
         }
     }
 
-    pub async fn finish_data_entry(self) -> Result<Self, CommitteeSessionTransitionError> {
+    pub fn finish_data_entry(self) -> Result<Self, CommitteeSessionTransitionError> {
         match self {
             CommitteeSessionStatus::Created => Err(CommitteeSessionTransitionError::Invalid),
             CommitteeSessionStatus::DataEntryNotStarted => {
@@ -192,5 +190,275 @@ impl Display for CommitteeSessionTransitionError {
 impl Default for CommitteeSessionStatus {
     fn default() -> Self {
         Self::Created
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_log::test;
+
+    /// Created --> Created: prepare_data_entry
+    #[test]
+    fn committee_session_status_created_to_created() {
+        assert_eq!(
+            CommitteeSessionStatus::Created.prepare_data_entry(),
+            Ok(CommitteeSessionStatus::Created)
+        );
+    }
+
+    /// DataEntryNotStarted --> Created: prepare_data_entry
+    #[test]
+    fn committee_session_status_data_entry_not_started_to_created() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryNotStarted.prepare_data_entry(),
+            Ok(CommitteeSessionStatus::Created)
+        );
+    }
+
+    /// DataEntryInProgress --> Created: prepare_data_entry
+    #[test]
+    fn committee_session_status_data_entry_in_progress_to_created() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryInProgress.prepare_data_entry(),
+            Ok(CommitteeSessionStatus::Created)
+        );
+    }
+
+    /// DataEntryPaused --> Created: prepare_data_entry
+    #[test]
+    fn committee_session_status_data_entry_paused_to_created() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryPaused.prepare_data_entry(),
+            Ok(CommitteeSessionStatus::Created)
+        );
+    }
+
+    /// DataEntryFinished --> Created: prepare_data_entry
+    #[test]
+    fn committee_session_status_data_entry_finished_to_created() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryFinished.prepare_data_entry(),
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// Created --> DataEntryNotStarted: ready for data entry
+    #[test(sqlx::test(fixtures(path = "../../fixtures", scripts("election_6"))))]
+    async fn committee_session_status_created_to_data_entry_not_started_no_polling_stations(
+        pool: SqlitePool,
+    ) {
+        let polling_stations_repo = PollingStations::new(pool.clone());
+        assert_eq!(
+            CommitteeSessionStatus::Created
+                .ready_for_data_entry(6, polling_stations_repo)
+                .await,
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// Created --> DataEntryNotStarted: ready for data entry
+    #[test(sqlx::test(fixtures(path = "../../fixtures", scripts("election_2"))))]
+    async fn committee_session_status_created_to_data_entry_not_started_with_polling_stations(
+        pool: SqlitePool,
+    ) {
+        let polling_stations_repo = PollingStations::new(pool.clone());
+        assert_eq!(
+            CommitteeSessionStatus::Created
+                .ready_for_data_entry(2, polling_stations_repo)
+                .await,
+            Ok(CommitteeSessionStatus::DataEntryNotStarted)
+        );
+    }
+
+    /// DataEntryNotStarted --> DataEntryNotStarted: ready for data entry
+    #[test(sqlx::test(fixtures(path = "../../fixtures", scripts("election_2"))))]
+    async fn committee_session_status_data_entry_not_started_to_data_entry_not_started(
+        pool: SqlitePool,
+    ) {
+        let polling_stations_repo = PollingStations::new(pool.clone());
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryNotStarted
+                .ready_for_data_entry(2, polling_stations_repo)
+                .await,
+            Ok(CommitteeSessionStatus::DataEntryNotStarted)
+        );
+    }
+
+    /// DataEntryInProgress --> DataEntryNotStarted: ready for data entry
+    #[test(sqlx::test(fixtures(path = "../../fixtures", scripts("election_2"))))]
+    async fn committee_session_status_data_entry_in_progress_to_data_entry_not_started(
+        pool: SqlitePool,
+    ) {
+        let polling_stations_repo = PollingStations::new(pool.clone());
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryInProgress
+                .ready_for_data_entry(2, polling_stations_repo)
+                .await,
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// DataEntryPaused --> DataEntryNotStarted: ready for data entry
+    #[test(sqlx::test(fixtures(path = "../../fixtures", scripts("election_2"))))]
+    async fn committee_session_status_data_entry_paused_to_data_entry_not_started(
+        pool: SqlitePool,
+    ) {
+        let polling_stations_repo = PollingStations::new(pool.clone());
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryPaused
+                .ready_for_data_entry(2, polling_stations_repo)
+                .await,
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// DataEntryFinished --> DataEntryNotStarted: ready for data entry
+    #[test(sqlx::test(fixtures(path = "../../fixtures", scripts("election_2"))))]
+    async fn committee_session_status_data_entry_finished_to_data_entry_not_started(
+        pool: SqlitePool,
+    ) {
+        let polling_stations_repo = PollingStations::new(pool.clone());
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryFinished
+                .ready_for_data_entry(2, polling_stations_repo)
+                .await,
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// Created --> DataEntryInProgress: start_data_entry
+    #[test]
+    fn committee_session_status_created_to_data_entry_in_progress() {
+        assert_eq!(
+            CommitteeSessionStatus::Created.start_data_entry(),
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// DataEntryNotStarted --> DataEntryInProgress: start_data_entry
+    #[test]
+    fn committee_session_status_data_entry_not_started_to_data_entry_in_progress() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryNotStarted.start_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryInProgress)
+        );
+    }
+
+    /// DataEntryInProgress --> DataEntryInProgress: start_data_entry
+    #[test]
+    fn committee_session_status_data_entry_in_progress_to_data_entry_in_progress() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryInProgress.start_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryInProgress)
+        );
+    }
+
+    /// DataEntryPaused --> DataEntryInProgress: start_data_entry
+    #[test]
+    fn committee_session_status_data_entry_paused_to_data_entry_in_progress() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryPaused.start_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryInProgress)
+        );
+    }
+
+    /// DataEntryFinished --> DataEntryInProgress: start_data_entry
+    #[test]
+    fn committee_session_status_data_entry_finished_to_data_entry_in_progress() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryFinished.start_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryInProgress)
+        );
+    }
+
+    /// Created --> DataEntryPaused: pause_data_entry
+    #[test]
+    fn committee_session_status_created_to_data_entry_paused() {
+        assert_eq!(
+            CommitteeSessionStatus::Created.pause_data_entry(),
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// DataEntryNotStarted --> DataEntryPaused: pause_data_entry
+    #[test]
+    fn committee_session_status_data_entry_not_started_to_data_entry_paused() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryNotStarted.pause_data_entry(),
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// DataEntryInProgress --> DataEntryPaused: pause_data_entry
+    #[test]
+    fn committee_session_status_data_entry_in_progress_to_data_entry_paused() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryInProgress.pause_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryPaused)
+        );
+    }
+
+    /// DataEntryPaused --> DataEntryPaused: pause_data_entry
+    #[test]
+    fn committee_session_status_data_entry_paused_to_data_entry_paused() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryPaused.pause_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryPaused)
+        );
+    }
+
+    /// DataEntryFinished --> DataEntryPaused: pause_data_entry
+    #[test]
+    fn committee_session_status_data_entry_finished_to_data_entry_paused() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryFinished.pause_data_entry(),
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// Created --> DataEntryFinished: finish_data_entry
+    #[test]
+    fn committee_session_status_created_to_data_entry_finished() {
+        assert_eq!(
+            CommitteeSessionStatus::Created.finish_data_entry(),
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// DataEntryNotStarted --> DataEntryFinished: finish_data_entry
+    #[test]
+    fn committee_session_status_data_entry_not_started_to_data_entry_finished() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryNotStarted.finish_data_entry(),
+            Err(CommitteeSessionTransitionError::Invalid)
+        );
+    }
+
+    /// DataEntryInProgress --> DataEntryFinished: finish_data_entry
+    #[test]
+    fn committee_session_status_data_entry_in_progress_to_data_entry_finished() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryInProgress.finish_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryFinished)
+        );
+    }
+
+    /// DataEntryPaused --> DataEntryFinished: finish_data_entry
+    #[test]
+    fn committee_session_status_data_entry_paused_to_data_entry_finished() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryPaused.finish_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryFinished)
+        );
+    }
+
+    /// DataEntryFinished --> DataEntryFinished: finish_data_entry
+    #[test]
+    fn committee_session_status_data_entry_finished_to_data_entry_finished() {
+        assert_eq!(
+            CommitteeSessionStatus::DataEntryFinished.finish_data_entry(),
+            Ok(CommitteeSessionStatus::DataEntryFinished)
+        );
     }
 }

27bca3c1cb1c3b89adc99c97cb6ef96fdf39a761
Marlon Baeten <marlon@tweedegolf.com>
2025-07-24 10:18:17 +0200

    Remove unnecessary file

--- frontend/e2e-tests/page-objects/election/ElectionHomePgObj.ts
+++ frontend/e2e-tests/page-objects/election/ElectionHomePgObj.ts
@@ -4,6 +4,6 @@ export class ElectionHome {
   readonly downloadBijlage1: Locator;
 
   constructor(protected readonly page: Page) {
-    this.downloadBijlage1 = page.getByRole("cell", { name: "Na 31-2 Bijlage" });
+    this.downloadBijlage1 = page.getByRole("cell", { name: "Na 31-2 Bijlage 1" });
   }
 }

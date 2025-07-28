b12dd2cb017873b9f0aad331712ea96af09feba5
Bram Tweedegolf <bram@tweedegolf.com>
2025-07-28 16:46:05 +0200

    Use transaction for create many polling stations

--- backend/src/polling_station/repository.rs
+++ backend/src/polling_station/repository.rs
@@ -1,5 +1,5 @@
 use axum::extract::FromRef;
-use sqlx::{QueryBuilder, SqlitePool, query, query_as};
+use sqlx::{SqlitePool, query, query_as};
 
 use super::structs::{PollingStation, PollingStationRequest};
 use crate::AppState;
@@ -137,32 +137,48 @@ impl PollingStations {
         election_id: u32,
         new_polling_stations: Vec<PollingStationRequest>,
     ) -> Result<Vec<PollingStation>, sqlx::Error> {
-        // Max number of parameters for a statement
-        // See: https://www.sqlite.org/limits.html
-        const BIND_LIMIT: usize = 999;
-
         let mut stations: Vec<PollingStation> = Vec::new();
-
-        let batches = new_polling_stations.chunks(BIND_LIMIT / 8);
-        for batch in batches {
-            let mut inserted: Vec<PollingStation> = QueryBuilder::new(
-                "INSERT INTO polling_stations (election_id, name, number, number_of_voters, polling_station_type, address, postal_code, locality) "
-              ).push_values(batch.iter(), |mut b, station| {
-                b.push_bind(election_id)
-                 .push_bind(&station.name)
-                 .push_bind(station.number)
-                 .push_bind(station.number_of_voters)
-                 .push_bind(&station.polling_station_type)
-                 .push_bind(&station.address)
-                 .push_bind(&station.postal_code)
-                 .push_bind(&station.locality);
-              })
-              .push(" RETURNING id, election_id, name, number, number_of_voters, polling_station_type, address, postal_code, locality")
-              .build_query_as()
-              .fetch_all(&self.0)
-              .await?;
-            stations.append(&mut inserted);
+        let mut tx = self.0.begin().await?;
+        for new_polling_station in new_polling_stations {
+            stations.push(
+                query_as!(
+                    PollingStation,
+                    r#"
+                INSERT INTO polling_stations (
+                  election_id,
+                  name,
+                  number,
+                  number_of_voters,
+                  polling_station_type,
+                  address,
+                  postal_code,
+                  locality
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                RETURNING
+                  id AS "id: u32",
+                  election_id AS "election_id: u32",
+                  name,
+                  number,
+                  number_of_voters,
+                  polling_station_type AS "polling_station_type: _",
+                  address,
+                  postal_code,
+                  locality
+                "#,
+                    election_id,
+                    new_polling_station.name,
+                    new_polling_station.number,
+                    new_polling_station.number_of_voters,
+                    new_polling_station.polling_station_type,
+                    new_polling_station.address,
+                    new_polling_station.postal_code,
+                    new_polling_station.locality,
+                )
+                .fetch_one(&mut *tx)
+                .await?,
+            );
         }
+        tx.commit().await?;
 
         Ok(stations)
     }

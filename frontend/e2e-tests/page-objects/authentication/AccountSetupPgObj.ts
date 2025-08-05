ec3ca1db36afbf24cfc61ee67a88cae236d5a6e3
Marlon <marlonbaeten@users.noreply.github.com>
2025-08-05 10:54:11 +0200

    Improve NavBar page object (#1901)

--- frontend/e2e-tests/page-objects/authentication/AccountSetupPgObj.ts
+++ frontend/e2e-tests/page-objects/authentication/AccountSetupPgObj.ts
@@ -1,16 +1,12 @@
 import { type Locator, type Page } from "@playwright/test";
 
-import { NavBar } from "../NavBarPgObj";
-
 export class AccountSetupPgObj {
-  readonly navBar: NavBar;
   readonly fullname: Locator;
   readonly password: Locator;
   readonly passwordRepeat: Locator;
   readonly nextBtn: Locator;
 
   constructor(protected readonly page: Page) {
-    this.navBar = new NavBar(page);
     this.fullname = page.getByRole("textbox", { name: "Jouw naam (roepnaam +" });
     this.password = page.getByRole("textbox", { name: "Kies nieuw wachtwoord" });
     this.passwordRepeat = page.getByRole("textbox", { name: "Herhaal wachtwoord" });
--- frontend/e2e-tests/page-objects/nav_bar/TypistNavBarPgObj.ts
+++ frontend/e2e-tests/page-objects/nav_bar/TypistNavBarPgObj.ts
@@ -1,18 +1,16 @@
 import { type Locator, type Page } from "@playwright/test";
 
-export class NavBar {
+export class TypistNavBar {
   readonly logout: Locator;
   readonly navigation: Locator;
   readonly username: Locator;
   readonly role: Locator;
-  readonly electionOverviewButton: Locator;
 
   constructor(protected readonly page: Page) {
     this.navigation = page.getByRole("navigation", { name: "primary-navigation" });
     this.logout = this.navigation.getByRole("link", { name: "Afmelden" });
     this.username = this.navigation.getByTestId("navbar-username");
     this.role = this.navigation.getByTestId("navbar-role");
-    this.electionOverviewButton = page.getByRole("link", { name: "Verkiezingen" });
   }
 
   async clickElection(electionLocation: string, electionName: string) {

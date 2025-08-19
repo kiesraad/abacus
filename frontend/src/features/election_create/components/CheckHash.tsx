c5e179d58511f0deed64207aec3371277063f239
Bram Tweedegolf <bram@tweedegolf.com>
2025-08-19 11:07:43 +0200

    Apply form updates to checkhash

--- frontend/src/features/election_create/components/CheckHash.tsx
+++ frontend/src/features/election_create/components/CheckHash.tsx
@@ -1,11 +1,14 @@
-import { FormEvent, ReactNode, useState } from "react";
+import { FormEvent, ReactNode, useEffect, useState } from "react";
 
 import { Alert } from "@/components/ui/Alert/Alert";
 import { Button } from "@/components/ui/Button/Button";
 import { Form } from "@/components/ui/Form/Form";
+import { FormLayout } from "@/components/ui/Form/FormLayout";
 import { InputField } from "@/components/ui/InputField/InputField";
+import { KeyboardKeys } from "@/components/ui/KeyboardKeys/KeyboardKeys";
 import { t } from "@/i18n/translate";
 import { RedactedEmlHash } from "@/types/generated/openapi";
+import { KeyboardKey } from "@/types/ui";
 import { formatFullDateWithoutTimezone } from "@/utils/dateTime";
 
 import { RedactedHash, Stub } from "./RedactedHash";
@@ -17,10 +20,20 @@ interface CheckHashProps {
   description: ReactNode;
   redactedHash: RedactedEmlHash;
   error: ReactNode | undefined;
+  setError: () => void;
   onSubmit: (chunks: string[]) => void;
 }
 
-export function CheckHash({ date, title, header, description, redactedHash, error, onSubmit }: CheckHashProps) {
+export function CheckHash({
+  date,
+  title,
+  header,
+  description,
+  redactedHash,
+  error,
+  setError,
+  onSubmit,
+}: CheckHashProps) {
   const [stubs, setStubs] = useState<Stub[]>(
     redactedHash.redacted_indexes.map((redacted_index: number) => ({
       selected: false,
@@ -34,8 +47,12 @@ export function CheckHash({ date, title, header, description, redactedHash, erro
     const stub = newStubs[stubIndex];
     if (stub) {
       stub.selected = highlight;
+      stub.error = "";
     }
     setStubs(newStubs);
+    if (!highlight) {
+      setError(undefined);
+    }
   }
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
@@ -47,10 +64,13 @@ export function CheckHash({ date, title, header, description, redactedHash, erro
       const value = formData.get(stub.index.toString());
       const newStubs = [...stubs];
       const newStub = newStubs[i];
+
       if (newStub) {
         newStub.error = "";
         if (typeof value !== "string" || value.length !== 4) {
           newStub.error = t("election.check_eml.check_hash.hint");
+        } else if (error) {
+          newStub.error = t("error.api_error.InvalidHash");
         } else {
           completeHash[stub.index] = value;
         }
@@ -64,53 +84,72 @@ export function CheckHash({ date, title, header, description, redactedHash, erro
     }
   }
 
+  // If there is an error, add error to stubs
+  useEffect(() => {
+    setStubs([
+      ...stubs.map((stub) => {
+        stub.error = error ? t("error.api_error.InvalidHash") : "";
+        return stub;
+      }),
+    ]);
+  }, [error]);
+
   return (
     <section className="md">
-      <h2>{header}</h2>
-      {(stubs.some((stub) => stub.error.length > 0) || error) && (
-        <Alert type="error" title={t("election.check_eml.error.title")} inline>
-          <p> {t("election.check_eml.error.description")} </p>
-        </Alert>
-      )}
-      <p className="mt-lg">{description}</p>
-      <Alert type="notify" variant="no-icon" margin="mb-md" small>
-        <p>
-          <strong>{title}</strong>
-          <br />
-          <span className="capitalize-first">{formatFullDateWithoutTimezone(new Date(date))}</span>
-        </p>
-        <div>
-          <span>
-            <strong>{t("digital_signature")}</strong> ({t("hashcode")}):
-          </span>
-          <RedactedHash hash={redactedHash.chunks} stubs={stubs} />
-        </div>
-      </Alert>
-      <p>{t("election.check_eml.check_hash.description")}</p>
-      <Form onSubmit={handleSubmit}>
-        {stubs.map((stub, stubIndex) => (
-          <InputField
-            key={stub.index}
-            name={stub.index.toString()}
-            type="text"
-            label={t("election.check_eml.check_hash.label", { stub: stubIndex + 1 })}
-            hint={t("election.check_eml.check_hash.hint")}
-            error={stub.error}
-            fieldSize="medium"
-            fieldWidth="narrow"
-            margin="mb-md"
-            onFocus={() => {
-              highlightStub(stubIndex, true);
-            }}
-            onBlur={() => {
-              highlightStub(stubIndex, false);
-            }}
-            autoFocus={stubIndex === 0}
-          />
-        ))}
-        <div className="mt-lg">
-          <Button type="submit">{t("next")}</Button>
-        </div>
+      <Form title={header} onSubmit={handleSubmit}>
+        <FormLayout>
+          <FormLayout.Section>
+            <FormLayout.Alert>
+              {(stubs.some((stub) => stub.error.length > 0) || error) && (
+                <Alert type="error" title={t("election.check_eml.error.title")} inline>
+                  <p> {t("election.check_eml.error.description")} </p>
+                </Alert>
+              )}
+            </FormLayout.Alert>
+            <p>{description}</p>
+            <FormLayout.Alert>
+              <Alert type="notify" variant="no-icon" margin="mb-md" small>
+                <p>
+                  <strong>{title}</strong>
+                  <br />
+                  <span className="capitalize-first">{formatFullDateWithoutTimezone(new Date(date))}</span>
+                </p>
+                <div>
+                  <span>
+                    <strong>{t("digital_signature")}</strong> ({t("hashcode")}):
+                  </span>
+                  <RedactedHash hash={redactedHash.chunks} stubs={stubs} />
+                </div>
+              </Alert>
+            </FormLayout.Alert>
+            <p>{t("election.check_eml.check_hash.description")}</p>
+
+            {stubs.map((stub, stubIndex) => (
+              <InputField
+                key={stub.index}
+                name={stub.index.toString()}
+                type="text"
+                label={t("election.check_eml.check_hash.label", { stub: stubIndex + 1 })}
+                hint={t("election.check_eml.check_hash.hint")}
+                error={stub.error}
+                fieldSize="medium"
+                fieldWidth="narrow"
+                onFocus={() => {
+                  highlightStub(stubIndex, true);
+                }}
+                onBlur={() => {
+                  highlightStub(stubIndex, false);
+                }}
+                autoFocus={stubIndex === 0}
+              />
+            ))}
+          </FormLayout.Section>
+
+          <FormLayout.Controls>
+            <Button type="submit">{t("next")}</Button>
+            <KeyboardKeys keys={[KeyboardKey.Shift, KeyboardKey.Enter]} />
+          </FormLayout.Controls>
+        </FormLayout>
       </Form>
     </section>
   );

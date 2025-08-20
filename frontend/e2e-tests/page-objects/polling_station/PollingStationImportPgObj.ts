9defc8f7883884aa172e331a1c0e5ac57ed3ce6a
Bram <bramtweedegolf@users.noreply.github.com>
2025-08-20 15:06:28 +0200

    Coordinator polling station import frontend (#1963)
    Co-authored-by: Oliver <oliver@sensibly.nl>
    Co-authored-by: Mark Janssen <20283+praseodym@users.noreply.github.com>

---
+++ frontend/e2e-tests/page-objects/polling_station/PollingStationImportPgObj.ts
@@ -0,0 +1,22 @@
+import { type Locator, type Page } from "@playwright/test";
+
+export class PollingStationImportPgObj {
+  readonly header: Locator;
+  readonly error: Locator;
+  readonly upload: Locator;
+  readonly importButton: Locator;
+
+  constructor(protected readonly page: Page) {
+    this.header = page.getByRole("heading", { level: 1, name: "Stembureaus importeren" });
+    this.importButton = page.getByRole("button", { name: "Stembureaus importeren" });
+    this.error = page.getByRole("strong").filter({ hasText: "Ongeldig stembureaubestand" });
+    this.upload = page.getByRole("button", { name: "Bestand kiezen" });
+  }
+
+  async uploadFile(page: Page, path: string) {
+    const fileChooserPromise = page.waitForEvent("filechooser");
+    await page.getByText("Bestand kiezen").click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path);
+  }
+}
--- frontend/e2e-tests/page-objects/polling_station/PollingStationListEmptyPgObj.ts
+++ frontend/e2e-tests/page-objects/polling_station/PollingStationListEmptyPgObj.ts
@@ -3,9 +3,11 @@ import { type Locator, type Page } from "@playwright/test";
 export class PollingStationListEmptyPgObj {
   readonly alert: Locator;
   readonly createPollingStation: Locator;
+  readonly importButton: Locator;
 
   constructor(protected readonly page: Page) {
     this.alert = page.locator("role=alert");
     this.createPollingStation = page.getByRole("link", { name: "Handmatig invullen" });
+    this.importButton = page.getByRole("link", { name: /Importeren uit een bestand/ });
   }
 }

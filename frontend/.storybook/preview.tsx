dcef0dd5f4917a5675afed575e99121520bf5f9c
Mark Janssen <20283+praseodym@users.noreply.github.com>
2025-09-02 10:08:52 +0200

    Configure `testIdAttribute` for Storybook tests (#2084)
    Co-authored-by: Oliver <oliver@sensibly.nl>

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 import { StaticRouter } from "react-router";
 
 import type { Preview } from "@storybook/react-vite";
+import { configure } from "storybook/test";
 
 import { ApiResponseStatus } from "@/api/ApiResult";
 import { ElectionProviderContext } from "@/hooks/election/ElectionProviderContext";
@@ -13,6 +14,11 @@ import { TestUserProvider } from "@/testing/TestUserProvider";
 import { ElectionDetailsResponse, ElectionWithPoliticalGroups, PollingStation, Role } from "@/types/generated/openapi";
 
 const preview: Preview = {
+  beforeAll: () => {
+    configure({
+      testIdAttribute: "id",
+    });
+  },
   parameters: {
     controls: {
       matchers: {

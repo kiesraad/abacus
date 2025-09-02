d7f6b27f3801a0591cc9e326e5aef6da145120a5
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-09-02 11:16:08 +0200

    removes unused file parameter from FileInput

--- frontend/src/components/ui/FileInput/FileInput.tsx
+++ frontend/src/components/ui/FileInput/FileInput.tsx
@@ -5,11 +5,7 @@ import cls from "./FileInput.module.css";
 
 type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-interface FileInputProps extends InputProps {
-  file?: File | undefined;
-}
-
-export function FileInput({ id, children, file, ...props }: FileInputProps) {
+export function FileInput({ id, children, ...props }: InputProps) {
   return (
     <span>
       <div className={cls.fileInputBtn}>
@@ -17,7 +13,7 @@ export function FileInput({ id, children, file, ...props }: FileInputProps) {
         <input {...props} id={id} type="file" name={props.name || id} className={cls.fileInput} />
       </div>
       <label htmlFor={id} className={cls.selectedFile}>
-        {file?.name || t("no_file_chosen")}
+        {t("no_file_chosen")}
       </label>
     </span>
   );

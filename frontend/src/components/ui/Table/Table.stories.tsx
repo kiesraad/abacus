7af0d58f07386c541f127c14f23639fb965b756f
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2025-09-09 15:24:33 +0200

    adds more Table storybook tests

--- frontend/src/components/ui/Table/Table.stories.tsx
+++ frontend/src/components/ui/Table/Table.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryFn, StoryObj } from "@storybook/react-vite";
-import { expect } from "storybook/test";
+import { expect, fn, userEvent, within } from "storybook/test";
 
 import { IconCheckHeart } from "@/components/generated/icons";
 import { Fraction } from "@/types/generated/openapi";
@@ -7,6 +7,16 @@ import { Fraction } from "@/types/generated/openapi";
 import { Badge } from "../Badge/Badge";
 import { Icon } from "../Icon/Icon";
 import { Table } from "./Table";
+
+type Props = {
+  onClick: () => void;
+};
+
+export default {
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<Props>;
 
 const data: [number, string, string][] = [
   [1, "some", "value"],
@@ -37,7 +47,6 @@ export const BasicTable: StoryObj = {
   },
   play: async ({ canvas }) => {
     const table = canvas.getByRole("table");
-    await expect(table).toHaveTextContent("Large ");
     await expect(table).toHaveTableContent([
       ["Large font", "Fixed width", "Some value"],
       ["Element 1", "some", "value"],
@@ -95,153 +104,248 @@ export const InformationTable: StoryFn = () => (
   </div>
 );
 
-export const StyledTable: StoryFn = () => (
-  <Table id="styled_table">
-    <Table.Header className="bg-gray">
-      <Table.HeaderCell>Nummer</Table.HeaderCell>
-      <Table.HeaderCell>Stembureau</Table.HeaderCell>
-    </Table.Header>
-    <Table.Body className="fs-sm">
-      <Table.Row>
-        <Table.NumberCell className="bg-gray">33</Table.NumberCell>
-        <Table.Cell>
-          <span>Op rolletjes</span>
-          <Badge type="first_entry_in_progress" />
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.NumberCell className="bg-gray">34</Table.NumberCell>
-        <Table.Cell>
-          <span>Testplek</span>
-          <Badge type="first_entry_not_started" />
-        </Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-);
-
-export const LinkTable: StoryFn = () => {
-  return (
-    <Table id="link_table">
-      <Table.Header>
-        <Table.HeaderCell>Number</Table.HeaderCell>
-        <Table.HeaderCell>Click me</Table.HeaderCell>
-        <Table.HeaderCell>Look a chevron right of here</Table.HeaderCell>
-      </Table.Header>
-      <Table.Body className="fs-md">
-        {data.map((row) => (
-          <Table.LinkRow key={row[0]} to={`#row${row[0]}`}>
-            <Table.NumberCell>{row[0]}</Table.NumberCell>
-            <Table.Cell>{row[1]}</Table.Cell>
-            <Table.Cell>{row[2]}</Table.Cell>
-          </Table.LinkRow>
-        ))}
-      </Table.Body>
-    </Table>
-  );
-};
-
-export const TotalTableWithFractions: StoryFn = () => {
-  const data: [number, string, Fraction, number][] = [
-    [1, "Political Group A", { integer: 3, numerator: 149, denominator: 150 }, 15],
-    [2, "Political Group B", { integer: 5, numerator: 0, denominator: 1 }, 11],
-    [3, "Political Group C", { integer: 8, numerator: 1, denominator: 150 }, 4],
-  ];
-  let total = 0;
-  return (
-    <>
-      <h2>Fractions</h2>
-      <Table id="total_table_with_fractions">
-        <Table.Header>
-          <Table.HeaderCell className="text-align-r">Number</Table.HeaderCell>
-          <Table.HeaderCell className="w-full">List name</Table.HeaderCell>
-          <Table.HeaderCell span={2} className="text-align-r">
-            Fractions
-          </Table.HeaderCell>
-          <Table.HeaderCell className="text-align-r">Seats</Table.HeaderCell>
+export const StyledTable: StoryObj = {
+  render: () => {
+    return (
+      <Table id="styled_table">
+        <Table.Header className="bg-gray">
+          <Table.HeaderCell>Nummer</Table.HeaderCell>
+          <Table.HeaderCell>Stembureau</Table.HeaderCell>
         </Table.Header>
-        <Table.Body>
-          {data.map((row) => {
-            total += row[3];
-            return (
-              <Table.Row key={row[0]}>
-                <Table.NumberCell>{row[0]}</Table.NumberCell>
-                <Table.Cell>{row[1]}</Table.Cell>
-                <Table.DisplayFractionCells>{row[2]}</Table.DisplayFractionCells>
-                <Table.NumberCell>{row[3]}</Table.NumberCell>
-              </Table.Row>
-            );
-          })}
-          <Table.TotalRow>
-            <Table.Cell />
-            <Table.Cell />
-            <Table.Cell colSpan={2} className="text-align-r">
-              Total
+        <Table.Body className="fs-sm">
+          <Table.Row>
+            <Table.NumberCell className="bg-gray">33</Table.NumberCell>
+            <Table.Cell>
+              <span>Op rolletjes</span>
+              <Badge type="first_entry_in_progress" />
             </Table.Cell>
-            <Table.NumberCell>{total}</Table.NumberCell>
-          </Table.TotalRow>
+          </Table.Row>
+          <Table.Row>
+            <Table.NumberCell className="bg-gray">34</Table.NumberCell>
+            <Table.Cell>
+              <span>Testplek</span>
+              <Badge type="first_entry_not_started" />
+            </Table.Cell>
+          </Table.Row>
         </Table.Body>
       </Table>
-
-      <h2 className="mt-lg">Fractions with only whole numbers</h2>
-      <Table id="total_table_with_fractions_whole_numbers">
-        <Table.Header>
-          <Table.HeaderCell className="text-align-r">Number</Table.HeaderCell>
-          <Table.HeaderCell className="w-full">List name</Table.HeaderCell>
-          <Table.HeaderCell span={2} className="text-align-r">
-            Fractions
-          </Table.HeaderCell>
-          <Table.HeaderCell className="text-align-r">Seats</Table.HeaderCell>
-        </Table.Header>
-        <Table.Body>
-          {data.map((row) => {
-            total += row[3];
-            return (
-              <Table.Row key={row[0]}>
-                <Table.NumberCell>{row[0]}</Table.NumberCell>
-                <Table.Cell>{row[1]}</Table.Cell>
-                <Table.DisplayFractionCells>{{ ...row[2], numerator: 0, denominator: 0 }}</Table.DisplayFractionCells>
-                <Table.NumberCell>{row[3]}</Table.NumberCell>
-              </Table.Row>
-            );
-          })}
-          <Table.TotalRow>
-            <Table.Cell />
-            <Table.Cell />
-            <Table.Cell colSpan={2} className="text-align-r">
-              Total
-            </Table.Cell>
-            <Table.NumberCell>{total}</Table.NumberCell>
-          </Table.TotalRow>
-        </Table.Body>
-      </Table>
-    </>
-  );
+    );
+  },
+  play: async ({ canvas }) => {
+    const table = canvas.getByRole("table");
+    await expect(table).toHaveTableContent([
+      ["Nummer", "Stembureau"],
+      ["33", "Op rolletjes 1e invoer"],
+      ["34", "Testplek 1e invoer"],
+    ]);
+  },
 };
 
-export const IconBadgeTable: StoryFn = () => (
-  <Table id="icon_badge_table">
-    <Table.Header>
-      <Table.HeaderCell>Number</Table.HeaderCell>
-      <Table.HeaderCell>With icon</Table.HeaderCell>
-      <Table.HeaderCell>With badge</Table.HeaderCell>
-    </Table.Header>
-    <Table.Body>
-      {data.map((row) => (
-        <Table.LinkRow key={row[0]} to={`#row${row[0]}`}>
-          <Table.NumberCell>{row[0]}</Table.NumberCell>
-          <Table.Cell>
-            <Icon icon={<IconCheckHeart />} color="accept" />
-            <span>{row[1]}</span>
-          </Table.Cell>
-          <Table.Cell>
-            <span>{row[2]}</span>
-            <Badge type="first_entry_in_progress" showIcon />
-          </Table.Cell>
-        </Table.LinkRow>
-      ))}
-    </Table.Body>
-  </Table>
-);
+export const ClickTable: StoryObj<Props> = {
+  render: ({ onClick }) => {
+    return (
+      <Table id="link_table">
+        <Table.Header>
+          <Table.HeaderCell>Number</Table.HeaderCell>
+          <Table.HeaderCell>Click me</Table.HeaderCell>
+          <Table.HeaderCell>Look a chevron right of here</Table.HeaderCell>
+        </Table.Header>
+        <Table.Body className="fs-md">
+          {data.map((row) => (
+            <Table.ClickRow key={row[0]} onClick={onClick}>
+              <Table.NumberCell>{row[0]}</Table.NumberCell>
+              <Table.Cell>{row[1]}</Table.Cell>
+              <Table.Cell>{row[2]}</Table.Cell>
+            </Table.ClickRow>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  },
+  play: async ({ args, canvas }) => {
+    const table = canvas.getByRole("table");
+    await expect(table).toHaveTableContent([
+      ["Number", "Click me", "Look a chevron right of here"],
+      ["1", "some", "value"],
+      ["2", "other", "value"],
+      ["3", "another", "thing"],
+    ]);
 
-export default {} satisfies Meta;
+    const rows = within(table).getAllByRole("row");
+    await userEvent.click(rows[1]!);
+    await expect(args.onClick).toHaveBeenCalled();
+  },
+};
+
+export const LinkTable: StoryObj = {
+  render: () => {
+    return (
+      <Table id="link_table">
+        <Table.Header>
+          <Table.HeaderCell>Number</Table.HeaderCell>
+          <Table.HeaderCell>Click me</Table.HeaderCell>
+          <Table.HeaderCell>Look a chevron right of here</Table.HeaderCell>
+        </Table.Header>
+        <Table.Body className="fs-md">
+          {data.map((row) => (
+            <Table.LinkRow key={row[0]} to={`#row${row[0]}`}>
+              <Table.NumberCell>{row[0]}</Table.NumberCell>
+              <Table.Cell>{row[1]}</Table.Cell>
+              <Table.Cell>{row[2]}</Table.Cell>
+            </Table.LinkRow>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  },
+  play: async ({ canvas }) => {
+    const table = canvas.getByRole("table");
+    await expect(table).toHaveTableContent([
+      ["Number", "Click me", "Look a chevron right of here"],
+      ["1", "some", "value"],
+      ["2", "other", "value"],
+      ["3", "another", "thing"],
+    ]);
+
+    const rows = within(table).getAllByRole("row");
+    await userEvent.click(rows[1]!);
+  },
+};
+
+export const TotalTableWithFractions: StoryObj = {
+  render: () => {
+    const data: [number, string, Fraction, number][] = [
+      [1, "Political Group A", { integer: 3, numerator: 149, denominator: 150 }, 15],
+      [2, "Political Group B", { integer: 5, numerator: 0, denominator: 1 }, 11],
+      [3, "Political Group C", { integer: 8, numerator: 1, denominator: 150 }, 4],
+    ];
+    let total = 0;
+    return (
+      <>
+        <h2>Fractions</h2>
+        <Table id="total_table_with_fractions">
+          <Table.Header>
+            <Table.HeaderCell className="text-align-r">Number</Table.HeaderCell>
+            <Table.HeaderCell className="w-full">List name</Table.HeaderCell>
+            <Table.HeaderCell span={2} className="text-align-r">
+              Fractions
+            </Table.HeaderCell>
+            <Table.HeaderCell className="text-align-r">Seats</Table.HeaderCell>
+          </Table.Header>
+          <Table.Body>
+            {data.map((row) => {
+              total += row[3];
+              return (
+                <Table.Row key={row[0]}>
+                  <Table.NumberCell>{row[0]}</Table.NumberCell>
+                  <Table.Cell>{row[1]}</Table.Cell>
+                  <Table.DisplayFractionCells>{row[2]}</Table.DisplayFractionCells>
+                  <Table.NumberCell>{row[3]}</Table.NumberCell>
+                </Table.Row>
+              );
+            })}
+            <Table.TotalRow>
+              <Table.Cell />
+              <Table.Cell />
+              <Table.Cell colSpan={2} className="text-align-r">
+                Total
+              </Table.Cell>
+              <Table.NumberCell>{total}</Table.NumberCell>
+            </Table.TotalRow>
+          </Table.Body>
+        </Table>
+
+        <h2 className="mt-lg">Fractions with only whole numbers</h2>
+        <Table id="total_table_with_fractions_whole_numbers">
+          <Table.Header>
+            <Table.HeaderCell className="text-align-r">Number</Table.HeaderCell>
+            <Table.HeaderCell className="w-full">List name</Table.HeaderCell>
+            <Table.HeaderCell span={2} className="text-align-r">
+              Fractions
+            </Table.HeaderCell>
+            <Table.HeaderCell className="text-align-r">Seats</Table.HeaderCell>
+          </Table.Header>
+          <Table.Body>
+            {data.map((row) => {
+              total += row[3];
+              return (
+                <Table.Row key={row[0]}>
+                  <Table.NumberCell>{row[0]}</Table.NumberCell>
+                  <Table.Cell>{row[1]}</Table.Cell>
+                  <Table.DisplayFractionCells>{{ ...row[2], numerator: 0, denominator: 0 }}</Table.DisplayFractionCells>
+                  <Table.NumberCell>{row[3]}</Table.NumberCell>
+                </Table.Row>
+              );
+            })}
+            <Table.TotalRow>
+              <Table.Cell />
+              <Table.Cell />
+              <Table.Cell colSpan={2} className="text-align-r">
+                Total
+              </Table.Cell>
+              <Table.NumberCell>{total}</Table.NumberCell>
+            </Table.TotalRow>
+          </Table.Body>
+        </Table>
+      </>
+    );
+  },
+  play: async ({ canvas }) => {
+    const fractionsTable = canvas.getByTestId("total_table_with_fractions");
+    await expect(fractionsTable).toHaveTableContent([
+      ["Number", "List name", "Fractions", "Seats"],
+      ["1", "Political Group A", "3", "149/150", "15"],
+      ["2", "Political Group B", "5", "", "11"],
+      ["3", "Political Group C", "8", "1/150", "4"],
+      ["", "", "Total", "30"],
+    ]);
+    const wholeNumbersTable = canvas.getByTestId("total_table_with_fractions_whole_numbers");
+    await expect(wholeNumbersTable).toHaveTableContent([
+      ["Number", "List name", "Fractions", "Seats"],
+      ["1", "Political Group A", "3", "", "15"],
+      ["2", "Political Group B", "5", "", "11"],
+      ["3", "Political Group C", "8", "", "4"],
+      ["", "", "Total", "60"],
+    ]);
+  },
+};
+
+export const IconBadgeTable: StoryObj = {
+  render: () => {
+    return (
+      <Table id="icon_badge_table">
+        <Table.Header>
+          <Table.HeaderCell>Number</Table.HeaderCell>
+          <Table.HeaderCell>With icon</Table.HeaderCell>
+          <Table.HeaderCell>With badge</Table.HeaderCell>
+        </Table.Header>
+        <Table.Body>
+          {data.map((row) => (
+            <Table.LinkRow key={row[0]} to={`#row${row[0]}`}>
+              <Table.NumberCell>{row[0]}</Table.NumberCell>
+              <Table.Cell>
+                <Icon icon={<IconCheckHeart />} color="accept" />
+                <span>{row[1]}</span>
+              </Table.Cell>
+              <Table.Cell>
+                <span>{row[2]}</span>
+                <Badge type="first_entry_in_progress" showIcon />
+              </Table.Cell>
+            </Table.LinkRow>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  },
+  play: async ({ canvas }) => {
+    const table = canvas.getByRole("table");
+    await expect(table).toHaveTableContent([
+      ["Number", "With icon", "With badge"],
+      ["1", "some", "value 1e invoer"],
+      ["2", "other", "value 1e invoer"],
+      ["3", "another", "thing 1e invoer"],
+    ]);
+  },
+};
+
+// export default {} satisfies Meta;

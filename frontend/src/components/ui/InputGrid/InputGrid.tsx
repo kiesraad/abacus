8405fd791e8432adfef933425c56efaf6f5d7851
Oliver <oliver@sensibly.nl>
2025-09-15 11:39:29 +0200

    Show previous values column if there are previous values

--- frontend/src/components/ui/InputGrid/InputGrid.tsx
+++ frontend/src/components/ui/InputGrid/InputGrid.tsx
@@ -4,38 +4,48 @@ import { cn } from "@/utils/classnames";
 
 import cls from "./InputGrid.module.css";
 
-export type InputGridRowCells = [ReactNode, ReactNode, ReactNode];
-
 export interface InputGridProps {
+  id?: string;
   zebra?: boolean;
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
-export function InputGrid({ zebra, children }: InputGridProps) {
+export function InputGrid({ id, zebra, children }: InputGridProps) {
   const ref = useRef<HTMLTableElement>(null);
 
   return (
-    <table role="none" ref={ref} className={cn(cls.inputGrid, zebra && cls.zebra)}>
+    <table id={id} role="none" ref={ref} className={cn(cls.inputGrid, zebra && cls.zebra)}>
       {children}
     </table>
   );
 }
 
-InputGrid.Header = ({ field, value, title }: { field: string; value: string; title: string }) => (
+InputGrid.Header = ({
+  field,
+  previous,
+  value,
+  title,
+}: {
+  field: string;
+  previous?: string;
+  value: string;
+  title: string;
+}) => (
   <thead>
     <tr>
       <th className={cls.field}>{field}</th>
+      {previous && <th className={cls.previous}>{previous}</th>}
       <th className={cls.value}>{value}</th>
       <th className={cls.title}>{title}</th>
     </tr>
   </thead>
 );
 
-InputGrid.Body = ({ children }: { children: React.ReactNode }) => <tbody>{children}</tbody>;
+InputGrid.Body = ({ children }: { children: ReactNode }) => <tbody>{children}</tbody>;
 
-InputGrid.Separator = () => (
+InputGrid.Separator = ({ showPrevious }: { showPrevious?: boolean }) => (
   <tr className={cls.separator}>
-    <td colSpan={3} />
+    <td colSpan={showPrevious ? 4 : 3} />
   </tr>
 );
 
@@ -44,24 +54,35 @@ InputGrid.Row = ({
   isTotal,
   addSeparator,
   id,
+  showPrevious,
 }: {
-  children: InputGridRowCells;
+  children: ReactNode;
   isTotal?: boolean;
   addSeparator?: boolean;
   id?: string;
+  showPrevious?: boolean;
 }) => (
   <>
     <tr className={cn(isTotal && cls.total)} id={`row-${id}`}>
       {children}
     </tr>
-    {addSeparator && <InputGrid.Separator />}
+    {addSeparator && <InputGrid.Separator showPrevious={showPrevious} />}
   </>
 );
 
-InputGrid.ListTotal = ({ children, id }: { children: InputGridRowCells; id?: string }) => (
+InputGrid.ListTotal = ({
+  children,
+  id,
+  showPrevious,
+}: {
+  children: ReactNode;
+  id?: string;
+  showPrevious?: boolean;
+}) => (
   <>
     <tr className={cls.totalSeparator}>
       <td className={cls.field}></td>
+      {showPrevious && <td className={cls.previous}></td>}
       <td className={cls.value}></td>
       <td className={cls.title}></td>
     </tr>

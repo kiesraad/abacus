10bb75a15d11372039a2687862de340cf1875d95
Bram Tweedegolf <bram@tweedegolf.com>
2025-09-15 14:00:28 +0200

    Remove election tree parsing and usage from eml

--- backend/tests/election_admin_integration_tests.rs
+++ backend/tests/election_admin_integration_tests.rs
@@ -72,26 +72,6 @@ async fn test_election_validate_invalid_election_number_of_seats(pool: SqlitePoo
 }
 
 #[test(sqlx::test(fixtures(path = "../fixtures", scripts("users"))))]
-async fn test_election_validate_invalid_election_missing_region(pool: SqlitePool) {
-    let addr = serve_api(pool).await;
-
-    let url = format!("http://{addr}/api/elections/import/validate");
-    let admin_cookie = shared::admin_login(&addr).await;
-    let response = reqwest::Client::new()
-        .post(&url)
-        .header("cookie", admin_cookie)
-        .json(&serde_json::json!({
-          "election_data": include_str!("../src/eml/tests/eml110a_invalid_election_missing_region.eml.xml"),
-        }))
-        .send()
-        .await
-        .unwrap();
-
-    // Ensure the response is what we expect
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
-}
-
-#[test(sqlx::test(fixtures(path = "../fixtures", scripts("users"))))]
 async fn test_election_validate_invalid_xml(pool: SqlitePool) {
     let addr = serve_api(pool).await;
 

698df027a1983602ec2b7c02a9779cec97b10fbf
cikzh <marlonpeeters@tweedegolf.com>
2025-09-16 14:10:00 +0200

    Added test for double investigations

--- backend/tests/investigation_integration_test.rs
+++ backend/tests/investigation_integration_test.rs
@@ -1,5 +1,6 @@
 #![cfg(test)]
 use hyper::StatusCode;
+use reqwest::Response;
 use serde_json::json;
 use sqlx::SqlitePool;
 use test_log::test;
@@ -9,61 +10,70 @@ use crate::utils::serve_api;
 pub mod shared;
 pub mod utils;
 
-#[test(sqlx::test(fixtures(path = "../fixtures", scripts("election_7_four_sessions", "users"))))]
-async fn test_investigation_creation_and_conlusion(pool: SqlitePool) {
+async fn create_investigation(pool: SqlitePool, polling_station_id: u32) -> Response {
     let addr = serve_api(pool).await;
-    let url = format!("http://{addr}/api/polling_stations/741/investigations");
+    let url = format!("http://{addr}/api/polling_stations/{polling_station_id}/investigations");
     let coordinator_cookie = shared::coordinator_login(&addr).await;
     let body = json!({
         "reason": "Test reason"
     });
-    let response = reqwest::Client::new()
+    reqwest::Client::new()
         .post(&url)
         .header("cookie", coordinator_cookie.clone())
         .header("Content-Type", "application/json")
         .body(body.to_string())
         .send()
         .await
-        .unwrap();
+        .unwrap()
+}
 
-    assert_eq!(response.status(), StatusCode::OK);
-
+async fn conclude_investigation(pool: SqlitePool, polling_station_id: u32) -> Response {
+    let addr = serve_api(pool).await;
+    let coordinator_cookie = shared::coordinator_login(&addr).await;
     let body = json!({
         "findings": "Test findings",
         "corrected_results": false
     });
-    let url = format!("http://{addr}/api/polling_stations/741/investigations");
-    let response = reqwest::Client::new()
+    let url = format!("http://{addr}/api/polling_stations/{polling_station_id}/investigations");
+    reqwest::Client::new()
         .put(&url)
         .header("cookie", coordinator_cookie)
         .header("Content-Type", "application/json")
         .body(body.to_string())
         .send()
         .await
-        .unwrap();
+        .unwrap()
+}
 
-    assert_eq!(response.status(), StatusCode::OK);
+#[test(sqlx::test(fixtures(path = "../fixtures", scripts("election_7_four_sessions", "users"))))]
+async fn test_investigation_creation_and_conlusion(pool: SqlitePool) {
+    assert_eq!(
+        create_investigation(pool.clone(), 741).await.status(),
+        StatusCode::OK
+    );
+    assert_eq!(
+        conclude_investigation(pool.clone(), 741).await.status(),
+        StatusCode::OK
+    );
 }
 
 #[test(sqlx::test(fixtures(path = "../fixtures", scripts("election_7_four_sessions", "users"))))]
 async fn test_investigation_creation_fails_for_wrong_polling_station(pool: SqlitePool) {
-    let addr = serve_api(pool).await;
-
     // 732 is an  existing polling station, but in the wrong committee session
-    let url = format!("http://{addr}/api/polling_stations/732/investigations");
-    let coordinator_cookie = shared::coordinator_login(&addr).await;
-    let body = json!({
-        "reason": "Test reason"
-    });
+    assert_eq!(
+        create_investigation(pool.clone(), 732).await.status(),
+        StatusCode::NOT_FOUND
+    );
+}
 
-    let response = reqwest::Client::new()
-        .post(&url)
-        .header("cookie", coordinator_cookie.clone())
-        .header("Content-Type", "application/json")
-        .body(body.to_string())
-        .send()
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+#[test(sqlx::test(fixtures(path = "../fixtures", scripts("election_7_four_sessions", "users"))))]
+async fn test_investigation_creation_fails_on_creating_second_investigation(pool: SqlitePool) {
+    assert_eq!(
+        create_investigation(pool.clone(), 741).await.status(),
+        StatusCode::OK
+    );
+    assert_eq!(
+        create_investigation(pool.clone(), 741).await.status(),
+        StatusCode::CONFLICT
+    );
 }

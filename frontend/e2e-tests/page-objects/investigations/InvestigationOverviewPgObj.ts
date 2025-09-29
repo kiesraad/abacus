fab3c9caee3b6818ef43b82fab1afdb8b5dcd475
Marlon Baeten <marlon@tweedegolf.com>
2025-09-29 10:33:09 +0200

    Split create and finish investigation in full flow test

--- frontend/e2e-tests/page-objects/investigations/InvestigationOverviewPgObj.ts
+++ frontend/e2e-tests/page-objects/investigations/InvestigationOverviewPgObj.ts
@@ -1,9 +1,17 @@
 import { type Locator, type Page } from "@playwright/test";
 
 export class InvestigationOverviewPgObj {
+  readonly header: Locator;
   readonly addInvestigationButton: Locator;
+  readonly fillInvestigationLink: Locator;
 
   constructor(protected readonly page: Page) {
+    this.header = page.getByRole("heading", { level: 1 });
     this.addInvestigationButton = page.getByRole("link", { name: "Onderzoek toevoegen" });
+    this.fillInvestigationLink = page.getByRole("link", { name: "Nu invullen" });
+  }
+
+  findInvestigationEditLinkByPollingStation(number: string) {
+    return this.page.getByTestId(`investigation-${number}`).getByRole("link", { name: /Bewerken|Nu invullen/ });
   }
 }

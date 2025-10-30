77893d5db7b7ea76f75089f7a197fc56d754e08f
Oliver <oliver@sensibly.nl>
2025-10-30 13:09:41 +0100

    Frontend file size check for upload
    Co-authored-by: cikzh <marlonpeeters@tweedegolf.com>

--- frontend/src/features/election_create/components/UploadCandidatesDefinition.tsx
+++ frontend/src/features/election_create/components/UploadCandidatesDefinition.tsx
@@ -13,6 +13,9 @@ import { ELECTION_IMPORT_VALIDATE_REQUEST_PATH, ElectionDefinitionValidateRespon
 import { useElectionCreateContext } from "../hooks/useElectionCreateContext";
 import { CheckHash } from "./CheckHash";
 
+// Maximum file upload this for this component in Megabytes
+const MAX_FILE_UPLOAD_SIZE_MB: number = 5;
+
 export function UploadCandidatesDefinition() {
   const { state, dispatch } = useElectionCreateContext();
   const navigate = useNavigate();
@@ -29,6 +32,22 @@ export function UploadCandidatesDefinition() {
   async function onFileChange(e: ChangeEvent<HTMLInputElement>) {
     const currentFile = e.target.files ? e.target.files[0] : undefined;
     if (currentFile !== undefined) {
+      // Get the size of the string in bytes after JSON encoding
+      const fileSize: number = new Blob([JSON.stringify(await currentFile.text())]).size;
+      if (fileSize > MAX_FILE_UPLOAD_SIZE_MB * 1024 * 1024) {
+        setError(
+          tx(
+            "file_too_large",
+            {},
+            {
+              filename: currentFile.name,
+              max_size: `${MAX_FILE_UPLOAD_SIZE_MB}`,
+            },
+          ),
+        );
+        return;
+      }
+
       setFile(currentFile);
       const data = await currentFile.text();
       const response = await create({
@@ -50,12 +69,11 @@ export function UploadCandidatesDefinition() {
         if (response instanceof ApiError && response.code === 413) {
           setError(
             tx(
-              "election.invalid_candidates_definition.file_too_large",
+              "file_too_large",
+              {},
               {
-                file: () => <strong>{currentFile.name}</strong>,
-              },
-              {
-                max_size: response.message,
+                filename: currentFile.name,
+                max_size: `${MAX_FILE_UPLOAD_SIZE_MB}`,
               },
             ),
           );

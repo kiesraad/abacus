5bb38834a6350f6b1a92fb00db399069fe390241
Marlon <marlonbaeten@users.noreply.github.com>
2025-11-04 10:01:25 +0100

    Improve the Network Error page (#2380)
    Co-authored-by: Marjolein Tamis <80948475+marjoleintamis@users.noreply.github.com>

--- frontend/src/components/error/FatalErrorPage.tsx
+++ frontend/src/components/error/FatalErrorPage.tsx
@@ -10,7 +10,7 @@ import { Error } from "./Error";
 
 interface FatalErrorPageProps {
   title?: TranslationPath;
-  message?: string;
+  message?: string | React.ReactNode;
   reference?: ErrorReference;
   code?: number;
   error?: Error;

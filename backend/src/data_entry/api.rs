4cb08790a7807dfaf004eba6b801a53c2483dfb5
Marlon Baeten <marlon@tweedegolf.com>
2025-11-11 16:58:50 +0100

    Only throw InvalidData errors in DataEntryProvider

--- backend/src/data_entry/api.rs
+++ backend/src/data_entry/api.rs
@@ -255,6 +255,7 @@ impl ResolveDifferencesAction {
         (status = 401, description = "Unauthorized", body = ErrorResponse),
         (status = 403, description = "Forbidden", body = ErrorResponse),
         (status = 409, description = "Request cannot be completed", body = ErrorResponse),
+        (status = 422, description = "Invalid data", body = ErrorResponse),
         (status = 500, description = "Internal server error", body = ErrorResponse),
     ),
     params(

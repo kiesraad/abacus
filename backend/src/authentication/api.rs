cd8cba850825fc856ac15eeb378c3bb02ed79da3
Oliver <oliver@sensibly.nl>
2025-12-10 12:08:50 +0100

    Enforce account setup in `FromRequestParts<S> for User`, add `PartialUser` as an escape for PUT /api/account

--- backend/src/authentication/api.rs
+++ backend/src/authentication/api.rs
@@ -13,7 +13,9 @@ use utoipa::ToSchema;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 use super::{
-    SECURE_COOKIES, SESSION_COOKIE_NAME, SESSION_LIFE_TIME, error::AuthenticationError, role::Role,
+    SECURE_COOKIES, SESSION_COOKIE_NAME, SESSION_LIFE_TIME,
+    error::AuthenticationError,
+    role::{PartialUser, Role},
     user::User,
 };
 use crate::{
@@ -216,7 +218,7 @@ async fn account(user: Option<User>) -> Result<impl IntoResponse, APIError> {
   security(("cookie_auth" = ["administrator", "coordinator", "typist"])),
 )]
 async fn account_update(
-    user: User,
+    PartialUser(user): PartialUser,
     State(pool): State<SqlitePool>,
     audit_service: AuditService,
     Json(account): Json<AccountUpdateRequest>,

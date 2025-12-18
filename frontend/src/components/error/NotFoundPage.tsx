59145e199b577f47c9a22ca67243c8eb35951ab6
Lionqueen94 <lionqueen94@gmail.com>
2025-12-18 11:59:26 +0100

    Fixed error pages rendering

--- frontend/src/components/error/NotFoundPage.tsx
+++ frontend/src/components/error/NotFoundPage.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/footer/Footer";
 import { NavBar } from "@/components/navbar/NavBar";
+import { AppFrame } from "@/components/ui/AppFrame/AppFrame";
 import { AppLayout } from "@/components/ui/AppLayout/AppLayout";
 import { TranslationPath } from "@/i18n/i18n.types";
 import { t, tx } from "@/i18n/translate";
@@ -14,14 +15,16 @@ export interface NotFoundPageProps {
 
 export function NotFoundPage({ message, vars, path }: NotFoundPageProps) {
   return (
-    <AppLayout>
-      {/* Show NavBar for / to avoid call to useElection outside ElectionProvider */}
-      <NavBar location={{ pathname: "/" }} />
-      <Error title={t(message, vars)}>
-        {path && <p>{tx("error.page_not_found", undefined, { path })}</p>}
-        <p>{t("error.not_found_feedback")}</p>
-      </Error>
-      <Footer />
-    </AppLayout>
+    <AppFrame>
+      <AppLayout>
+        {/* Show NavBar for / to avoid call to useElection outside ElectionProvider */}
+        <NavBar location={{ pathname: "/" }} />
+        <Error title={t(message, vars)}>
+          {path && <p>{tx("error.page_not_found", undefined, { path })}</p>}
+          <p>{t("error.not_found_feedback")}</p>
+        </Error>
+        <Footer />
+      </AppLayout>
+    </AppFrame>
   );
 }

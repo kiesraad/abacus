d4827ccfee1b5cf12efdfeab4cebb0d84129d5cc
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2026-01-08 14:46:15 +0100

    adds test for incorrect election definition hash

--- frontend/src/features/election_create/components/UploadElectionDefinition.test.tsx
+++ frontend/src/features/election_create/components/UploadElectionDefinition.test.tsx
@@ -1,10 +1,12 @@
 import { userEvent } from "@testing-library/user-event";
 import { describe, expect, test, vi } from "vitest";
+import { newElectionMockData } from "@/testing/api-mocks/ElectionMockData";
 import { overrideOnce } from "@/testing/server";
 import { render, screen } from "@/testing/test-utils";
-import type { NewElection } from "@/types/generated/openapi";
+import type { ElectionDefinitionValidateResponse, NewElection, PollingStationRequest } from "@/types/generated/openapi";
 import * as uploadFileSize from "@/utils/uploadFileSize";
 import * as useElectionCreateContext from "../hooks/useElectionCreateContext";
+import { ElectionCreateContextProvider } from "./ElectionCreateContextProvider";
 import { UploadElectionDefinition } from "./UploadElectionDefinition";
 
 async function renderPage() {
@@ -18,7 +20,77 @@ const election = { name: "Naam", location: "Plek" } as NewElection;
 const filename = "foo.txt";
 const file = new File(["foo"], filename, { type: "text/plain" });
 
+function electionValidateResponse(
+  election: NewElection,
+  polling_stations: PollingStationRequest[] | undefined = undefined,
+  matching_election: boolean | undefined = undefined,
+): ElectionDefinitionValidateResponse {
+  return {
+    hash: {
+      chunks: [
+        "asdf",
+        "qwer",
+        "",
+        "tyui",
+        "ghjk",
+        "bnml",
+        "1234",
+        "5678",
+        "8765",
+        "",
+        "a345",
+        "qwer",
+        "lgmg",
+        "thnr",
+        "nytf",
+        "sdfr",
+      ],
+      redacted_indexes: [2, 9],
+    },
+    election,
+    polling_stations,
+    number_of_voters: 0,
+    polling_station_definition_matches_election: matching_election,
+  };
+}
+
 describe("UploadElectionDefinition component", () => {
+  test("It shows error when providing incorrect hash", async () => {
+    render(
+      <ElectionCreateContextProvider>
+        <UploadElectionDefinition />
+      </ElectionCreateContextProvider>,
+    );
+
+    const user = userEvent.setup();
+
+    expect(await screen.findByRole("heading", { level: 2, name: "Importeer verkiezingsdefinitie" })).toBeVisible();
+    const input = await screen.findByLabelText("Bestand kiezen");
+    expect(input).toBeVisible();
+    expect(await screen.findByLabelText("Geen bestand gekozen")).toBeVisible();
+
+    overrideOnce("post", "/api/elections/import/validate", 200, electionValidateResponse(newElectionMockData));
+    await user.upload(input, file);
+
+    expect(screen.queryByLabelText("Geen bestand gekozen")).not.toBeInTheDocument();
+    expect(screen.getAllByText(filename).length).toBe(1);
+
+    const inputPart1 = screen.getByRole("textbox", { name: "Controle deel 1" });
+    await user.type(inputPart1, "zxcv");
+
+    const inputPart2 = screen.getByRole("textbox", { name: "Controle deel 2" });
+    await user.type(inputPart2, "gfsd");
+
+    overrideOnce("post", "/api/elections/import/validate", 400, {
+      error: "Invalid hash",
+      fatal: false,
+      reference: "InvalidHash",
+    });
+    await user.click(screen.getByRole("button", { name: "Volgende" }));
+
+    expect(await screen.findByRole("alert")).toHaveTextContent("Controle digitale vingerafdruk niet gelukt");
+  });
+
   test("It shows an error when uploading invalid file", async () => {
     const state = { election, numberOfVoters: 0 };
     const dispatch = vi.fn();

45549eef50b07eaaa36fce83fe9d7fb7c396c6be
Lionqueen94 <lionqueen94@gmail.com>
2026-01-23 21:50:40 +0100

    Moved DisplayFraction from crate to abacus

--- backend/apportionment/src/fraction.rs
+++ backend/apportionment/src/fraction.rs
@@ -7,8 +7,8 @@ use std::{
 #[derive(Clone, Copy)]
 // type invariant: denominator has to be nonzero
 pub struct Fraction {
-    numerator: u64,
-    denominator: u64,
+    pub numerator: u64,
+    pub denominator: u64,
 }
 
 impl Fraction {
@@ -146,45 +146,9 @@ impl Debug for Fraction {
     }
 }
 
-impl PartialEq for DisplayFraction {
-    fn eq(&self, other: &Self) -> bool {
-        Fraction::from(*self) == Fraction::from(*other)
-    }
-}
-
-// TODO: Move to abacus
-/// Fraction with the integer part split out for display purposes
-#[derive(Clone, Copy, Debug)]
-pub struct DisplayFraction {
-    integer: u64,
-    numerator: u64,
-    denominator: u64,
-}
-
-impl From<Fraction> for DisplayFraction {
-    fn from(fraction: Fraction) -> Self {
-        let remainder = fraction.fractional_part();
-        Self {
-            integer: fraction.integer_part(),
-            numerator: remainder.numerator,
-            denominator: remainder.denominator,
-        }
-    }
-}
-
-impl From<DisplayFraction> for Fraction {
-    fn from(display_fraction: DisplayFraction) -> Self {
-        Self {
-            numerator: display_fraction.numerator
-                + display_fraction.integer * display_fraction.denominator,
-            denominator: display_fraction.denominator,
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::{DisplayFraction, Fraction};
+    use super::Fraction;
 
     #[test]
     fn test_from_u64() {
@@ -263,63 +227,5 @@ mod tests {
     #[test]
     fn test_smaller_than() {
         assert!(Fraction::new(1, 3) < Fraction::new(1, 2));
-    }
-
-    #[test]
-    fn test_display_fraction_integer_gt_0() {
-        let fraction = DisplayFraction::from(Fraction::new(11, 5));
-        assert_eq!(
-            fraction,
-            DisplayFraction {
-                integer: 2,
-                numerator: 1,
-                denominator: 5
-            }
-        );
-    }
-
-    #[test]
-    fn test_display_fraction_integer_0() {
-        let fraction = DisplayFraction::from(Fraction::new(2, 5));
-        assert_eq!(
-            fraction,
-            DisplayFraction {
-                integer: 0,
-                numerator: 2,
-                denominator: 5
-            }
-        );
-    }
-
-    #[test]
-    fn test_display_fraction_eq() {
-        assert_eq!(
-            DisplayFraction {
-                integer: 1,
-                numerator: 1,
-                denominator: 4
-            },
-            DisplayFraction {
-                integer: 0,
-                numerator: 10,
-                denominator: 8
-            }
-        );
-    }
-
-    #[test]
-    fn test_display_fraction_ne() {
-        assert_ne!(
-            DisplayFraction {
-                integer: 1,
-                numerator: 1,
-                denominator: 4
-            },
-            DisplayFraction {
-                integer: 0,
-                numerator: 9,
-                denominator: 8
-            }
-        );
     }
 }
--- backend/src/apportionment/mod.rs
+++ backend/src/apportionment/mod.rs
@@ -1,3 +1,4 @@
 mod api;
+mod display_fraction;
 
 pub use api::*;

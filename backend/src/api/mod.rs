7b5e78c6b9078c669f7d5a87146b345b649a7723
Oliver <oliver@sensibly.nl>
2026-01-27 15:42:55 +0100

    Move document module

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -2,4 +2,5 @@ pub mod audit;
 pub mod authentication;
 pub mod committee_session;
 pub mod data_entry;
+pub mod document;
 pub mod user;
--- backend/src/document/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Document module
-//!
-//! This module creates PDF documents from typst templates
-
-mod api;
-
-pub use self::api::*;

e4400c7fad2b74a1e5818d22ccc83ef264ffcbf1
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2026-02-18 11:07:03 +0100

    renames button attribute in InitialiseWelcomePgObj to createAdminAccount

--- frontend/e2e-tests/tests/initialisation.e2e.ts
+++ frontend/e2e-tests/tests/initialisation.e2e.ts
@@ -17,7 +17,7 @@ test.describe("initialisation", () => {
 
     const welcomePage = new InitialiseWelcomePgObj(page);
     await expect(welcomePage.header).toBeVisible();
-    await welcomePage.button.click();
+    await welcomePage.createAdminAccount.click();
 
     // add the first admin
     const password = getTestPassword(firstAdmin.username);
@@ -33,6 +33,7 @@ test.describe("initialisation", () => {
     // login as first admin
     const firstLoginPage = new FirstLoginPgObj(page);
     await expect(firstLoginPage.header).toBeVisible();
+    await expect(firstLoginPage.alert).toHaveText("Account van beheerder ingesteld");
     await firstLoginPage.username.fill(firstAdmin.username);
     await firstLoginPage.password.fill(password);
     await firstLoginPage.loginBtn.click();
@@ -53,7 +54,7 @@ test.describe("initialisation", () => {
 
     const welcomePage = new InitialiseWelcomePgObj(page);
     await expect(welcomePage.header).toBeVisible();
-    await welcomePage.button.click();
+    await welcomePage.createAdminAccount.click();
 
     const password = getTestPassword(firstAdmin.username);
 

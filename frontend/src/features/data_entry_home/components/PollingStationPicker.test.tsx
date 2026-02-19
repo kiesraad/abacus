12c74efd8b7a8d005499e91c19f19697dfc03b3f
Tamara <96083100+lama-d3v@users.noreply.github.com>
2026-02-19 13:58:00 +0100

    Fixed frontend test

--- frontend/src/features/data_entry_home/components/PollingStationPicker.test.tsx
+++ frontend/src/features/data_entry_home/components/PollingStationPicker.test.tsx
@@ -248,8 +248,11 @@ describe("Test PollingStationPicker", () => {
       const pollingStation = await screen.findByTestId("pollingStation");
 
       await user.type(pollingStation, "33");
-      const pollingStationSearching = await screen.findByTestId("pollingStationNumberInputFeedback");
-      expect(within(pollingStationSearching).getByText("aan het zoeken …")).toBeVisible();
+
+      await waitFor(async () => {
+        const pollingStationSearching = await screen.findByTestId("pollingStationNumberInputFeedback");
+        expect(within(pollingStationSearching).getByText("aan het zoeken …")).toBeVisible();
+      });
     });
 
     test("Selecting polling station with second data entry opens correct page", async () => {

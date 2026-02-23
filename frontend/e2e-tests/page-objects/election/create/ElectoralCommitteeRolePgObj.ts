a56df9302b106b34fd174fbe7e41ba03aac7fcbc
Tamara <96083100+lama-d3v@users.noreply.github.com>
2026-02-23 13:58:10 +0100

    Fixed full-flow e2e test

--- frontend/e2e-tests/page-objects/election/create/ElectoralCommitteeRolePgObj.ts
+++ frontend/e2e-tests/page-objects/election/create/ElectoralCommitteeRolePgObj.ts
@@ -7,7 +7,7 @@ export class ElectoralCommitteeRolePgObj {
 
   constructor(protected readonly page: Page) {
     this.header = page.getByRole("heading", { level: 2, name: "Rol van het stembureau" });
-    this.gsb = page.getByRole("checkbox", { name: "Gemeentelijk stembureau (GSB)" });
+    this.gsb = page.getByRole("radio", { name: "Gemeentelijk stembureau (GSB)" });
     this.next = page.getByRole("button", { name: "Volgende" });
   }
 }

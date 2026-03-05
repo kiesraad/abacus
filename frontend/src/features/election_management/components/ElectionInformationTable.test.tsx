b29f04c29335cc99cc2d163f559b90bfe0473118
Tamara <96083100+lama-d3v@users.noreply.github.com>
2026-03-05 17:52:06 +0100

    Added tests for CSB for election management

--- frontend/src/features/election_management/components/ElectionInformationTable.test.tsx
+++ frontend/src/features/election_management/components/ElectionInformationTable.test.tsx
@@ -2,7 +2,7 @@ import { waitFor } from "@testing-library/react";
 import * as ReactRouter from "react-router";
 import { beforeEach, describe, expect, test, vi } from "vitest";
 
-import { committeeSessionMockData } from "@/testing/api-mocks/CommitteeSessionMockData";
+import { committeeSessionMockData, csbCommitteeSessionMockData } from "@/testing/api-mocks/CommitteeSessionMockData";
 import { TestUserProvider } from "@/testing/TestUserProvider";
 import { render, screen, within } from "@/testing/test-utils";
 import type { CommitteeSessionStatus, Role } from "@/types/generated/openapi";
@@ -11,7 +11,7 @@ import { ElectionInformationTable } from "./ElectionInformationTable";
 
 const navigate = vi.fn();
 
-const renderTable = (
+const renderGSBTable = (
   userRole: Role,
   electionNumberOfVoters: number,
   committeeSessionNumber: number,
@@ -61,6 +61,55 @@ const renderTable = (
   );
 };
 
+const renderCSBTable = (
+  userRole: Role,
+  committeeSessionNumber: number,
+  committeeSessionStatus: CommitteeSessionStatus,
+) => {
+  render(
+    <TestUserProvider userRole={userRole}>
+      <ElectionInformationTable
+        election={{
+          id: 1,
+          name: "Gemeenteraadsverkiezingen 2026",
+          committee_category: "CSB",
+          counting_method: "DSO",
+          election_id: "Heemdamseburg_2024",
+          location: "Heemdamseburg",
+          domain_id: "0035",
+          category: "Municipal",
+          number_of_seats: 0,
+          number_of_voters: 0,
+          election_date: "2024-11-30",
+          nomination_date: "2024-11-01",
+          political_groups: [
+            {
+              number: 1,
+              name: "Wijzen van Water en Wind",
+              candidates: [
+                {
+                  number: 1,
+                  initials: "A.",
+                  first_name: "Alice",
+                  last_name: "Foo",
+                  locality: "Amsterdam",
+                  gender: "Female",
+                },
+              ],
+            },
+          ],
+        }}
+        committeeSession={{
+          ...csbCommitteeSessionMockData,
+          number: committeeSessionNumber,
+          status: committeeSessionStatus,
+        }}
+        numberOfPollingStations={1}
+      />
+    </TestUserProvider>,
+  );
+};
+
 describe("ElectionInformationTable", () => {
   beforeEach(() => {
     vi.spyOn(ReactRouter, "useNavigate").mockImplementation(() => navigate);
@@ -68,7 +117,7 @@ describe("ElectionInformationTable", () => {
 
   test("renders a table with the election information for first committee session status created for coordinator", async () => {
     // Number of voters can technically not be lower than 1
-    renderTable("coordinator_gsb", 0, 1, "created");
+    renderGSBTable("coordinator_gsb", 0, 1, "created");
 
     const election_information_table = await screen.findByTestId("election-information-table");
     expect(election_information_table).toBeVisible();
@@ -91,7 +140,7 @@ describe("ElectionInformationTable", () => {
   });
 
   test("renders a table with the election information for first committee session status in_preparation for coordinator", async () => {
-    renderTable("coordinator_gsb", 1234, 1, "in_preparation");
+    renderGSBTable("coordinator_gsb", 1234, 1, "in_preparation");
 
     const election_information_table = await screen.findByTestId("election-information-table");
     expect(election_information_table).toBeVisible();
@@ -114,7 +163,7 @@ describe("ElectionInformationTable", () => {
   });
 
   test("renders a table with the election information for first committee session status data_entry for coordinator", async () => {
-    renderTable("coordinator_gsb", 1234, 1, "data_entry");
+    renderGSBTable("coordinator_gsb", 1234, 1, "data_entry");
 
     const election_information_table = await screen.findByTestId("election-information-table");
     expect(election_information_table).toBeVisible();
@@ -137,7 +186,7 @@ describe("ElectionInformationTable", () => {
   });
 
   test("renders a table with the election information for second committee session status created for coordinator", async () => {
-    renderTable("coordinator_gsb", 1234, 2, "created");
+    renderGSBTable("coordinator_gsb", 1234, 2, "created");
 
     const election_information_table = await screen.findByTestId("election-information-table");
     expect(election_information_table).toBeVisible();
@@ -160,7 +209,7 @@ describe("ElectionInformationTable", () => {
   });
 
   test("renders a table with the election information for administrator", async () => {
-    renderTable("administrator", 1234, 1, "created");
+    renderGSBTable("administrator", 1234, 1, "created");
 
     const election_information_table = await screen.findByTestId("election-information-table");
     expect(election_information_table).toBeVisible();
@@ -179,6 +228,21 @@ describe("ElectionInformationTable", () => {
     tableRows[3]!.click();
     await waitFor(() => {
       expect(navigate).toHaveBeenCalledWith("number-of-voters");
+    });
+  });
+
+  describe("CSB", () => {
+    test("renders a table with the election information for first committee session status created for coordinator", async () => {
+      renderCSBTable("coordinator_csb", 1, "created");
+
+      const election_information_table = await screen.findByTestId("election-information-table");
+      expect(election_information_table).toBeVisible();
+      expect(election_information_table).toHaveTableContent([
+        ["Verkiezing", "Gemeenteraadsverkiezingen 2026, 30 november"],
+        ["Kiesgebied", "0035 - Gemeente Heemdamseburg"],
+        ["Lijsten en kandidaten", "1 lijst en 1 kandidaat"],
+        ["Type stembureau", "Centraal stembureau"],
+      ]);
     });
   });
 });

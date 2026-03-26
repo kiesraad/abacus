0b7d832e1f5832ffaa29695c6f404326e273c98b
Tamara <96083100+lama-d3v@users.noreply.github.com>
2026-03-26 13:29:54 +0100

    Added Model P 22-2 Bijlage 1 rendering to backend (#2967)

--- backend/src/domain/models/model_p_22_2.rs
+++ backend/src/domain/models/model_p_22_2.rs
@@ -7,6 +7,7 @@ use crate::domain::{
     models::{PdfFileModel, PdfModel, ToPdfFileModel},
     results::{voters_counts::VotersCounts, votes_counts::VotesCounts},
     summary::SummaryDifferencesCounts,
+    votes_table::VotesTables,
 };
 
 /// Contains a limited summary of the election results, added up from the votes of all polling stations.
@@ -50,16 +51,39 @@ impl ToPdfFileModel for ModelP22_2Input {
     }
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct ModelP22_2Bijlage1Input {
+    pub election: ElectionWithPoliticalGroups,
+    pub hash: String,
+    pub creation_date_time: String,
+    pub votes_tables: VotesTables,
+}
+
+impl ToPdfFileModel for ModelP22_2Bijlage1Input {
+    fn to_pdf_file_model(self, file_name: String) -> PdfFileModel {
+        PdfFileModel::new(file_name, PdfModel::ModelP22_2Bijlage1(Box::new(self)))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, io::BufReader};
-
     use super::*;
+    use std::{fs::File, io::BufReader};
 
     #[test]
     fn test_json_matches_struct() {
         let reader = BufReader::new(File::open("templates/inputs/model-p-22-2.json").unwrap());
         serde_json::from_reader::<_, ModelP22_2Input>(reader)
             .expect("model-p-22-2.json should deserialize to struct ModelP22_2Input");
+    }
+
+    #[test]
+    fn test_bijlage_json_matches_struct() {
+        let reader =
+            BufReader::new(File::open("templates/inputs/model-p-22-2-bijlage1.json").unwrap());
+        serde_json::from_reader::<_, ModelP22_2Bijlage1Input>(reader).expect(
+            "model-p-22-2-bijlage1.json should deserialize to struct ModelP22_2Bijlage1Input",
+        );
     }
 }

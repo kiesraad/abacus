fd82ce98afc563b18a13faf576506467bf5e443d
Oliver <oliver@sensibly.nl>
2026-03-27 10:55:47 +0100

    Move data entry list title and empty state to DataEntryList

--- frontend/src/features/data_entry_home/components/CollapsibleDataEntryList.tsx
+++ frontend/src/features/data_entry_home/components/CollapsibleDataEntryList.tsx
@@ -1,4 +1,3 @@
-import { Alert } from "@/components/ui/Alert/Alert";
 import { DataEntryList } from "@/features/data_entry_home/components/DataEntryList";
 import type { DataEntryStatusWithUserStatus } from "@/features/data_entry_home/utils/util";
 import { t } from "@/i18n/translate";
@@ -27,14 +26,7 @@ export function CollapsibleDataEntryList({
             {t("data_entry_home.view_list")}
           </span>
         </summary>
-        <h3 className="mb-lg">{t("data_entry_home.choose_polling_station")}</h3>
-        {availableDataEntries.length === 0 ? (
-          <Alert type="notify" small>
-            <p>{t("data_entry_home.there_are_no_polling_stations_left_to_fill_in")}</p>
-          </Alert>
-        ) : (
-          <DataEntryList electionId={electionId} dataEntries={availableDataEntries} />
-        )}
+        <DataEntryList electionId={electionId} dataEntries={availableDataEntries} />
       </details>
     </div>
   );

b90a80134c354c6f7bf1341735d96ddcc5b94308
Oliver <oliver@sensibly.nl>
2026-03-30 17:48:31 +0200

    Show data entry picker only for GSB

--- frontend/src/features/data_entry_home/components/DataEntryHomePage.test.tsx
+++ frontend/src/features/data_entry_home/components/DataEntryHomePage.test.tsx
@@ -8,16 +8,40 @@ import type { Message } from "@/hooks/messages/MessagesContext";
 import { MessagesProvider } from "@/hooks/messages/MessagesProvider";
 import * as useMessages from "@/hooks/messages/useMessages";
 import * as useUser from "@/hooks/user/useUser";
-import { electionDetailsMockResponse, getElectionMockData } from "@/testing/api-mocks/ElectionMockData";
+import {
+  electionDetailsMockResponse,
+  getCSBElectionMockData,
+  getElectionMockData,
+} from "@/testing/api-mocks/ElectionMockData";
 import { getElectionStatusMockData, statusResponseMock } from "@/testing/api-mocks/ElectionStatusMockData";
 import { ElectionListRequestHandler, ElectionRequestHandler } from "@/testing/api-mocks/RequestHandlers";
-import { overrideOnce, server } from "@/testing/server";
+import { server } from "@/testing/server";
 import { renderReturningRouter, screen, within } from "@/testing/test-utils";
 import { getTypistUser } from "@/testing/user-mock-data";
-import type { ElectionDetailsResponse, ElectionStatusResponse } from "@/types/generated/openapi";
+import type {
+  CommitteeCategory,
+  CommitteeSession,
+  ElectionDetailsResponse,
+  ElectionStatusResponse,
+} from "@/types/generated/openapi";
 import { DataEntryHomePage } from "./DataEntryHomePage";
 
-function renderDataEntryHomePage({ statuses }: ElectionStatusResponse = statusResponseMock) {
+async function renderDataEntryHomePage(
+  category: CommitteeCategory,
+  session: Partial<CommitteeSession> = {},
+  { statuses }: ElectionStatusResponse = statusResponseMock,
+) {
+  let electionResponse: ElectionDetailsResponse;
+  switch (category) {
+    case "GSB":
+      electionResponse = getElectionMockData({}, session);
+      break;
+    case "CSB":
+      electionResponse = getCSBElectionMockData({}, session);
+      break;
+  }
+  server.use(http.get("/api/elections/1", () => HttpResponse.json(electionResponse, { status: 200 })));
+
   const refetch = vi.fn();
   vi.spyOn(useElectionStatus, "useElectionStatus").mockReturnValue({ statuses, refetch });
 
@@ -28,6 +52,7 @@ function renderDataEntryHomePage({ statuses }: ElectionStatusResponse = statusRe
       </MessagesProvider>
     </ElectionProvider>,
   );
+  expect(await screen.findByRole("heading", { level: 1, name: electionResponse.election.name })).toBeVisible();
 
   return { router, refetch };
 }
@@ -36,22 +61,10 @@ describe("DataEntryHomePage", () => {
   beforeEach(() => {
     vi.spyOn(useUser, "useUser").mockReturnValue(getTypistUser());
     server.use(ElectionListRequestHandler, ElectionRequestHandler);
-    overrideOnce("get", "/api/elections/1", 200, electionDetailsMockResponse);
   });
 
-  test("Election name", async () => {
-    renderDataEntryHomePage();
-
-    expect(
-      await screen.findByRole("heading", {
-        level: 1,
-        name: electionDetailsMockResponse.election.name,
-      }),
-    ).toBeVisible();
-  });
-
-  test("Alert not visible when unfinished", async () => {
-    renderDataEntryHomePage();
+  test.each(["GSB", "CSB"] as const)("Alert not visible when unfinished (%s)", async (cat: CommitteeCategory) => {
+    await renderDataEntryHomePage(cat);
 
     // Wait for the page to be loaded
     expect(
@@ -65,26 +78,28 @@ describe("DataEntryHomePage", () => {
     expect(screen.queryByText("Alle stembureaus zijn ingevoerd")).not.toBeInTheDocument();
   });
 
-  test("Alert visible when completed", async () => {
-    renderDataEntryHomePage(getElectionStatusMockData([{ status: "definitive" }, { status: "definitive" }]));
+  test.each(["GSB", "CSB"] as const)("Alert visible when completed (%s)", async (cat: CommitteeCategory) => {
+    const statusResponse = getElectionStatusMockData([{ status: "definitive" }, { status: "definitive" }]);
+    await renderDataEntryHomePage(cat, {}, statusResponse);
+
     expect(await screen.findByText("Alle stembureaus zijn ingevoerd")).toBeVisible();
   });
 
-  test("Messages are shown", async () => {
+  test.each(["GSB", "CSB"] as const)("Messages are shown (%s)", async (cat: CommitteeCategory) => {
     vi.spyOn(useMessages, "useMessages").mockReturnValue({
       hasMessages: vi.fn(),
       popMessages: vi.fn(() => [{ title: "Let op: fouten in het proces-verbaal" }] as Message[]),
       pushMessage: vi.fn(),
     });
 
-    renderDataEntryHomePage();
+    await renderDataEntryHomePage(cat);
 
     const alert = await screen.findByRole("alert");
     expect(within(alert).getByRole("strong")).toHaveTextContent("Let op: fouten in het proces-verbaal");
   });
 
-  test("Show different title for next data entry", async () => {
-    const { router } = renderDataEntryHomePage();
+  test.each(["GSB", "CSB"] as const)("Show different title for next entry (%s)", async (cat: CommitteeCategory) => {
+    const { router } = await renderDataEntryHomePage(cat);
     expect(await screen.findByRole("heading", { name: "Welk stembureau ga je invoeren?" })).toBeVisible();
 
     await router.navigate({ hash: "next" });
@@ -94,24 +109,8 @@ describe("DataEntryHomePage", () => {
     expect(await screen.findByRole("heading", { name: "Verder met een volgend stembureau?" })).toBeVisible();
   });
 
-  test("Alert when committee session is paused is shown", async () => {
-    server.use(
-      http.get("/api/elections/1", () =>
-        HttpResponse.json(getElectionMockData({}, { status: "paused" }) satisfies ElectionDetailsResponse, {
-          status: 200,
-        }),
-      ),
-    );
-
-    renderDataEntryHomePage();
-
-    // Wait for the page to be loaded
-    expect(
-      await screen.findByRole("heading", {
-        level: 1,
-        name: electionDetailsMockResponse.election.name,
-      }),
-    ).toBeVisible();
+  test.each(["GSB", "CSB"] as const)("Show alert when session is paused (%s)", async (cat: CommitteeCategory) => {
+    await renderDataEntryHomePage(cat, { status: "paused" });
 
     const pausedModal = await screen.findByRole("dialog");
     expect(within(pausedModal).getByRole("heading", { level: 3, name: "Invoer gepauzeerd" })).toBeVisible();
@@ -123,7 +122,7 @@ describe("DataEntryHomePage", () => {
   });
 
   test("Refresh statuses when the collapsed data entry list is opened", async () => {
-    const { refetch } = renderDataEntryHomePage();
+    const { refetch } = await renderDataEntryHomePage("GSB");
     await waitFor(() => {
       expect(refetch).toHaveBeenCalledTimes(1);
     });
@@ -132,5 +131,29 @@ describe("DataEntryHomePage", () => {
     const openList = await screen.findByTestId("openList");
     await user.click(openList);
     expect(refetch).toHaveBeenCalledTimes(2);
+  });
+
+  test("Show picker and collapsed list for GSB", async () => {
+    await renderDataEntryHomePage("GSB");
+
+    // Show picker and collapsed list
+    expect(await screen.findByLabelText("Voer het nummer in:")).toBeVisible();
+    expect(await screen.findByTestId("openList")).toBeVisible();
+
+    // List is hidden
+    expect(screen.queryByRole("heading", { name: "Kies het stembureau" })).not.toBeVisible();
+    expect(screen.queryByRole("table")).not.toBeVisible();
+  });
+
+  test("Show only expanded list for CSB", async () => {
+    await renderDataEntryHomePage("CSB");
+
+    // Picker and collapsed list not rendered
+    expect(screen.queryByLabelText("Voer het nummer in:")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("openList")).not.toBeInTheDocument();
+
+    // List is shown
+    expect(await screen.findByRole("heading", { name: "Kies het stembureau" })).toBeVisible();
+    expect(await screen.findByRole("table")).toBeVisible();
   });
 });

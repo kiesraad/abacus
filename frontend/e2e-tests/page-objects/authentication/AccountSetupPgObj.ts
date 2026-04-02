c97f968d5b3cc31ddbf5430ec327fe17767e10b4
Joep Schuurkes <145749778+jschuurk-kr@users.noreply.github.com>
2026-04-02 09:55:08 +0200

    Restrict incomplete users to setup and logout pages (#3117)
    Co-authored-by: Lionqueen94 <lionqueen94@gmail.com>

--- frontend/e2e-tests/page-objects/authentication/AccountSetupPgObj.ts
+++ frontend/e2e-tests/page-objects/authentication/AccountSetupPgObj.ts
@@ -1,12 +1,14 @@
 import type { Locator, Page } from "@playwright/test";
 
 export class AccountSetupPgObj {
+  readonly heading: Locator;
   readonly fullname: Locator;
   readonly password: Locator;
   readonly passwordRepeat: Locator;
   readonly saveBtn: Locator;
 
   constructor(protected readonly page: Page) {
+    this.heading = page.getByRole("heading", { level: 1, name: "Account instellen" });
     this.fullname = page.getByRole("textbox", { name: "Jouw naam (roepnaam +" });
     this.password = page.getByRole("textbox", { name: "Kies nieuw wachtwoord" });
     this.passwordRepeat = page.getByRole("textbox", { name: "Herhaal wachtwoord" });

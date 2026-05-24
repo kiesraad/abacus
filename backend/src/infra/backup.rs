3227283338774925f0c7daf839f7b2f02c315bf4
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2026-05-24 20:16:54 +0200

    feat: add creation of backup directory

--- backend/src/infra/backup.rs
+++ backend/src/infra/backup.rs
@@ -1,30 +1,41 @@
 use sqlx::SqlitePool;
 use std::error::Error;
+use std::path::{Path, PathBuf};
 
 pub struct BackupConfig {
-    pub directory: String,
+    pub directory: PathBuf,
 }
 
-async fn backup_database(pool: &SqlitePool, destination: &str) -> Result<(), sqlx::Error> {
+async fn backup_database(pool: &SqlitePool, destination: &Path) -> Result<(), Box<dyn Error>> {
+    let destination = destination
+        .to_str()
+        .ok_or("error formatting string")?
+        .to_string();
     let mut connection = pool.acquire().await?;
-    sqlx::query("VACUUM INTO ?").bind(destination).execute(&mut *connection).await?;
+    sqlx::query("VACUUM INTO ?")
+        .bind(destination)
+        .execute(&mut *connection)
+        .await?;
     Ok(())
 }
 
-async fn create_backup_directory() {
-    todo!();
+async fn create_backup_directory(backupconfig: &BackupConfig) -> Result<(), Box<dyn Error>> {
+    std::fs::create_dir_all(&backupconfig.directory)?;
+    Ok(())
 }
 
-pub async fn store_backup_directory_path(backupconfig: &mut BackupConfig) -> Result<(), Box<dyn Error>> {
+pub fn store_backup_directory_path(backupconfig: &mut BackupConfig) -> Result<(), Box<dyn Error>> {
     let executable_path = std::env::current_exe()?;
     let executable_directory = executable_path.parent().ok_or("parent does not exist")?;
     let backup_directory = executable_directory.join("database_backups");
-    let destination = backup_directory.to_str().ok_or("error formatting string")?.to_string();
-    backupconfig.directory = destination;
+    backupconfig.directory = backup_directory;
     Ok(())
 }
 
-pub async fn create_local_backup(pool: &SqlitePool, backupconfig: &BackupConfig) -> Result<(), Box<dyn Error>> {
+pub async fn create_local_backup(
+    pool: &SqlitePool,
+    backupconfig: &BackupConfig,
+) -> Result<(), Box<dyn Error>> {
     backup_database(pool, &backupconfig.directory).await?;
     Ok(())
 }

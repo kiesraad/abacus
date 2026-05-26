7a07a1efadf64f0d0dc60ff8a7fde6b5cc6c3e8f
mohs8421 <thomas.kosel@gmx.de>
2026-05-26 10:52:32 +0200

    Refactoring validate function signature (#3335)
    Signed-off-by: Thomas Kosel <thomas.kosel@voltfang.de>

--- backend/src/domain/results/political_group_total_votes.rs
+++ backend/src/domain/results/political_group_total_votes.rs
@@ -45,9 +45,8 @@ impl Validate for Vec<PoliticalGroupTotalVotes> {
     fn validate(
         &self,
         election: &ElectionWithPoliticalGroups,
-        validation_results: &mut ValidationResults,
         path: &FieldPath,
-    ) -> Result<(), DataError> {
+    ) -> Result<ValidationResults, DataError> {
         // check if the list of political group total votes has the correct length
         if election.political_groups.len() != self.len() {
             return Err(DataError::new(
@@ -57,6 +56,7 @@ impl Validate for Vec<PoliticalGroupTotalVotes> {
 
         // check each political group total votes
         let mut previous_number = PGNumber::from(0);
+        let mut validation_results = ValidationResults::default();
         for (i, pgv) in self.iter().enumerate() {
             let number = pgv.number;
             if number <= previous_number {
@@ -66,10 +66,12 @@ impl Validate for Vec<PoliticalGroupTotalVotes> {
             }
             previous_number = number;
 
-            pgv.total
-                .validate(election, validation_results, &path.index(i).field("total"))?;
+            validation_results.join(
+                pgv.total
+                    .validate(election, &path.index(i).field("total"))?,
+            );
         }
-        Ok(())
+        Ok(validation_results)
     }
 }
 
@@ -126,14 +128,8 @@ mod tests {
         // Remove first political group from election
         election.political_groups.remove(0);
 
-        let mut validation_results = ValidationResults::default();
-        let result = political_group_votes.validate(
-            &election,
-            &mut validation_results,
-            &"political_group_votes".into(),
-        );
+        let result = political_group_votes.validate(&election, &"political_group_votes".into());
 
-        assert!(result.is_err());
         assert!(
             result
                 .unwrap_err()
@@ -151,12 +147,8 @@ mod tests {
         political_group_votes[1].number = PGNumber::from(3);
         election.political_groups[1].number = PGNumber::from(3);
 
-        let mut validation_results = ValidationResults::default();
-        let result: Result<(), DataError> = political_group_votes.validate(
-            &election,
-            &mut validation_results,
-            &"political_group_votes".into(),
-        );
+        let result: Result<ValidationResults, DataError> =
+            political_group_votes.validate(&election, &"political_group_votes".into());
 
         assert!(result.is_ok());
     }
@@ -170,12 +162,8 @@ mod tests {
         political_group_votes[0].number = PGNumber::from(3);
         election.political_groups[0].number = PGNumber::from(3);
 
-        let mut validation_results = ValidationResults::default();
-        let result: Result<(), DataError> = political_group_votes.validate(
-            &election,
-            &mut validation_results,
-            &"political_group_votes".into(),
-        );
+        let result: Result<ValidationResults, DataError> =
+            political_group_votes.validate(&election, &"political_group_votes".into());
 
         assert!(result.is_err());
     }
@@ -193,14 +181,8 @@ mod tests {
                 votes: 0,
             });
 
-        let mut validation_results = ValidationResults::default();
-        let result = political_group_votes.validate(
-            &election,
-            &mut validation_results,
-            &"political_group_votes".into(),
-        );
+        let result = political_group_votes.validate(&election, &"political_group_votes".into());
 
-        assert!(result.is_err());
         assert!(
             result
                 .unwrap_err()
@@ -217,12 +199,7 @@ mod tests {
         // Change number of the last candidate on the first list
         political_group_votes[0].candidate_votes[2].number = CandidateNumber::from(5);
 
-        let mut validation_results = ValidationResults::default();
-        let result = political_group_votes.validate(
-            &election,
-            &mut validation_results,
-            &"political_group_votes".into(),
-        );
+        let result = political_group_votes.validate(&election, &"political_group_votes".into());
 
         assert!(result.is_ok());
     }
@@ -235,12 +212,7 @@ mod tests {
         // Change number of the second candidate on the first list to a non-increasing number
         political_group_votes[0].candidate_votes[1].number = CandidateNumber::from(1);
 
-        let mut validation_results = ValidationResults::default();
-        let result = political_group_votes.validate(
-            &election,
-            &mut validation_results,
-            &"political_group_votes".into(),
-        );
+        let result = political_group_votes.validate(&election, &"political_group_votes".into());
 
         assert!(result.is_err());
     }

eee787d50ce6dd1e0f4024f33a44aca2bc1d010e
Lionqueen94 <lionqueen94@gmail.com>
2026-06-30 20:45:15 +0200

    Added extra ApportionmentTable story

--- frontend/src/features/apportionment/components/ApportionmentTable.stories.tsx
+++ frontend/src/features/apportionment/components/ApportionmentTable.stories.tsx
@@ -1,6 +1,8 @@
 import type { StoryObj } from "@storybook/react-vite";
 import { expect } from "storybook/test";
 import * as gte19Seats from "../testing/gte-19-seats";
+import * as gte19SeatsAndP7 from "../testing/gte-19-seats-and-p7";
+import { getNotAssignedSeats } from "../utils/utils";
 import { ApportionmentTable } from "./ApportionmentTable";
 
 export const Default: StoryObj = {
@@ -26,6 +28,36 @@ export const Default: StoryObj = {
       ["3", "Political Group C", "1", "-", "1"],
       ["4", "Political Group D", "1", "1", "2"],
       ["5", "Blanco (Smit, G.)", "1", "1", "2"],
+      ["", "Totaal", "19", "4", "23"],
+    ]);
+  },
+};
+
+export const NotAssignedSeats: StoryObj = {
+  render: () => {
+    return (
+      <ApportionmentTable
+        standings={gte19SeatsAndP7.seat_assignment.standings}
+        politicalGroups={gte19SeatsAndP7.election.political_groups}
+        fullSeats={gte19SeatsAndP7.seat_assignment.full_seats}
+        residualSeats={gte19SeatsAndP7.seat_assignment.residual_seats}
+        seats={gte19SeatsAndP7.seat_assignment.seats}
+        notAssignedSeats={getNotAssignedSeats(gte19SeatsAndP7.state)}
+      />
+    );
+  },
+  play: async ({ canvas }) => {
+    const table = canvas.getByRole("table");
+    await expect(table).toBeVisible();
+    expect(table).toHaveTableContent([
+      ["Lijst", "Lijstnaam", "Volle zetels", "Restzetels", "Totaal zetels"],
+      ["", "Nog niet toegewezen", "-", "3", "3"],
+      ["1", "Partij voor de Stemmer", "9", "1", "10"],
+      ["2", "Algemene Partij", "2", "-", "2"],
+      ["3", "KEUS", "2", "-", "2"],
+      ["4", "Algemene Lijst", "2", "-", "2"],
+      ["5", "Unie van kandidaten", "2", "-", "2"],
+      ["6", "Lijst van stemmers", "2", "-", "2"],
       ["", "Totaal", "19", "4", "23"],
     ]);
   },

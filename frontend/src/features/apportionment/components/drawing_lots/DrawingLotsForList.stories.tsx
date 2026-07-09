07b5a1d9b66bc86704b6c71608910d1691f80788
Lionqueen94 <lionqueen94@gmail.com>
2026-07-09 13:30:17 +0200

    Implemented copy change in design for DrawingLotsForList on DrawingLotsPage

--- frontend/src/features/apportionment/components/drawing_lots/DrawingLotsForList.stories.tsx
+++ frontend/src/features/apportionment/components/drawing_lots/DrawingLotsForList.stories.tsx
@@ -26,7 +26,7 @@ export const HighestAverage: StoryObj = {
       "De partij met het hoogste gemiddeld aantal stemmen na het toewijzen van de restzetel krijgt de restzetel",
     );
     await expect(listitems[3]).toHaveTextContent(
-      "Lijst 2 - Algemene Partij, Lijst 3 - KEUS, Lijst 4 - Algemene Lijst, Lijst 5 - Unie van kandidaten en Lijst 6 - Lijst van stemmers krijgen samen het hoogste gemiddeld aantal stemmen per zetel (46 2/3 stemmen)",
+      "Lijst 2 - Algemene Partij, Lijst 3 - KEUS, Lijst 4 - Algemene Lijst, Lijst 5 - Unie van kandidaten en Lijst 6 - Lijst van stemmers hebben samen het hoogste gemiddeld aantal stemmen per zetel (46 2/3 stemmen)",
     );
     await expect(listitems[4]).toHaveTextContent("Daarom moet er geloot worden welke lijst de restzetel krijgt");
   },
--- frontend/src/features/apportionment/components/drawing_lots/DrawingLotsPage.test.tsx
+++ frontend/src/features/apportionment/components/drawing_lots/DrawingLotsPage.test.tsx
@@ -171,7 +171,7 @@ describe("DrawingLotsPage", () => {
       "De partij met het hoogste gemiddeld aantal stemmen na het toewijzen van de restzetel krijgt de restzetel",
     );
     expect(listitems[3]).toHaveTextContent(
-      "Lijst 2 - Algemene Partij, Lijst 3 - KEUS, Lijst 4 - Algemene Lijst, Lijst 5 - Unie van kandidaten en Lijst 6 - Lijst van stemmers krijgen samen het hoogste gemiddeld aantal stemmen per zetel (46 2/3 stemmen)",
+      "Lijst 2 - Algemene Partij, Lijst 3 - KEUS, Lijst 4 - Algemene Lijst, Lijst 5 - Unie van kandidaten en Lijst 6 - Lijst van stemmers hebben samen het hoogste gemiddeld aantal stemmen per zetel (46 2/3 stemmen)",
     );
     expect(listitems[4]).toHaveTextContent("Daarom moet er geloot worden welke lijst de restzetel krijgt");
 

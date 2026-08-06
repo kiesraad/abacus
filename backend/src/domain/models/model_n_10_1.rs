f62a840a033927efcfd73ebe6118000ef60e43c6
Lionqueen94 <lionqueen94@gmail.com>
2026-08-07 01:14:04 +0200

    Add Model N 10-1 & inlegvel (#3759)
    Co-authored-by: Sander Boom <5956570+sanderboom@users.noreply.github.com>

---
+++ backend/src/domain/models/model_n_10_1.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+
+use crate::domain::{
+    election::Election,
+    models::{PdfFileModel, PdfModel, ToPdfFileModel, votes_table::CandidatesTables},
+    polling_station::PollingStation,
+};
+
+#[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct ModelN10_1Input {
+    pub election: Election,
+    pub polling_station: PollingStation,
+    pub candidates_tables: CandidatesTables,
+}
+
+impl ToPdfFileModel for ModelN10_1Input {
+    fn to_pdf_file_model(self, file_name: String) -> PdfFileModel {
+        PdfFileModel::new(file_name, PdfModel::ModelN10_1(Box::new(self)))
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct ModelN10_1InlegvelInput {
+    pub election: Election,
+    pub polling_station: PollingStation,
+}
+
+impl ToPdfFileModel for ModelN10_1InlegvelInput {
+    fn to_pdf_file_model(self, file_name: String) -> PdfFileModel {
+        PdfFileModel::new(file_name, PdfModel::ModelN10_1Inlegvel(Box::new(self)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{fs::File, io::BufReader};
+
+    #[test]
+    fn test_json_variations_match_struct() {
+        let mut reader = BufReader::new(
+            File::open("templates/inputs/model-n-10-1-variations/model-n-10-1-GR.json").unwrap(),
+        );
+        serde_json::from_reader::<_, ModelN10_1Input>(reader)
+            .expect("model-n-10-1-GR.json should deserialize to struct ModelN10_1Input");
+
+        reader = BufReader::new(
+            File::open("templates/inputs/model-n-10-1-variations/model-n-10-1-PS.json").unwrap(),
+        );
+        serde_json::from_reader::<_, ModelN10_1Input>(reader)
+            .expect("model-n-10-1-PS.json should deserialize to struct ModelN10_1Input");
+
+        reader = BufReader::new(
+            File::open("templates/inputs/model-n-10-1-variations/model-n-10-1-WS.json").unwrap(),
+        );
+        serde_json::from_reader::<_, ModelN10_1Input>(reader)
+            .expect("model-n-10-1-WS.json should deserialize to struct ModelN10_1Input");
+    }
+
+    #[test]
+    fn test_inlegvel_json_matches_struct() {
+        let reader =
+            BufReader::new(File::open("templates/inputs/model-n-10-1-inlegvel.json").unwrap());
+        serde_json::from_reader::<_, ModelN10_1InlegvelInput>(reader).expect(
+            "model-n-10-1-inlegvel.json should deserialize to struct ModelN10_1InlegvelInput",
+        );
+    }
+}

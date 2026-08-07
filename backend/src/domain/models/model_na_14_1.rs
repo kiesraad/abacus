2cd9a754470178b7bd7dcbbaab4225cd7baa4831
Lionqueen94 <lionqueen94@gmail.com>
2026-08-07 14:53:54 +0200

    Added Model Na 14-1 versie 2

--- backend/src/domain/models/model_na_14_1.rs
+++ backend/src/domain/models/model_na_14_1.rs
@@ -3,8 +3,13 @@ use serde::{Deserialize, Serialize};
 use crate::domain::{
     committee_session::CommitteeSession,
     election::Election,
-    models::{PdfFileModel, PdfModel, ToPdfFileModel, votes_table::CandidatesTables},
+    investigation::PollingStationInvestigation,
+    models::{
+        PdfFileModel, PdfModel, ToPdfFileModel,
+        votes_table::{CandidatesTables, VotesTablesWithOnlyPreviousVotes},
+    },
     polling_station::PollingStation,
+    results::common_polling_station_results::CommonPollingStationResultsWithoutVotes,
 };
 
 #[derive(Serialize, Deserialize)]
@@ -19,6 +24,23 @@ pub struct ModelNa14_1Versie1Input {
 impl ToPdfFileModel for ModelNa14_1Versie1Input {
     fn to_pdf_file_model(self, file_name: String) -> PdfFileModel {
         PdfFileModel::new(file_name, PdfModel::ModelNa14_1Versie1(Box::new(self)))
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct ModelNa14_1Versie2Input {
+    pub committee_session: CommitteeSession,
+    pub election: Election,
+    pub polling_station: PollingStation,
+    pub previous_results: CommonPollingStationResultsWithoutVotes,
+    pub investigation: PollingStationInvestigation,
+    pub votes_tables: VotesTablesWithOnlyPreviousVotes,
+}
+
+impl ToPdfFileModel for ModelNa14_1Versie2Input {
+    fn to_pdf_file_model(self, file_name: String) -> PdfFileModel {
+        PdfFileModel::new(file_name, PdfModel::ModelNa14_1Versie2(Box::new(self)))
     }
 }
 
@@ -58,6 +80,39 @@ mod tests {
         );
         serde_json::from_reader::<_, ModelNa14_1Versie1Input>(reader).expect(
             "model-na-14-1-versie-1-WS.json should deserialize to struct ModelNa14_1Versie1Input",
+        );
+    }
+
+    #[test]
+    fn test_version_2_json_variations_match_struct() {
+        let mut reader = BufReader::new(
+            File::open(
+                "templates/inputs/model-na-14-1-versie-2-variations/model-na-14-1-versie-2-GR.json",
+            )
+            .unwrap(),
+        );
+        serde_json::from_reader::<_, ModelNa14_1Versie2Input>(reader).expect(
+            "model-na-14-1-versie-2-GR.json should deserialize to struct ModelNa14_1Versie2Input",
+        );
+
+        reader = BufReader::new(
+            File::open(
+                "templates/inputs/model-na-14-1-versie-2-variations/model-na-14-1-versie-2-PS.json",
+            )
+            .unwrap(),
+        );
+        serde_json::from_reader::<_, ModelNa14_1Versie2Input>(reader).expect(
+            "model-na-14-1-versie-2-PS.json should deserialize to struct ModelNa14_1Versie2Input",
+        );
+
+        reader = BufReader::new(
+            File::open(
+                "templates/inputs/model-na-14-1-versie-2-variations/model-na-14-1-versie-2-WS.json",
+            )
+            .unwrap(),
+        );
+        serde_json::from_reader::<_, ModelNa14_1Versie2Input>(reader).expect(
+            "model-na-14-1-versie-2-WS.json should deserialize to struct ModelNa14_1Versie2Input",
         );
     }
 }

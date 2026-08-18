dc2b3154ee3ff0e0a15d875dfd3e765ce73e40cb
Lionqueen94 <lionqueen94@gmail.com>
2026-08-19 00:29:17 +0200

    Remove duplicate DownloadButton and replaced href with "#"

--- frontend/e2e-tests/page-objects/investigations/InvestigationPrintCorrigendumPgObj.ts
+++ frontend/e2e-tests/page-objects/investigations/InvestigationPrintCorrigendumPgObj.ts
@@ -4,12 +4,12 @@ export class InvestigationPrintCorrigendumPgObj {
   readonly header: Locator;
   readonly backToInvestigationsButton: Locator;
   readonly continueButton: Locator;
-  readonly downloadButton: Locator;
+  readonly downloadLink: Locator;
 
   constructor(protected readonly page: Page) {
     this.header = page.getByRole("heading", { name: "Print het corrigendum" });
     this.backToInvestigationsButton = page.getByRole("link", { name: "Terug naar alle onderzoeken" });
     this.continueButton = page.getByRole("link", { name: "Verder naar bevindingen" });
-    this.downloadButton = page.getByTestId("download-corrigendum-button");
+    this.downloadLink = page.getByRole("link", { name: /Download corrigendum voor stembureau \d+/ });
   }
 }

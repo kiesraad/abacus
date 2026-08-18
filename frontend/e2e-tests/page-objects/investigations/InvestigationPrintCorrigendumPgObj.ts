9919a7d36bde886e469f92b3bf53c4d77d31b2b8
Lionqueen94 <lionqueen94@gmail.com>
2026-08-18 18:15:21 +0200

    Fixed e2e test

--- frontend/e2e-tests/page-objects/investigations/InvestigationPrintCorrigendumPgObj.ts
+++ frontend/e2e-tests/page-objects/investigations/InvestigationPrintCorrigendumPgObj.ts
@@ -4,12 +4,12 @@ export class InvestigationPrintCorrigendumPgObj {
   readonly header: Locator;
   readonly backToInvestigationsButton: Locator;
   readonly continueButton: Locator;
-  readonly downloadLink: Locator;
+  readonly downloadButton: Locator;
 
   constructor(protected readonly page: Page) {
     this.header = page.getByRole("heading", { name: "Print het corrigendum" });
     this.backToInvestigationsButton = page.getByRole("link", { name: "Terug naar alle onderzoeken" });
     this.continueButton = page.getByRole("link", { name: "Verder naar bevindingen" });
-    this.downloadLink = page.getByRole("link", { name: /Download corrigendum voor stembureau \d+/ });
+    this.downloadButton = page.getByTestId("download-corrigendum-button");
   }
 }

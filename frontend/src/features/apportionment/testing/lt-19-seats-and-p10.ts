915a98fc3d6bd66e99e2e6d7638af4a115aef756
Lionqueen94 <lionqueen94@gmail.com>
2026-09-15 17:10:06 +0200

    Corrected testdata to have correct authority_id and other fields for CSB, and corrected election_id, name, domain, etc. where needed

--- frontend/src/features/apportionment/testing/lt-19-seats-and-p10.ts
+++ frontend/src/features/apportionment/testing/lt-19-seats-and-p10.ts
@@ -862,7 +862,7 @@ export const election: ElectionWithPoliticalGroups = {
   committee_category: "CSB",
   election_id: "GR2026_Juinen",
   location: "Juinen",
-  authority_id: "0035",
+  authority_id: "CSB",
   authority_name: "Juinen",
   authority_region: "Juinen",
   district: { district: "None" },
--- frontend/src/features/election_management/components/ElectionInformationTable.test.tsx
+++ frontend/src/features/election_management/components/ElectionInformationTable.test.tsx
@@ -22,10 +22,10 @@ const renderGSBTable = (
       <ElectionInformationTable
         election={{
           id: 1,
-          name: "Gemeenteraadsverkiezingen 2026",
+          name: "Gemeenteraad Heemdamseburg 2026",
           committee_category: "GSB",
           counting_method: "DSO",
-          election_id: "Heemdamseburg_2024",
+          election_id: "GR2026_Heemdamseburg",
           location: "Heemdamseburg",
           authority_id: "0035",
           authority_name: "Heemdamseburg",
@@ -36,8 +36,8 @@ const renderGSBTable = (
           sub_category: "GR2",
           number_of_seats: 29,
           number_of_voters: electionNumberOfVoters,
-          election_date: "2024-11-30",
-          nomination_date: "2024-11-01",
+          election_date: "2026-11-30",
+          nomination_date: "2026-11-01",
           political_groups: [
             {
               number: 1,
